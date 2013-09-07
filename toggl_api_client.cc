235ce436bda53a4e135b03fdd358f369b4a633ad
Tanel Lebedev <tanel.lebedev@gmail.com>
2013-09-07 19:09:06 +0300

    Basic Toggl objects

---
+++ toggl_api_client.cc
@@ -0,0 +1,26 @@
+
+
+namespace toggl {
+
+	class BaseModel {
+	};
+
+	class User : public BaseModel {
+	};
+
+	class Workspace : public BaseModel {
+	};
+
+	class Project : public BaseModel {
+	};
+
+	class Task : public BaseModel {
+	};
+
+	class Tag : public BaseModel {
+	};
+
+	class TimeEntry : public BaseModel {
+	};
+
+}
