cf0791bb30882fa2e7fdbce15d6ee3b0a74c9b5a
Tanel Lebedev <tanel.lebedev@gmail.com>
2013-09-08 00:25:16 +0300

    Get Toggl API token from env

--- kopsik.cc
+++ kopsik.cc
@@ -1,4 +1,15 @@
+#include <iostream>
+#include <cstdlib>
 
-int main() {
-
+int main(int argc, char* argv[]) {
+	char* buf = getenv("TOGGL_API_TOKEN");
+	if (!buf) {
+		std::cerr << "Please set TOGGL_API_TOKEN in environment" << std::endl;
+		return 1;
+	}
+	if (argc < 2) {
+		std::cerr << "Usage: " << argv[0] << " COMMAND" << std::endl;
+		return 1;
+	}
+	return 0;
 }
