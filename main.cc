cfc72a44a952f4e9e7cee88cf33d732daef67feb
Tanel Lebedev <tanel.lebedev@gmail.com>
2013-09-13 23:51:15 +0300

    More tests

--- main.cc
+++ main.cc
@@ -73,20 +73,50 @@ int Kopsik::main(const std::vector<std::string>& args) {
 
 			poco_assert(user.TimeEntries.size() == 3);
 			TimeEntry te1 = user.TimeEntries[0];
+			poco_assert(te1.ID == 89818605);
+			poco_assert(te1.GUID == "07fba193-91c4-0ec8-2894-820df0548a8f");
+			poco_assert(te1.WID == 123456789);
+			poco_assert(te1.PID == 2567324);
+			poco_assert(te1.Start == "2013-09-05T06:33:50+00:00");
+			poco_assert(te1.Stop == "2013-09-05T08:19:46+00:00");
+			poco_assert(te1.DurationInSeconds == 6356);
+			poco_assert(te1.Description == "Important things");
+			poco_assert(te1.DurOnly == false);
 			TimeEntry te2 = user.TimeEntries[1];
+			poco_assert(te2.ID == 89833438);
+			poco_assert(te2.GUID == "6c97dc31-582e-7662-1d6f-5e9d623b1685");
 			TimeEntry te3 = user.TimeEntries[2];
+			poco_assert(te3.ID == 89837259);
+			poco_assert(te3.GUID == "6a958efd-0e9a-d777-7e19-001b2d7ced92");
 
 			poco_assert(user.Workspaces.size() == 2);
 			Workspace w1 = user.Workspaces[0];
+			poco_assert(w1.ID == 123456788);
+			poco_assert(w1.Name == "stuff");
 			Workspace w2 = user.Workspaces[1];
+			poco_assert(w2.ID == 123456789);
+			poco_assert(w2.Name == "Toggl");
 
 			poco_assert(user.Tasks.size() == 2);
 			Task task1 = user.Tasks[0];
+			poco_assert(task1.ID == 1894794);
+			poco_assert(task1.Name == "blog (writing)");
+			poco_assert(task1.WID == 123456789);
+			poco_assert(task1.PID == 2585208);
 			Task task2 = user.Tasks[1];
+			poco_assert(task2.ID == 1879027);
+			poco_assert(task2.Name == "dadsad");
+			poco_assert(task2.WID == 123456789);
+			poco_assert(task2.PID == 2707058);
 
 			poco_assert(user.Clients.size() == 2);
 			Client c1 = user.Clients[0];
+			poco_assert(c1.ID == 1385144);
+			poco_assert(c1.Name == "ABC");
 			Client c2 = user.Clients[1];
+			poco_assert(c2.ID == 878318);
+			poco_assert(c2.Name == "Big Client");
+			poco_assert(c2.GUID == "59b464cd-0f8e-e601-ff44-f135225a6738");
 		}
 	}
 
