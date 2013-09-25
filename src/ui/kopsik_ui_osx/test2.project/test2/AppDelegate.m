4ef3a882eed54b154bca63891f9e92397752c36f
Tambet Masik <tambet@toggl.com>
2013-09-25 13:08:22 +0300

    set kopsik db_path

--- src/ui/kopsik_ui_osx/test2.project/test2/AppDelegate.m
+++ src/ui/kopsik_ui_osx/test2.project/test2/AppDelegate.m
@@ -26,8 +26,32 @@
   int minor = 0;
   int patch = 0;
   kopsik_version(&major, &minor, &patch);
+  
+  kopsik_set_db_path([self.supportDirector UTF8String]);
   NSString *s = [NSString stringWithFormat:@"libkopsik version %d.%d.%d", major, minor, patch];
   NSLog(@"%@", s);
 }
+
+- (NSString *)supportDirector
+{
+  NSString *path;
+  NSError *error;
+  NSArray* paths = NSSearchPathForDirectoriesInDomains(NSApplicationSupportDirectory, NSUserDomainMask, YES);
+  if ([paths count] == 0) {
+    NSLog(@"Unable to access application support directory!");
+  }
+  path = [[paths objectAtIndex:0] stringByAppendingPathComponent:@"libkopsik"];
+  
+	if (![[NSFileManager defaultManager] fileExistsAtPath:path]){
+		if (![[NSFileManager defaultManager] createDirectoryAtPath:path
+                                   withIntermediateDirectories:NO
+                                                    attributes:nil
+                                                         error:&error]){
+			NSLog(@"Create directory error: %@", error);
+		}
+	}
+  return path;
+}
+
 
 @end
