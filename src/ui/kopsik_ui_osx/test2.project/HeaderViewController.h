b84967ea44af53dc2e83a513938f192532dff01d
Tanel Lebedev <tanel.lebedev@gmail.com>
2013-10-02 20:48:34 +0300

    Fix copyright

--- src/ui/kopsik_ui_osx/test2.project/HeaderViewController.h
+++ src/ui/kopsik_ui_osx/test2.project/HeaderViewController.h
@@ -3,7 +3,7 @@
 //  kopsik_ui_osx
 //
 //  Created by Tanel Lebedev on 19/09/2013.
-//  Copyright (c) 2013 Alari. All rights reserved.
+//  Copyright (c) 2013 kopsik developers. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
