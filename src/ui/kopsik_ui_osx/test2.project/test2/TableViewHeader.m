f3742c027267182196feb58e7f851c5a574c0785
Tanel Lebedev <tanel.lebedev@gmail.com>
2013-10-12 00:36:51 +0200

    Table view header

---
+++ src/ui/kopsik_ui_osx/test2.project/test2/TableViewHeader.m
@@ -0,0 +1,29 @@
+//
+//  TableViewHeader.m
+//  kopsik_ui_osx
+//
+//  Created by Tanel Lebedev on 12/10/2013.
+//  Copyright (c) 2013 Alari. All rights reserved.
+//
+
+#import "TableViewHeader.h"
+
+@implementation TableViewHeader
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+	[super drawRect:dirtyRect];
+	
+    // Drawing code here.
+}
+
+@end
