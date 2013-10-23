5baad554954909e949e0f846a8c6516288472446
Tanel Lebedev <tanel.lebedev@gmail.com>
2013-10-23 18:01:32 +0300

    Hide dock icon in osx app

--- src/ui/osx/test2.project/test2/AppDelegate.m
+++ src/ui/osx/test2.project/test2/AppDelegate.m
@@ -30,6 +30,9 @@ NSString *kTimeTotalUnknown = @"--:--";
 {
   NSLog(@"applicationDidFinishLaunching");
 
+  // Hide dock icon
+  [NSApp setActivationPolicy: NSApplicationActivationPolicyAccessory];  
+
   [Bugsnag startBugsnagWithApiKey:@"2a46aa1157256f759053289f2d687c2f"];
   [Bugsnag configuration].releaseStage = @"development";
   
