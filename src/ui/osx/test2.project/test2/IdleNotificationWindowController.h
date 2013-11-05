03e931e63dfe6a2fca0a7ec7b02f3cbffd7e398f
Tanel Lebedev <tanel.lebedev@gmail.com>
2013-11-06 01:46:03 +0200

    Idle notification window

--- src/ui/osx/test2.project/test2/IdleNotificationWindowController.h
+++ src/ui/osx/test2.project/test2/IdleNotificationWindowController.h
@@ -1,19 +1,18 @@
 //
-//  IdleDetectedViewController.h
+//  IdleNotificationWindowController.h
 //  kopsik_ui_osx
 //
-//  Created by Tanel Lebedev on 04/11/2013.
-//  Copyright (c) 2013 kopsik developers. All rights reserved.
+//  Created by Tanel Lebedev on 06/11/2013.
+//  Copyright (c) 2013 Alari. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
 
-@interface IdleDetectedViewController : NSViewController
+@interface IdleNotificationWindowController : NSWindowController
 @property (weak) IBOutlet NSButton *stopButton;
 @property (weak) IBOutlet NSButton *splitButton;
 @property (weak) IBOutlet NSButton *ignoreButton;
 - (IBAction)stopButtonClicked:(id)sender;
 - (IBAction)splitButtonClicked:(id)sender;
 - (IBAction)ignoreButtonClicked:(id)sender;
-
 @end
---
+++ src/ui/osx/test2.project/test2/IdleNotificationWindowController.m
@@ -0,0 +1,43 @@
+//
+//  IdleNotificationWindowController.m
+//  kopsik_ui_osx
+//
+//  Created by Tanel Lebedev on 06/11/2013.
+//  Copyright (c) 2013 Alari. All rights reserved.
+//
+
+#import "IdleNotificationWindowController.h"
+
+@interface IdleNotificationWindowController ()
+
+@end
+
+@implementation IdleNotificationWindowController
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+- (IBAction)stopButtonClicked:(id)sender {
+}
+
+- (IBAction)splitButtonClicked:(id)sender {
+}
+
+- (IBAction)ignoreButtonClicked:(id)sender {
+}
+
+
+@end
