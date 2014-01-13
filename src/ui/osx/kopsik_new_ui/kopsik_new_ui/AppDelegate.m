67a174beda198f8d8ca4f56ab23b85ea15fc583e
Tanel Lebedev <tanel.lebedev@gmail.com>
2014-01-13 17:31:09 +0200

    New UI prototype (osx)

---
+++ src/ui/osx/kopsik_new_ui/kopsik_new_ui/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  kopsik_new_ui
+//
+//  Created by Alari on 12/17/13.
+//  Copyright (c) 2013 Alari. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ src/ui/osx/kopsik_new_ui/kopsik_new_uiTests/kopsik_new_uiTests.m
@@ -0,0 +1,34 @@
+//
+//  kopsik_new_uiTests.m
+//  kopsik_new_uiTests
+//
+//  Created by Alari on 12/17/13.
+//  Copyright (c) 2013 Alari. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface kopsik_new_uiTests : XCTestCase
+
+@end
+
+@implementation kopsik_new_uiTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
