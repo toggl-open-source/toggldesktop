fbe13d3a7bfec103b0fb0872ed325c459df103e6
Tanel Lebedev <tanel.lebedev@gmail.com>
2014-02-20 04:30:41 +0200

    Remove unused UI code

--- src/ui/osx/test2.project/FeedbackWindowController.m
+++ src/ui/osx/test2.project/FeedbackWindowController.m
@@ -17,19 +17,6 @@
 
 @implementation FeedbackWindowController
 
-- (id)initWithWindow:(NSWindow *)window {
-    self = [super initWithWindow:window];
-    if (self) {
-    }
-    return self;
-}
-
-- (void)windowDidLoad {
-    [super windowDidLoad];
-    
-    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
-}
-
 - (IBAction)uploadImageClick:(id)sender {
   NSOpenPanel* panel = [NSOpenPanel openPanel];
   [panel beginWithCompletionHandler:^(NSInteger result){
@@ -64,7 +51,6 @@
   [self.selectedImageTextField setHidden:YES];
   [self.contentTextView setString:@""];
   [self.topicComboBox setStringValue:@""];
-
 }
 
 @end
