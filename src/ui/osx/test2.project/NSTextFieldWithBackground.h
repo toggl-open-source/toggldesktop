34ada75924e4fdb395c0ddb75303098c56bc3d46
Tanel Lebedev <tanel.lebedev@gmail.com>
2014-03-05 16:16:27 +0200

    Redraw dirty rect in any case, fixes #98 (osx)

---
+++ src/ui/osx/test2.project/NSTextFieldWithBackground.h
@@ -0,0 +1,13 @@
+//
+//  NSTextFieldWithBackground.h
+//  kopsik_ui_osx
+//
+//  Created by Tanel Lebedev on 05/03/2014.
+//  Copyright (c) 2014 Alari. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface NSTextFieldWithBackground : NSTextField
+
+@end
