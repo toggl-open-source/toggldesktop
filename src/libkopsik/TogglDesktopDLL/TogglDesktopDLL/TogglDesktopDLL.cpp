99da0a10a4b5087fa3c6b4a157682d38df01fd26
Tanel Lebedev <tanel.lebedev@gmail.com>
2014-03-13 00:52:49 +0200

    DLL was missing crypto

--- src/libkopsik/TogglDesktopDLL/TogglDesktopDLL/TogglDesktopDLL.cpp
+++
@@ -1,6 +0,0 @@
-// TogglDesktopDLL.cpp : Defines the exported functions for the DLL application.
-//
-
-#include "stdafx.h"
-
-
--- src/libkopsik/TogglDesktopDLL/TogglDesktopDLL/TogglDesktopDLL.h
+++
@@ -1,5 +0,0 @@
-#ifdef TOGGLDESKTOPSDLL_EXPORTS
-#define TOGGLDESKTOPDLL_API __declspec(dllexport)
-#else
-#define TOGGLDESKTOPDLL_API __declspec(dllimport)
-#endif
