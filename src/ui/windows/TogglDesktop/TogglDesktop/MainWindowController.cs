be9fd429a11db4ca8083effc7c5480115b07ddea
Tanel Lebedev <tanel.lebedev@gmail.com>
2014-03-13 00:55:18 +0200

    Start with Windows UI

---
+++ src/ui/windows/TogglDesktop/TogglDesktop/MainWindowController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TogglDesktop
+{
+    public partial class MainWindowController : Form
+    {
+        public MainWindowController()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindowController_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
