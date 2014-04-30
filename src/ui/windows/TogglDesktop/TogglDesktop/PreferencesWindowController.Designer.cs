bfc72cf46e957218caeb37a3acf2d0f63e46c391
Tanel Lebedev <tanel.lebedev@gmail.com>
2014-04-30 22:39:19 +0300

    Preferences window (win)

--- src/ui/windows/TogglDesktop/TogglDesktop/PreferencesWindowController.Designer.cs
+++ src/ui/windows/TogglDesktop/TogglDesktop/PreferencesWindowController.Designer.cs
@@ -28,20 +28,140 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PreferencesWindowController));
+            this.checkBoxUseProxy = new System.Windows.Forms.CheckBox();
+            this.groupBoxProxySettings = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.groupBoxProxySettings.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // checkBoxUseProxy
+            // 
+            this.checkBoxUseProxy.AutoSize = true;
+            this.checkBoxUseProxy.Location = new System.Drawing.Point(13, 24);
+            this.checkBoxUseProxy.Name = "checkBoxUseProxy";
+            this.checkBoxUseProxy.Size = new System.Drawing.Size(169, 17);
+            this.checkBoxUseProxy.TabIndex = 0;
+            this.checkBoxUseProxy.Text = "Use proxy to connect to Toggl";
+            this.checkBoxUseProxy.UseVisualStyleBackColor = true;
+            // 
+            // groupBoxProxySettings
+            // 
+            this.groupBoxProxySettings.Controls.Add(this.textBox4);
+            this.groupBoxProxySettings.Controls.Add(this.textBox3);
+            this.groupBoxProxySettings.Controls.Add(this.textBox2);
+            this.groupBoxProxySettings.Controls.Add(this.textBox1);
+            this.groupBoxProxySettings.Controls.Add(this.label4);
+            this.groupBoxProxySettings.Controls.Add(this.label3);
+            this.groupBoxProxySettings.Controls.Add(this.label2);
+            this.groupBoxProxySettings.Controls.Add(this.label1);
+            this.groupBoxProxySettings.Location = new System.Drawing.Point(13, 47);
+            this.groupBoxProxySettings.Name = "groupBoxProxySettings";
+            this.groupBoxProxySettings.Size = new System.Drawing.Size(252, 143);
+            this.groupBoxProxySettings.TabIndex = 1;
+            this.groupBoxProxySettings.TabStop = false;
+            this.groupBoxProxySettings.Text = "Proxy Settings";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Host";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Port";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Usernane";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Password";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(81, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(156, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(81, 54);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(156, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(81, 80);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(156, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(81, 108);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(156, 20);
+            this.textBox4.TabIndex = 7;
             // 
             // PreferencesWindowController
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.groupBoxProxySettings);
+            this.Controls.Add(this.checkBoxUseProxy);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "PreferencesWindowController";
             this.Text = "Preferences";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.PreferencesWindowController_FormClosing);
+            this.groupBoxProxySettings.ResumeLayout(false);
+            this.groupBoxProxySettings.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckBox checkBoxUseProxy;
+        private System.Windows.Forms.GroupBox groupBoxProxySettings;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
