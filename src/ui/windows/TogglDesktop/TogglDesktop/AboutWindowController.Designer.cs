465076df05bfb9ecad2866a80728470a29fa088c
Tanel Lebedev <tanel.lebedev@gmail.com>
2014-04-30 12:47:19 +0300

    About window controls

--- src/ui/windows/TogglDesktop/TogglDesktop/AboutWindowController.Designer.cs
+++ src/ui/windows/TogglDesktop/TogglDesktop/AboutWindowController.Designer.cs
@@ -29,20 +29,107 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutWindowController));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelVersion = new System.Windows.Forms.Label();
+            this.richTextBoxCredits = new System.Windows.Forms.RichTextBox();
+            this.buttonCheckingForUpdate = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(94, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 68);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(108, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "TogglDesktop";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelVersion
+            // 
+            this.labelVersion.AutoSize = true;
+            this.labelVersion.Location = new System.Drawing.Point(108, 108);
+            this.labelVersion.Name = "labelVersion";
+            this.labelVersion.Size = new System.Drawing.Size(69, 13);
+            this.labelVersion.TabIndex = 2;
+            this.labelVersion.Text = "Version 1.0.0";
+            // 
+            // richTextBoxCredits
+            // 
+            this.richTextBoxCredits.Enabled = false;
+            this.richTextBoxCredits.Location = new System.Drawing.Point(12, 139);
+            this.richTextBoxCredits.Name = "richTextBoxCredits";
+            this.richTextBoxCredits.Size = new System.Drawing.Size(260, 71);
+            this.richTextBoxCredits.TabIndex = 3;
+            this.richTextBoxCredits.Text = "";
+            // 
+            // buttonCheckingForUpdate
+            // 
+            this.buttonCheckingForUpdate.Location = new System.Drawing.Point(12, 216);
+            this.buttonCheckingForUpdate.Name = "buttonCheckingForUpdate";
+            this.buttonCheckingForUpdate.Size = new System.Drawing.Size(260, 23);
+            this.buttonCheckingForUpdate.TabIndex = 4;
+            this.buttonCheckingForUpdate.Text = "Checking for update...";
+            this.buttonCheckingForUpdate.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 253);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Release channel";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(151, 253);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 6;
             // 
             // AboutWindowController
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(284, 290);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonCheckingForUpdate);
+            this.Controls.Add(this.richTextBoxCredits);
+            this.Controls.Add(this.labelVersion);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "AboutWindowController";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.AboutWindowController_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelVersion;
+        private System.Windows.Forms.RichTextBox richTextBoxCredits;
+        private System.Windows.Forms.Button buttonCheckingForUpdate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
