45e3e9e920cd5fa98b48a6caea67a3ad61647b53
Tanel Lebedev <tanel.lebedev@gmail.com>
2014-05-20 19:41:36 +0300

    Display time entry fields in editor, except for tags

--- src/ui/windows/TogglDesktop/TogglDesktop/TimeEntryEditViewController.cs
+++ src/ui/windows/TogglDesktop/TogglDesktop/TimeEntryEditViewController.cs
@@ -15,6 +15,8 @@ namespace TogglDesktop
         public TimeEntryEditViewController()
         {
             InitializeComponent();
+
+            KopsikApi.OnTimeEntryEditor += OnTimeEntryEditor;
         }
 
         private void TimeEntryEditViewController_Load(object sender, EventArgs e)
@@ -48,5 +50,61 @@ namespace TogglDesktop
             KopsikApi.kopsik_view_time_entry_list(KopsikApi.ctx);
         }
 
+        void OnTimeEntryEditor(
+    bool open,
+    ref KopsikApi.KopsikTimeEntryViewItem te,
+    string focused_field_name)
+        {
+            KopsikApi.KopsikTimeEntryViewItem n = te;
+            DisplayTimeEntryEditor(open, n, focused_field_name);
+        }
+
+        void DisplayTimeEntryEditor(
+            bool open,
+            KopsikApi.KopsikTimeEntryViewItem te,
+            string focused_field_name)
+        {
+            if (InvokeRequired)
+            {
+                Invoke((MethodInvoker)delegate { DisplayTimeEntryEditor(open, te, focused_field_name); });
+                return;
+            }
+            if (!comboBoxDescription.Focused)
+            {
+                comboBoxDescription.Text = te.Description;
+            }
+            if (!comboBoxProject.Focused)
+            {
+                comboBoxProject.Text = te.ProjectAndTaskLabel;
+            }
+            if (!textBoxDuration.Focused)
+            {
+                textBoxDuration.Text = te.Duration;
+            }
+            if (!textBoxStartTime.Focused)
+            {
+                textBoxStartTime.Text = te.StartTimeString;
+            }
+            if (!textBoxEndTime.Focused)
+            {
+                textBoxEndTime.Text = te.EndTimeString;
+            }
+            if (!dateTimePickerStartDate.Focused)
+            {
+                dateTimePickerStartDate.Value = KopsikApi.DateTimeFromUnix(te.Started);
+            }
+            checkBoxBillable.Checked = te.Billable;
+            if (te.UpdatedAt >= 0)
+            {
+                DateTime updatedAt = KopsikApi.DateTimeFromUnix(te.UpdatedAt);
+                toolStripStatusLabelLastUpdate.Text = updatedAt.ToString();
+                toolStripStatusLabelLastUpdate.Visible = true;
+            }
+            else
+            {
+                toolStripStatusLabelLastUpdate.Visible = false;
+            }
+            
+        }
     }
 }
