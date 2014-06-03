bfaa27202925df3c515834e3d920013c998dddb6
Tanel Lebedev <tanel.lebedev@gmail.com>
2014-06-04 01:55:32 +0300

    Disallow multiple instances, fixes #343

--- src/ui/windows/TogglDesktop/TogglDesktop/Program.cs
+++ src/ui/windows/TogglDesktop/TogglDesktop/Program.cs
@@ -6,12 +6,14 @@ using System.Windows.Forms;
 using System.Runtime.InteropServices;
 using System.Reflection;
 using System.Diagnostics;
+using System.Threading;
 
 namespace TogglDesktop
 {
     static class Program
     {
         public static bool ShuttingDown = false;
+        private const string appGUID = "29067F3B-F706-46CB-92D2-1EA1E72A4CE3";
 
         /// <summary>
         /// The main entry point for the application.
@@ -19,17 +21,27 @@ namespace TogglDesktop
         [STAThread]
         static void Main()
         {
-            Bugsnag.Library.BugSnag bs = new Bugsnag.Library.BugSnag()
+            using (Mutex mutex = new Mutex(false, "Global\\" + appGUID))
             {
-                apiKey = "2a46aa1157256f759053289f2d687c2f"
-            };
+                if (!mutex.WaitOne(0, false))
+                {
+                    MessageBox.Show("Another copy of Toggl Desktop is already running." +
+                        Environment.NewLine + "This copy will now quit.");
+                    return;
+                }
 
-            Application.ThreadException += new System.Threading.ThreadExceptionEventHandler(Application_ThreadException);
-            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
-            Application.ApplicationExit += new EventHandler(onApplicationExit);
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainWindowController());
+                Bugsnag.Library.BugSnag bs = new Bugsnag.Library.BugSnag()
+                {
+                    apiKey = "2a46aa1157256f759053289f2d687c2f"
+                };
+
+                Application.ThreadException += new System.Threading.ThreadExceptionEventHandler(Application_ThreadException);
+                AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+                Application.ApplicationExit += new EventHandler(onApplicationExit);
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new MainWindowController());
+            }
         }
 
         static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
