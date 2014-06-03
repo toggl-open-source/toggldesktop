ebc917ab77321b2343a78c18f6b282e78faadb1e
Tanel Lebedev <tanel.lebedev@gmail.com>
2014-06-03 11:46:34 +0300

    bugsnag client for c#

---
+++ third_party/net-bugsnag/Bugsnag.Library/Data/Event.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bugsnag.Library.Data
+{
+    /// <summary>
+    /// Error event that Bugsnag should be notified of. A notifier
+    /// can choose to group notices into an array to minimize network traffic, or
+    /// can notify Bugsnag each time an event occurs
+    /// </summary>
+    [DataContract]
+    public class Event
+    {
+        public Event()
+        {
+            Exceptions = new List<Exception>();
+        }
+
+        /// <summary>
+        /// A unique identifier for a user affected by this event. This could be 
+        /// any distinct identifier that makes sense for your application/platform.
+        /// This field is optional but highly recommended.
+        /// </summary>
+        [DataMember(Name = "userId")]
+        public string UserId
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// The version number of the application which generated the error.
+        /// (optional, default none)
+        /// </summary>
+        [DataMember(Name = "appVersion")]
+        public string AppVersion
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// The operating system version of the client that the error was 
+        /// generated on. (optional, default none)
+        /// </summary>
+        [DataMember(Name = "osVersion")]
+        public string OSVersion
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// The release stage that this error occurred in, for example 
+        /// "development" or "production". This can be any string, but "production"
+        /// will be highlighted differently in bugsnag in the future, so please use
+        /// "production" appropriately.
+        /// </summary>
+        [DataMember(Name = "releaseStage")]
+        public string ReleaseStage
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// A string representing what was happening in the application at the 
+        /// time of the error. This string could be used for grouping purposes, 
+        /// depending on the event.
+        /// Usually this would represent the controller and action in a server 
+        /// based project. It could represent the screen that the user was 
+        /// interacting with in a client side project.
+        /// For example,
+        ///   * On Ruby on Rails the context could be controller#action
+        ///   * In Android, the context could be the top most Activity.
+        ///   * In iOS, the context could be the name of the top most UIViewController
+        /// </summary>
+        [DataMember(Name = "context")]
+        public string Context
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// An array of exceptions that occurred during this event. Most of the
+        /// time there will only be one exception, but some languages support 
+        /// "nested" or "caused by" exceptions. In this case, exceptions should 
+        /// be unwrapped and added to the array one at a time. The first exception
+        /// raised should be first in this array.
+        /// </summary>
+        [DataMember(Name = "exceptions")]
+        public List<Exception> Exceptions
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// An object containing any further data you wish to attach to this error
+        /// event. This should contain one or more objects, with each object being
+        /// displayed in its own tab on the event details on the Bugsnag website.
+        /// (Optional).
+        /// </summary>
+        [DataMember(Name = "metaData")]
+        public object ExtraData
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ third_party/net-bugsnag/Bugsnag.Library/Data/Exception.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bugsnag.Library.Data
+{
+    /// <summary>
+    /// Exception that occurred during this event. Most of the
+    /// time there will only be one exception, but some languages support 
+    /// "nested" or "caused by" exceptions. In this case, exceptions should 
+    /// be unwrapped and added to the exceptions array one at a time. 
+    /// </summary>
+    [DataContract]
+    public class Exception
+    {
+        /// <summary>
+        /// The class of error that occurred. This field is used to group the
+        /// errors together so should not contain any contextual information
+        /// that would prevent correct grouping. This would ordinarily be the
+        /// Exception name when dealing with an exception.
+        /// </summary>
+        [DataMember(Name = "errorClass")]
+        public string ErrorClass
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// The error message associated with the error. Usually this will 
+        /// contain some information about this specific instance of the error
+        /// and is not used to group the errors (optional, default none).
+        /// </summary>
+        [DataMember(Name = "message")]
+        public string Message
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// An array of stacktrace objects. Each object represents one line in
+        /// the exception's stacktrace. Bugsnag uses this information to help
+        /// with error grouping, as well as displaying it to the user.
+        /// </summary>
+        [DataMember(Name = "stacktrace")]
+        public List<Stacktrace> Stacktrace
+        {
+            get;
+            set;
+        }
+    }
+}
