6eb69ce7bfd215afe956151e933231328e6917f7
Tanel Lebedev <tanel.lebedev@gmail.com>
2014-06-05 15:46:43 +0300

    Fix bugsnag in c# app

--- src/ui/windows/TogglDesktop/TogglDesktop/Program.cs
+++ src/ui/windows/TogglDesktop/TogglDesktop/Program.cs
@@ -14,6 +14,16 @@ namespace TogglDesktop
     {
         public static bool ShuttingDown = false;
         private const string appGUID = "29067F3B-F706-46CB-92D2-1EA1E72A4CE3";
+        public static Bugsnag.Library.BugSnag bugsnag = null;
+        public static UInt64 UserID = 0;
+
+        public static bool IsLoggedIn
+        {
+            get
+            {
+                return UserID > 0;
+            }
+        }
 
         /// <summary>
         /// The main entry point for the application.
@@ -30,9 +40,10 @@ namespace TogglDesktop
                     return;
                 }
 
-                Bugsnag.Library.BugSnag bs = new Bugsnag.Library.BugSnag()
+                bugsnag = new Bugsnag.Library.BugSnag()
                 {
-                    apiKey = "2a46aa1157256f759053289f2d687c2f"
+                    apiKey = "2a46aa1157256f759053289f2d687c2f",
+                    OSVersion = Environment.OSVersion.ToString()
                 };
 
                 Application.ThreadException += new System.Threading.ThreadExceptionEventHandler(Application_ThreadException);
@@ -46,14 +57,18 @@ namespace TogglDesktop
 
         static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
         {
-            Bugsnag.Library.BugSnag bs = new Bugsnag.Library.BugSnag();
-            bs.Notify(e.ExceptionObject as Exception);
+            bugsnag.Notify(e.ExceptionObject as Exception, new
+            {
+                UserID = UserID
+            });
         }
 
         static void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
         {
-            Bugsnag.Library.BugSnag bs = new Bugsnag.Library.BugSnag();
-            bs.Notify(e.Exception);
+            bugsnag.Notify(e.Exception, new
+            {
+                UserID = UserID
+            });
         }
 
         private static void onApplicationExit(object sender, EventArgs e)
