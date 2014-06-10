f1f22542c6b0b7480d84eab46d81e0dd6c0e8da9
IndrekV <indzuliin@gmail.com>
2014-06-10 14:24:09 +0300

    Fixed About window logo and dimensions (win)

--- src/ui/windows/TogglDesktop/TogglDesktop/Properties/Resources.Designer.cs
+++ src/ui/windows/TogglDesktop/TogglDesktop/Properties/Resources.Designer.cs
@@ -80,6 +80,16 @@ namespace TogglDesktop.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap logo64 {
+            get {
+                object obj = ResourceManager.GetObject("logo64", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
         /// </summary>
         internal static System.Drawing.Icon toggl {
