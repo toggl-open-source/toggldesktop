2aedb3ae0f117a43193421155756fbafdc9e4e89
Tanel Lebedev <tanel.lebedev@gmail.com>
2014-06-26 22:22:12 +0300

    set poco libs install name (mac)

--- src/ui/osx/TogglDesktop/fix_dylib_paths.sh
+++ src/ui/osx/TogglDesktop/fix_dylib_paths.sh
@@ -1,17 +1,29 @@
-dylib=src/ui/osx/TogglDesktop/build/Release/TogglDesktop.app/Contents/Frameworks/Kopsik.dylib
+#!/usr/bin/env bash
 
 fix() {
-	lib=$1
-	oldpath=$(otool -L $dylib|fgrep $lib|awk -F' ' '{print $1}')
-	install_name_tool -change $oldpath @loader_path/$lib $dylib
+	wheretofix=$1
+	lib=$2
+	oldpath=$(otool -L $wheretofix|fgrep $lib|awk -F' ' '{print $1}'|grep -v @loader_path|grep -v ":")
+	if [ -z "$oldpath" ]; then
+		return
+	fi
+	install_name_tool -change $oldpath @loader_path/$lib $wheretofix
+	install_name_tool -id @loader_path/$lib $wheretofix
 }
 
-fix libPocoUtil.16.dylib
-fix libPocoData.16.dylib
-fix libPocoNetSSL.16.dylib
-fix libPocoXML.16.dylib
-fix libPocoDataSQLite.16.dylib
-fix libPocoNet.16.dylib
-fix libPocoFoundation.16.dylib
-fix libPocoCrypto.16.dylib
+fix_poco_paths() {
+	f=$1
+	fix $f libPocoUtil.16.dylib
+	fix $f libPocoData.16.dylib
+	fix $f libPocoNetSSL.16.dylib
+	fix $f libPocoXML.16.dylib
+	fix $f libPocoDataSQLite.16.dylib
+	fix $f libPocoNet.16.dylib
+	fix $f libPocoFoundation.16.dylib
+	fix $f libPocoCrypto.16.dylib
+}
 
+for f in src/ui/osx/TogglDesktop/build/Release/TogglDesktop.app/Contents/Frameworks/*.dylib
+do
+	fix_poco_paths $f
+done
