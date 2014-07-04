04d3d6f3878437b08ce247dd95587232742c46fb
Tanel Lebedev <tanel.lebedev@gmail.com>
2014-07-05 02:38:09 +0300

    te list layout issues

--- src/ui/windows/TogglDesktop/TogglDesktop/TimeEntryListViewController.Designer.cs
+++ src/ui/windows/TogglDesktop/TogglDesktop/TimeEntryListViewController.Designer.cs
@@ -29,8 +29,8 @@
         private void InitializeComponent()
         {
             this.panelHeader = new System.Windows.Forms.Panel();
-            this.entries = new System.Windows.Forms.FlowLayoutPanel();
             this.timerEditViewController = new TogglDesktop.TimerEditViewController();
+            this.entries = new System.Windows.Forms.FlowLayoutPanel();
             this.panelHeader.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -45,17 +45,6 @@
             this.panelHeader.Size = new System.Drawing.Size(314, 56);
             this.panelHeader.TabIndex = 0;
             // 
-            // entries
-            // 
-            this.entries.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.entries.AutoScroll = true;
-            this.entries.Location = new System.Drawing.Point(0, 59);
-            this.entries.Name = "entries";
-            this.entries.Size = new System.Drawing.Size(311, 292);
-            this.entries.TabIndex = 1;
-            // 
             // timerEditViewController
             // 
             this.timerEditViewController.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
@@ -67,10 +56,25 @@
             this.timerEditViewController.Size = new System.Drawing.Size(314, 56);
             this.timerEditViewController.TabIndex = 0;
             // 
+            // entries
+            // 
+            this.entries.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.entries.AutoScroll = true;
+            this.entries.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.entries.Location = new System.Drawing.Point(0, 59);
+            this.entries.Name = "entries";
+            this.entries.Size = new System.Drawing.Size(311, 292);
+            this.entries.TabIndex = 1;
+            this.entries.WrapContents = false;
+            // 
             // TimeEntryListViewController
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.Color.WhiteSmoke;
             this.Controls.Add(this.entries);
             this.Controls.Add(this.panelHeader);
