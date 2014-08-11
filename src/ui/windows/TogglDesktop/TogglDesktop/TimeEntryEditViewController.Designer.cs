86d1187bd7d7ec343edd79b7253fbf50099a4fbc
IndrekV <indzuliin@gmail.com>
2014-08-11 10:52:34 +0300

    Proper names for labels in edit view (win)

--- src/ui/windows/TogglDesktop/TogglDesktop/TimeEntryEditViewController.Designer.cs
+++ src/ui/windows/TogglDesktop/TogglDesktop/TimeEntryEditViewController.Designer.cs
@@ -29,18 +29,18 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.label1 = new System.Windows.Forms.Label();
+            this.labelDescription = new System.Windows.Forms.Label();
             this.labelProject = new System.Windows.Forms.Label();
             this.linkAddProject = new System.Windows.Forms.LinkLabel();
-            this.label3 = new System.Windows.Forms.Label();
+            this.labelDuration = new System.Windows.Forms.Label();
             this.textBoxDuration = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
+            this.labelStartEnd = new System.Windows.Forms.Label();
             this.textBoxStartTime = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
+            this.labelDash = new System.Windows.Forms.Label();
             this.textBoxEndTime = new System.Windows.Forms.TextBox();
-            this.label6 = new System.Windows.Forms.Label();
+            this.labelDate = new System.Windows.Forms.Label();
             this.dateTimePickerStartDate = new System.Windows.Forms.DateTimePicker();
-            this.label7 = new System.Windows.Forms.Label();
+            this.labelTags = new System.Windows.Forms.Label();
             this.statusStripLastUpdate = new System.Windows.Forms.StatusStrip();
             this.toolStripStatusLabelLastUpdate = new System.Windows.Forms.ToolStripStatusLabel();
             this.buttonsPanel = new System.Windows.Forms.Panel();
@@ -73,15 +73,15 @@
             this.panelAddProject.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label1
+            // labelDescription
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Lucida Sans Unicode", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.label1.Location = new System.Drawing.Point(15, 13);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(72, 15);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Description:";
+            this.labelDescription.AutoSize = true;
+            this.labelDescription.Font = new System.Drawing.Font("Lucida Sans Unicode", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.labelDescription.Location = new System.Drawing.Point(15, 13);
+            this.labelDescription.Name = "labelDescription";
+            this.labelDescription.Size = new System.Drawing.Size(72, 15);
+            this.labelDescription.TabIndex = 0;
+            this.labelDescription.Text = "Description:";
             // 
             // labelProject
             // 
@@ -106,15 +106,15 @@
             this.linkAddProject.Text = "Add new project";
             this.linkAddProject.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkAddProject_LinkClicked);
             // 
-            // label3
+            // labelDuration
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Lucida Sans Unicode", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.label3.Location = new System.Drawing.Point(15, 8);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(57, 15);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "Duration:";
+            this.labelDuration.AutoSize = true;
+            this.labelDuration.Font = new System.Drawing.Font("Lucida Sans Unicode", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.labelDuration.Location = new System.Drawing.Point(15, 8);
+            this.labelDuration.Name = "labelDuration";
+            this.labelDuration.Size = new System.Drawing.Size(57, 15);
+            this.labelDuration.TabIndex = 5;
+            this.labelDuration.Text = "Duration:";
             // 
             // textBoxDuration
             // 
@@ -125,15 +125,15 @@
             this.textBoxDuration.TabIndex = 4;
             this.textBoxDuration.Leave += new System.EventHandler(this.textBoxDuration_Leave);
             // 
-            // label4
+            // labelStartEnd
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Lucida Sans Unicode", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.label4.Location = new System.Drawing.Point(15, 8);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(88, 15);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "Start-end time:";
+            this.labelStartEnd.AutoSize = true;
+            this.labelStartEnd.Font = new System.Drawing.Font("Lucida Sans Unicode", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.labelStartEnd.Location = new System.Drawing.Point(15, 8);
+            this.labelStartEnd.Name = "labelStartEnd";
+            this.labelStartEnd.Size = new System.Drawing.Size(88, 15);
+            this.labelStartEnd.TabIndex = 7;
+            this.labelStartEnd.Text = "Start-end time:";
             // 
             // textBoxStartTime
             // 
@@ -144,14 +144,14 @@
             this.textBoxStartTime.TabIndex = 6;
             this.textBoxStartTime.Leave += new System.EventHandler(this.textBoxStartTime_Leave);
             // 
-            // label5
+            // labelDash
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(175, 8);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(10, 13);
-            this.label5.TabIndex = 9;
-            this.label5.Text = "-";
+            this.labelDash.AutoSize = true;
+            this.labelDash.Location = new System.Drawing.Point(175, 8);
+            this.labelDash.Name = "labelDash";
+            this.labelDash.Size = new System.Drawing.Size(10, 13);
+            this.labelDash.TabIndex = 9;
+            this.labelDash.Text = "-";
             // 
             // textBoxEndTime
             // 
@@ -162,15 +162,15 @@
             this.textBoxEndTime.TabIndex = 7;
             this.textBoxEndTime.Leave += new System.EventHandler(this.textBoxEndTime_Leave);
             // 
-            // label6
+            // labelDate
             // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Lucida Sans Unicode", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.label6.Location = new System.Drawing.Point(15, 8);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(34, 15);
-            this.label6.TabIndex = 11;
-            this.label6.Text = "Date:";
+            this.labelDate.AutoSize = true;
+            this.labelDate.Font = new System.Drawing.Font("Lucida Sans Unicode", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.labelDate.Location = new System.Drawing.Point(15, 8);
+            this.labelDate.Name = "labelDate";
+            this.labelDate.Size = new System.Drawing.Size(34, 15);
+            this.labelDate.TabIndex = 11;
+            this.labelDate.Text = "Date:";
             // 
             // dateTimePickerStartDate
             // 
@@ -181,15 +181,15 @@
             this.dateTimePickerStartDate.TabIndex = 9;
             this.dateTimePickerStartDate.Leave += new System.EventHandler(this.dateTimePickerStartDate_Leave);
             // 
-            // label7
+            // labelTags
             // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Lucida Sans Unicode", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.label7.Location = new System.Drawing.Point(15, 38);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(36, 15);
-            this.label7.TabIndex = 13;
-            this.label7.Text = "Tags:";
+            this.labelTags.AutoSize = true;
+            this.labelTags.Font = new System.Drawing.Font("Lucida Sans Unicode", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.labelTags.Location = new System.Drawing.Point(15, 38);
+            this.labelTags.Name = "labelTags";
+            this.labelTags.Size = new System.Drawing.Size(36, 15);
+            this.labelTags.TabIndex = 13;
+            this.labelTags.Text = "Tags:";
             // 
             // statusStripLastUpdate
             // 
@@ -295,7 +295,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panelBottom.Controls.Add(this.panelDateTag);
             this.panelBottom.Controls.Add(this.panelStartEndTime);
-            this.panelBottom.Controls.Add(this.label3);
+            this.panelBottom.Controls.Add(this.labelDuration);
             this.panelBottom.Controls.Add(this.textBoxDuration);
             this.panelBottom.Location = new System.Drawing.Point(0, 77);
             this.panelBottom.Name = "panelBottom";
@@ -309,9 +309,9 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panelDateTag.Controls.Add(this.checkedListBoxTags);
-            this.panelDateTag.Controls.Add(this.label6);
+            this.panelDateTag.Controls.Add(this.labelDate);
             this.panelDateTag.Controls.Add(this.dateTimePickerStartDate);
-            this.panelDateTag.Controls.Add(this.label7);
+            this.panelDateTag.Controls.Add(this.labelTags);
             this.panelDateTag.Controls.Add(this.checkBoxBillable);
             this.panelDateTag.Location = new System.Drawing.Point(0, 60);
             this.panelDateTag.MinimumSize = new System.Drawing.Size(0, 100);
@@ -322,9 +322,9 @@
             // 
             // panelStartEndTime
             // 
-            this.panelStartEndTime.Controls.Add(this.label4);
+            this.panelStartEndTime.Controls.Add(this.labelStartEnd);
             this.panelStartEndTime.Controls.Add(this.textBoxEndTime);
-            this.panelStartEndTime.Controls.Add(this.label5);
+            this.panelStartEndTime.Controls.Add(this.labelDash);
             this.panelStartEndTime.Controls.Add(this.textBoxStartTime);
             this.panelStartEndTime.Location = new System.Drawing.Point(0, 30);
             this.panelStartEndTime.Name = "panelStartEndTime";
@@ -499,7 +499,7 @@
             this.Controls.Add(this.comboBoxProject);
             this.Controls.Add(this.labelProject);
             this.Controls.Add(this.comboBoxDescription);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelDescription);
             this.Name = "TimeEntryEditViewController";
             this.Size = new System.Drawing.Size(317, 400);
             this.Load += new System.EventHandler(this.TimeEntryEditViewController_Load);
@@ -521,20 +521,20 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelDescription;
         private CustomComboBox comboBoxDescription;
         private System.Windows.Forms.Label labelProject;
         private CustomComboBox comboBoxProject;
         private System.Windows.Forms.LinkLabel linkAddProject;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelDuration;
         private System.Windows.Forms.TextBox textBoxDuration;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labelStartEnd;
         private System.Windows.Forms.TextBox textBoxStartTime;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label labelDash;
         private System.Windows.Forms.TextBox textBoxEndTime;
-        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label labelDate;
         private System.Windows.Forms.DateTimePicker dateTimePickerStartDate;
-        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label labelTags;
         private System.Windows.Forms.StatusStrip statusStripLastUpdate;
         private System.Windows.Forms.Panel buttonsPanel;
         private System.Windows.Forms.Button buttonDelete;
