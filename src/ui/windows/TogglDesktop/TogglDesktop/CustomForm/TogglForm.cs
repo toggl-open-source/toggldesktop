fec67ef78cb26aba2e087b915e56a8ff9afe9012
IndrekV <indzuliin@gmail.com>
2014-08-25 15:55:48 +0300

    Added shadow to custom form border (win)

--- src/ui/windows/TogglDesktop/TogglDesktop/CustomForm/TogglForm.cs
+++ src/ui/windows/TogglDesktop/TogglDesktop/CustomForm/TogglForm.cs
@@ -39,6 +39,17 @@ namespace TogglDesktop
             this.FormStyle = CreateFormStyle();
         }
 
+        private const int CS_DROPSHADOW = 0x20000;
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ClassStyle |= CS_DROPSHADOW;
+                return cp;
+            }
+        }
+
         protected override void OnResize(EventArgs e)
         {
             base.OnResize(e);
