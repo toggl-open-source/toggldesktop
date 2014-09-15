d23175c9377c573128f5189562c3cf3cec1e2f5c
IndrekV <indzuliin@gmail.com>
2014-09-15 13:32:13 +0300

    Fixed timer duration field cutoff (win)

--- src/ui/windows/TogglDesktop/TogglDesktop/TimerEditViewController.Designer.cs
+++ src/ui/windows/TogglDesktop/TogglDesktop/TimerEditViewController.Designer.cs
@@ -37,8 +37,10 @@
             this.linkLabelDuration = new System.Windows.Forms.Label();
             this.labelClearProject = new System.Windows.Forms.Label();
             this.panelLeft = new System.Windows.Forms.Panel();
+            this.durationPanel = new System.Windows.Forms.Panel();
             this.descriptionTextBox = new TogglDesktop.AutoCompleteTextBox();
             this.panelLeft.SuspendLayout();
+            this.durationPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBoxDuration
@@ -48,10 +50,9 @@
             this.textBoxDuration.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBoxDuration.Font = new System.Drawing.Font("Lucida Sans Unicode", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(186)));
             this.textBoxDuration.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(185)))), ((int)(((byte)(185)))), ((int)(((byte)(185)))));
-            this.textBoxDuration.Location = new System.Drawing.Point(255, 26);
-            this.textBoxDuration.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBoxDuration.Location = new System.Drawing.Point(4, 17);
             this.textBoxDuration.Name = "textBoxDuration";
-            this.textBoxDuration.Size = new System.Drawing.Size(120, 28);
+            this.textBoxDuration.Size = new System.Drawing.Size(80, 28);
             this.textBoxDuration.TabIndex = 2;
             this.textBoxDuration.Text = "00:00:00";
             this.textBoxDuration.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
@@ -68,10 +69,10 @@
             this.buttonStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonStart.Font = new System.Drawing.Font("Lucida Sans Unicode", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(186)));
             this.buttonStart.ForeColor = System.Drawing.Color.White;
-            this.buttonStart.Location = new System.Drawing.Point(389, 0);
+            this.buttonStart.Location = new System.Drawing.Point(259, 0);
             this.buttonStart.Margin = new System.Windows.Forms.Padding(0);
             this.buttonStart.Name = "buttonStart";
-            this.buttonStart.Size = new System.Drawing.Size(82, 86);
+            this.buttonStart.Size = new System.Drawing.Size(55, 56);
             this.buttonStart.TabIndex = 3;
             this.buttonStart.Text = "Start";
             this.buttonStart.UseVisualStyleBackColor = false;
@@ -88,10 +89,9 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.linkLabelProject.Font = new System.Drawing.Font("Lucida Sans Unicode", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(186)));
             this.linkLabelProject.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(185)))), ((int)(((byte)(185)))), ((int)(((byte)(185)))));
-            this.linkLabelProject.Location = new System.Drawing.Point(25, 54);
-            this.linkLabelProject.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.linkLabelProject.Location = new System.Drawing.Point(17, 35);
             this.linkLabelProject.Name = "linkLabelProject";
-            this.linkLabelProject.Size = new System.Drawing.Size(222, 31);
+            this.linkLabelProject.Size = new System.Drawing.Size(148, 20);
             this.linkLabelProject.TabIndex = 7;
             this.linkLabelProject.Text = "NEW - TOGGL";
             this.linkLabelProject.UseMnemonic = false;
@@ -105,10 +105,9 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.linkLabelDescription.Font = new System.Drawing.Font("Lucida Sans Unicode", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(186)));
             this.linkLabelDescription.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
-            this.linkLabelDescription.Location = new System.Drawing.Point(25, 0);
-            this.linkLabelDescription.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.linkLabelDescription.Location = new System.Drawing.Point(17, 0);
             this.linkLabelDescription.Name = "linkLabelDescription";
-            this.linkLabelDescription.Size = new System.Drawing.Size(222, 35);
+            this.linkLabelDescription.Size = new System.Drawing.Size(148, 23);
             this.linkLabelDescription.TabIndex = 8;
             this.linkLabelDescription.Text = "(no description)";
             this.linkLabelDescription.UseMnemonic = false;
@@ -120,10 +119,10 @@
             this.linkLabelDuration.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.linkLabelDuration.Font = new System.Drawing.Font("Lucida Sans Unicode", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(186)));
             this.linkLabelDuration.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(185)))), ((int)(((byte)(185)))), ((int)(((byte)(185)))));
-            this.linkLabelDuration.Location = new System.Drawing.Point(249, 49);
+            this.linkLabelDuration.Location = new System.Drawing.Point(2, 32);
             this.linkLabelDuration.Margin = new System.Windows.Forms.Padding(0);
             this.linkLabelDuration.Name = "linkLabelDuration";
-            this.linkLabelDuration.Size = new System.Drawing.Size(132, 35);
+            this.linkLabelDuration.Size = new System.Drawing.Size(88, 23);
             this.linkLabelDuration.TabIndex = 9;
             this.linkLabelDuration.Text = "00:00:00";
             this.linkLabelDuration.TextAlign = System.Drawing.ContentAlignment.TopRight;
@@ -138,10 +137,9 @@
             this.labelClearProject.Enabled = false;
             this.labelClearProject.Font = new System.Drawing.Font("Lucida Sans Unicode", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
             this.labelClearProject.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(185)))), ((int)(((byte)(185)))), ((int)(((byte)(185)))));
-            this.labelClearProject.Location = new System.Drawing.Point(6, 54);
-            this.labelClearProject.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelClearProject.Location = new System.Drawing.Point(4, 35);
             this.labelClearProject.Name = "labelClearProject";
-            this.labelClearProject.Size = new System.Drawing.Size(22, 31);
+            this.labelClearProject.Size = new System.Drawing.Size(15, 20);
             this.labelClearProject.TabIndex = 10;
             this.labelClearProject.Text = "x";
             this.labelClearProject.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -150,17 +148,28 @@
             // 
             // panelLeft
             // 
+            this.panelLeft.Controls.Add(this.durationPanel);
             this.panelLeft.Controls.Add(this.linkLabelDescription);
             this.panelLeft.Controls.Add(this.labelClearProject);
-            this.panelLeft.Controls.Add(this.textBoxDuration);
             this.panelLeft.Controls.Add(this.descriptionTextBox);
             this.panelLeft.Controls.Add(this.linkLabelProject);
-            this.panelLeft.Controls.Add(this.linkLabelDuration);
             this.panelLeft.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panelLeft.Location = new System.Drawing.Point(0, 0);
+            this.panelLeft.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panelLeft.Name = "panelLeft";
-            this.panelLeft.Size = new System.Drawing.Size(389, 86);
+            this.panelLeft.Size = new System.Drawing.Size(259, 56);
             this.panelLeft.TabIndex = 11;
+            // 
+            // durationPanel
+            // 
+            this.durationPanel.BackColor = System.Drawing.Color.Transparent;
+            this.durationPanel.Controls.Add(this.textBoxDuration);
+            this.durationPanel.Controls.Add(this.linkLabelDuration);
+            this.durationPanel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.durationPanel.Location = new System.Drawing.Point(166, 0);
+            this.durationPanel.Name = "durationPanel";
+            this.durationPanel.Size = new System.Drawing.Size(93, 56);
+            this.durationPanel.TabIndex = 11;
             // 
             // descriptionTextBox
             // 
@@ -172,10 +181,9 @@
             this.descriptionTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.descriptionTextBox.Font = new System.Drawing.Font("Lucida Sans Unicode", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(186)));
             this.descriptionTextBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(185)))), ((int)(((byte)(185)))), ((int)(((byte)(185)))));
-            this.descriptionTextBox.Location = new System.Drawing.Point(30, 31);
-            this.descriptionTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.descriptionTextBox.Location = new System.Drawing.Point(20, 20);
             this.descriptionTextBox.Name = "descriptionTextBox";
-            this.descriptionTextBox.Size = new System.Drawing.Size(217, 20);
+            this.descriptionTextBox.Size = new System.Drawing.Size(145, 20);
             this.descriptionTextBox.TabIndex = 1;
             this.descriptionTextBox.Text = "What are you doing?";
             this.descriptionTextBox.Enter += new System.EventHandler(this.descriptionTextBox_Enter);
@@ -185,16 +193,18 @@
             // 
             // TimerEditViewController
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(59)))), ((int)(((byte)(59)))));
             this.Controls.Add(this.panelLeft);
             this.Controls.Add(this.buttonStart);
             this.Margin = new System.Windows.Forms.Padding(0);
             this.Name = "TimerEditViewController";
-            this.Size = new System.Drawing.Size(471, 86);
+            this.Size = new System.Drawing.Size(314, 56);
             this.panelLeft.ResumeLayout(false);
             this.panelLeft.PerformLayout();
+            this.durationPanel.ResumeLayout(false);
+            this.durationPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -210,5 +220,6 @@
         private AutoCompleteTextBox descriptionTextBox;
         private System.Windows.Forms.Label labelClearProject;
         private System.Windows.Forms.Panel panelLeft;
+        private System.Windows.Forms.Panel durationPanel;
     }
 }
