a585cef83ccfdba4c99ade32debec096e74d2fff
IndrekV <indzuliin@gmail.com>
2014-10-06 16:36:28 +0300

    Revert "Using the WinSparkle.dll instead NuGet Package (win)"
    This reverts commit ff843b008c8ef3fe98668daab80fefe395ddf9a0.

--- src/ui/windows/TogglDesktop/TogglDesktop/AboutWindowController.cs
+++ src/ui/windows/TogglDesktop/TogglDesktop/AboutWindowController.cs
@@ -8,11 +8,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Diagnostics;
+using WinSparkleDotNet;
 
 namespace TogglDesktop
 {
     public partial class AboutWindowController : TogglForm
     {
+        private WinSparkleDotNet.WinSparkle sparkle = null;
 
         public AboutWindowController()
         {
@@ -23,21 +25,23 @@ namespace TogglDesktop
             bool updateCheckDisabled = Toggl.IsUpdateCheckDisabled();
             comboBoxChannel.Visible = !updateCheckDisabled;
             labelReleaseChannel.Visible = !updateCheckDisabled;
+
+            sparkle = new WinSparkleDotNet.WinSparkle();
         }
 
         private void AboutWindowController_FormClosing(object sender, FormClosingEventArgs e)
         {
             Hide();
             e.Cancel = true;
-            WinSparkle.win_sparkle_cleanup();
+            sparkle.Cleanup();
         }
 
         private void comboBoxChannel_SelectedIndexChanged(object sender, EventArgs e)
         {
             Toggl.SetUpdateChannel(comboBoxChannel.Text);
             String url = "https://assets.toggl.com/installers/windows_" + comboBoxChannel.Text + "_appcast.xml";
-            WinSparkle.win_sparkle_set_appcast_url(url);
-            WinSparkle.win_sparkle_check_update_with_ui();
+            sparkle.SetAppCastUrl(url);
+            sparkle.CheckUpdateWithUi();
         }
 
         private void linkLabelGithub_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
@@ -60,17 +64,16 @@ namespace TogglDesktop
             string channel = Toggl.UpdateChannel();
             comboBoxChannel.SelectedIndex = comboBoxChannel.Items.IndexOf(channel);
             String url = "https://assets.toggl.com/installers/windows_" + channel + "_appcast.xml";
-            WinSparkle.win_sparkle_set_appcast_url(url);
-            WinSparkle.win_sparkle_init();
+            sparkle.SetAppCastUrl(url);
             if (!Toggl.IsUpdateCheckDisabled())
             {
-                WinSparkle.win_sparkle_check_update_without_ui();
+                sparkle.CheckUpdateWithoutUi();
             }
         }
 
         internal void SparkleCleanUp()
         {
-            WinSparkle.win_sparkle_cleanup();
+            sparkle.Cleanup();
         }
     }
 }
