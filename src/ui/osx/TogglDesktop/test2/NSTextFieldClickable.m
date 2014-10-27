e975c63d8ddcc189f33451503e4c827c6a0048a8
IndrekV <indzuliin@gmail.com>
2014-10-27 16:01:44 +0200

    Fixed cursor color in timer duration field (osx)

--- src/ui/osx/TogglDesktop/test2/NSTextFieldClickable.m
+++ src/ui/osx/TogglDesktop/test2/NSTextFieldClickable.m
@@ -22,4 +22,19 @@
 	[self sendAction:@selector(textFieldClicked:) to:[self delegate]];
 }
 
+- (BOOL)becomeFirstResponder
+{
+    BOOL success = [super becomeFirstResponder];
+
+    if (success && self.isEditable)
+    {
+        NSTextView *textField = (NSTextView *)[self currentEditor];
+        if ([textField respondsToSelector:@selector(setInsertionPointColor:)])
+        {
+            [textField setInsertionPointColor:[NSColor whiteColor]];
+        }
+    }
+    return success;
+}
+
 @end
