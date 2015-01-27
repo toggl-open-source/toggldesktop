576cc27eb7be77963aa67bcd8b923840716bb985
Tanel Lebedev <tanel.lebedev@gmail.com>
2015-01-27 20:48:50 +0200

    Treat 'SSL context exception' as user error (lib)

--- src/error.cc
+++ src/error.cc
@@ -104,6 +104,9 @@ bool IsUserError(const error err) {
     if (value.find(kPaymentRequiredError) != std::string::npos) {
         return true;
     }
+    if (value.find("SSL context exception") != std::string::npos) {
+        return true;
+    }
     if (value.find("Access to file denied") != std::string::npos) {
         return true;
     }
