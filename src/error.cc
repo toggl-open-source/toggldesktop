120b2a3b1ed0fce981f3e2412756be6496d28fdc
Tanel Lebedev <tanel.lebedev@gmail.com>
2015-01-27 03:51:17 +0200

    Treat 'cannot access workspace' as user error

--- src/error.cc
+++ src/error.cc
@@ -158,6 +158,9 @@ bool IsUserError(const error err) {
     if (value.find("Invalid e-mail") != std::string::npos) {
         return true;
     }
+    if (value.find("cannot access workspace") != std::string::npos) {
+        return true;
+    }
     return false;
 }
 
