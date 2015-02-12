7763de47dc5bc1ce6bf2964a3c12498d9aaf389a
Indrek V <indzuliin@gmail.com>
2015-02-12 07:09:05 -0800

    Handle not loaded entrie list (win)

--- src/ui/windows/TogglDesktop/TogglDesktop/MainWindowController.cs
+++ src/ui/windows/TogglDesktop/TogglDesktop/MainWindowController.cs
@@ -237,9 +237,12 @@ namespace TogglDesktop
             Utils.LoadWindowLocation(this, editForm);
             timeEntryListViewController.setEditPopup(editForm);
 
-            timeEntryListViewController.getListing().Scroll += MainWindowControllerEntries_Scroll;
-            timeEntryListViewController.getListing().MouseWheel += MainWindowControllerEntries_Scroll;
-
+            FlowLayoutPanel listing = timeEntryListViewController.getListing();
+            if (listing != null)
+            {
+                listing.Scroll += MainWindowControllerEntries_Scroll;
+                listing.MouseWheel += MainWindowControllerEntries_Scroll;
+            }
             if (!Toggl.Start(TogglDesktop.Program.Version()))
             {
                 try
