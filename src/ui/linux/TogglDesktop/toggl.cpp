9c29d86b8c832185dbc2242a7847f92f00ad7725
Tanel Lebedev <tanel.lebedev@gmail.com>
2015-03-05 00:01:12 +0200

    enable update check without download (linux)

--- src/ui/linux/TogglDesktop/toggl.cpp
+++ src/ui/linux/TogglDesktop/toggl.cpp
@@ -165,9 +165,6 @@ TogglApi::TogglApi(
     ctx = toggl_context_init("linux_native_app",
                              version.toStdString().c_str());
 
-    // FIXME: enable
-    toggl_disable_update_check(ctx);
-
     QString appDirPath =
         QStandardPaths::writableLocation(
             QStandardPaths::DataLocation);
