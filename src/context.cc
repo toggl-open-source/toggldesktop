a7d021e96ec47db7842bc8987cf87baf5176bac0
Tanel Lebedev <tanel.lebedev@gmail.com>
2015-04-02 16:17:51 -0700

    Dont download updates unless in production (lib)

--- src/context.cc
+++ src/context.cc
@@ -864,6 +864,12 @@ error Context::downloadUpdate() {
             return noError;
         }
 
+		// To test updater in development, comment this block out:
+		if ("production" != environment_) {
+			logger().debug("Not in production, will not download updates");
+			return noError;
+		}
+
         // Load current update channel
         std::string update_channel("");
         error err = db()->LoadUpdateChannel(&update_channel);
