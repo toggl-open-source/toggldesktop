7e39fa27d9bf861184022d8568a62341029f5557
amulware <amul@amulware.net>
2015-06-24 14:52:44 +0200

    fixed small naming inconsistency (win)

--- src/ui/windows/TogglDesktop/TogglDesktop/AutoCompletion/Implementation/ModelItem.cs
+++ src/ui/windows/TogglDesktop/TogglDesktop/AutoCompletion/Implementation/ModelItem.cs
@@ -4,16 +4,16 @@ using TogglDesktop.WPF.AutoComplete;
 
 namespace TogglDesktop.AutoCompletion.Implementation
 {
-    class ModelItem : SimpleItem<ClientEntry, Toggl.Model>
+    class ModelItem : SimpleItem<ModelEntry, Toggl.Model>
     {
         public ModelItem(Toggl.Model model)
             : base(model, model.Name)
         {
         }
 
-        protected override ClientEntry createElement(Action selectWithClick)
+        protected override ModelEntry createElement(Action selectWithClick)
         {
-            return new ClientEntry(this.Item, selectWithClick);
+            return new ModelEntry(this.Item, selectWithClick);
         }
     }
 }
---
+++ src/ui/windows/TogglDesktop/TogglDesktop/WPF/AutoComplete/ModelEntry.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace TogglDesktop.WPF.AutoComplete
+{
+    sealed class ModelEntrySample
+    {
+        public string ModelName { get { return "Big money company"; } }
+
+        public Color BackgroundColor { get { return Color.FromRgb(255, 255, 255); } }
+    }
+
+    partial class ModelEntry
+    {
+        public ModelEntry(Toggl.Model item, Action selectWithClick)
+            : base(selectWithClick)
+        {
+            this.ModelName = item.Name;
+            InitializeComponent();
+        }
+
+        #region dependency properties
+
+        public static readonly DependencyProperty ModelNameProperty = DependencyProperty
+            .Register("ModelName", typeof(string), typeof(ModelEntry));
+
+        public string ModelName
+        {
+            get { return (string)this.GetValue(ModelNameProperty); }
+            set { this.SetValue(ModelNameProperty, value); }
+        }
+        #endregion
+    }
+}
