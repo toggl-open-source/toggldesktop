d18718357fe72d9c39e1c7d1619dbde6d83d74ad
Tanel Lebedev <tanel.lebedev@gmail.com>
2015-07-14 05:24:45 -0700

    fix client name rendering issue (lib)

--- src/project.cc
+++ src/project.cc
@@ -28,11 +28,12 @@ std::string Project::String() const {
     std::stringstream ss;
     ss  << "ID=" << ID()
         << " local_id=" << LocalID()
-        << " name=" << name_
+		<< " guid=" << GUID()
+		<< " name=" << name_
         << " wid=" << wid_
         << " cid=" << cid_
-        << " guid=" << GUID()
-        << " active=" << active_
+		<< " client_guid=" << client_guid_
+		<< " active=" << active_
         << " public=" << private_
         << " color=" << color_
         << " billable=" << billable_
