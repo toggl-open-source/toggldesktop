d2ea8517397eaf5f965d8ba42d73198d413c1e25
amulware <amul@amulware.net>
2015-07-14 14:48:15 +0200

    added shadows to auto completion popups (win)

---
+++ src/ui/windows/TogglDesktop/TogglDesktop/WPF/AddValueConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace TogglDesktop.WPF
+{
+    class AddValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (double)value + System.Convert.ToDouble(parameter);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
