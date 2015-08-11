48f21677efb14336f9546a7192ca5f38d511bfb7
amulware <amul@amulware.net>
2015-08-11 15:32:05 +0300

    Revert "added more debug output to struct-size check (lib)"
    This reverts commit e1bb71e3228a95b8da9440ce7a3b3f13c8d714d5.

--- src/toggl_api.cc
+++ src/toggl_api.cc
@@ -993,38 +993,32 @@ bool_t toggl_check_view_struct_size(
     const int view_item_size,
     const int settings_size,
     const int autotracker_view_item_size) {
-	bool ret = true;
     int size = sizeof(TogglTimeEntryView);
     if (time_entry_view_item_size != size) {
-        std::cerr << "Invalid time entry view item struct size, expected "
-			<< size << ", is " << time_entry_view_item_size << std::endl;
-        ret = false;
+        std::cerr << "Invalid time entry view item struct size" << std::endl;
+        return false;
     }
     size = sizeof(TogglAutocompleteView);
     if (autocomplete_view_item_size != size) {
-        std::cerr << "Invalid autocomplete view item struct size, expected "
-			<< size << ", is " << autocomplete_view_item_size << std::endl;
-		ret = false;
+        std::cerr << "Invalid autocomplete view item struct size" << std::endl;
+        return false;
     }
     size = sizeof(TogglGenericView);
     if (view_item_size != size) {
-        std::cerr << "Invalid view item struct size, expected "
-			<< size << ", is " << view_item_size << std::endl;
-		ret = false;
+        std::cerr << "Invalid view item struct size" << std::endl;
+        return false;
     }
     size = sizeof(TogglSettingsView);
     if (settings_size != size) {
-        std::cerr << "Invalid settings view item struct size, expected "
-			<< size << ", is " << settings_size << std::endl;
-		ret = false;
+        std::cerr << "Invalid settings view item struct size" << std::endl;
+        return false;
     }
     size = sizeof(TogglAutocompleteView);
     if (autocomplete_view_item_size != size) {
-        std::cerr << "Invalid autocomplete view item struct size, expected "
-			<< size << ", is " << autocomplete_view_item_size << std::endl;
-		ret = false;
+        std::cerr << "Invalid autocomplete view item struct size" << std::endl;
+        return false;
     }
-    return ret;
+    return true;
 }
 
 void toggl_set_idle_seconds(
