5a01b15b6dd333a9b26e245369bc0042847d5cf9
Tanel Lebedev <tanel.lebedev@gmail.com>
2015-08-12 05:36:52 -0700

    Start with unit tests (win)

---
+++ src/ui/windows/TogglDesktop/TogglDesktopDLLInteropTest/LibTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TogglDesktopDLLInteropTest
+{
+    [TestClass]
+    public class LibTest
+    {
+        [TestMethod]
+        public void TestStartUI()
+        {
+            Assert.IsTrue(TogglDesktop.Toggl.StartUI("0.0.0"));
+        }
+    }
+}
