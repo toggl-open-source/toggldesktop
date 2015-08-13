4b6a7f1a48ca126a1a45145b35bd07688330fc8b
Tanel Lebedev <tanel.lebedev@gmail.com>
2015-08-13 11:36:20 -0700

    Testing stuff (lib)

--- src/toggl_api.cc
+++ src/toggl_api.cc
@@ -33,13 +33,7 @@
 void *toggl_context_init(
     const char_t *app_name,
     const char_t *app_version) {
-    poco_check_ptr(app_name);
-    poco_check_ptr(app_version);
-
-    toggl::Context *ctx =
-        new toggl::Context(to_string(app_name), to_string(app_version));
-
-    return ctx;
+    return new toggl::Context(to_string(app_name), to_string(app_version));
 }
 
 bool_t toggl_ui_start(void *context) {
@@ -82,10 +76,6 @@ bool_t toggl_set_settings_remind_times(
     void *context,
     const char_t *remind_starts,
     const char_t *remind_ends) {
-
-    poco_check_ptr(remind_starts);
-    poco_check_ptr(remind_ends);
-
     return toggl::noError == app(context)->SetSettingsRemindTimes(
         to_string(remind_starts),
         to_string(remind_ends));
@@ -197,8 +187,6 @@ bool_t toggl_window_settings(
     int64_t *window_height,
     int64_t *window_width) {
 
-    poco_check_ptr(context);
-
     poco_check_ptr(window_x);
     poco_check_ptr(window_y);
     poco_check_ptr(window_height);
@@ -217,10 +205,6 @@ bool_t toggl_set_proxy_settings(void *context,
                                 const uint64_t proxy_port,
                                 const char_t *proxy_username,
                                 const char_t *proxy_password) {
-    poco_check_ptr(proxy_host);
-    poco_check_ptr(proxy_username);
-    poco_check_ptr(proxy_password);
-
     toggl::Proxy proxy;
     proxy.SetHost(to_string(proxy_host));
     proxy.SetPort(proxy_port);
@@ -233,7 +217,6 @@ bool_t toggl_set_proxy_settings(void *context,
 void toggl_set_cacert_path(
     void *context,
     const char_t *path) {
-    poco_check_ptr(path);
 
     toggl::HTTPSClient::Config.CACertPath = to_string(path);
 }
@@ -241,16 +224,12 @@ void toggl_set_cacert_path(
 bool_t toggl_set_db_path(
     void *context,
     const char_t *path) {
-    poco_check_ptr(path);
-
     return toggl::noError == app(context)->SetDBPath(to_string(path));
 }
 
 void toggl_set_update_path(
     void *context,
     const char_t *path) {
-    poco_check_ptr(path);
-
     return app(context)->SetUpdatePath(to_string(path));
 }
 
@@ -259,12 +238,9 @@ char_t *toggl_update_path(
     return copy_string(app(context)->UpdatePath());
 }
 
-
 void toggl_set_environment(
     void *context,
     const char_t *environment) {
-    poco_check_ptr(environment);
-
     return app(context)->SetEnvironment(to_string(environment));
 }
 
@@ -275,19 +251,14 @@ char_t *toggl_environment(
 
 void toggl_disable_update_check(
     void *context) {
-
     app(context)->DisableUpdateCheck();
 }
 
 void toggl_set_log_path(const char_t *path) {
-    poco_check_ptr(path);
-
     toggl::Context::SetLogPath(to_string(path));
 }
 
 void toggl_set_log_level(const char_t *level) {
-    poco_check_ptr(level);
-
     Poco::Logger::get("").setLevel(to_string(level));
 }
 
@@ -300,10 +271,6 @@ bool_t toggl_login(
     void *context,
     const char_t *email,
     const char_t *password) {
-
-    poco_check_ptr(email);
-    poco_check_ptr(password);
-
     return toggl::noError == app(context)->Login(to_string(email),
             to_string(password));
 }
@@ -312,10 +279,6 @@ bool_t toggl_signup(
     void *context,
     const char_t *email,
     const char_t *password) {
-
-    poco_check_ptr(email);
-    poco_check_ptr(password);
-
     return toggl::noError == app(context)->Signup(to_string(email),
             to_string(password));
 }
@@ -323,9 +286,6 @@ bool_t toggl_signup(
 bool_t toggl_google_login(
     void *context,
     const char_t *access_token) {
-
-    poco_check_ptr(access_token);
-
     return toggl::noError == app(context)->GoogleLogin(to_string(access_token));
 }
 
@@ -400,8 +360,6 @@ char_t *toggl_add_project(
     const char_t *client_guid,
     const char_t *project_name,
     const bool_t is_private) {
-
-    poco_check_ptr(time_entry_guid);
 
     toggl::Project *p = app(context)->CreateProject(
         workspace_id,
@@ -492,8 +450,6 @@ bool_t toggl_continue(
     void *context,
     const char_t *guid) {
 
-    poco_check_ptr(guid);
-
     std::stringstream ss;
     ss << "toggl_continue guid=" << guid;
     logger().debug(ss.str());
@@ -510,9 +466,6 @@ void toggl_edit(
     const char_t *guid,
     const bool_t edit_running_entry,
     const char_t *focused_field_name) {
-
-    poco_check_ptr(guid);
-    poco_check_ptr(focused_field_name);
 
     std::stringstream ss;
     ss << "toggl_edit guid=" << guid
@@ -542,8 +495,6 @@ bool_t toggl_delete_time_entry(
     void *context,
     const char_t *guid) {
 
-    poco_check_ptr(guid);
-
     std::stringstream ss;
     ss << "toggl_delete_time_entry guid=" << guid;
     logger().debug(ss.str());
@@ -556,9 +507,6 @@ bool_t toggl_set_time_entry_duration(
     void *context,
     const char_t *guid,
     const char_t *value) {
-
-    poco_check_ptr(guid);
-    poco_check_ptr(value);
 
     std::stringstream ss;
     ss  << "toggl_set_time_entry_duration guid=" << guid
@@ -576,30 +524,17 @@ bool_t toggl_set_time_entry_project(
     const uint64_t task_id,
     const uint64_t project_id,
     const char_t *project_guid) {
-
-    poco_check_ptr(guid);
-    std::string pguid("");
-    if (project_guid) {
-        pguid = to_string(project_guid);
-    }
-    return toggl::noError == app(context)->SetTimeEntryProject(to_string(guid),
+    return toggl::noError == app(context)->SetTimeEntryProject(
+		to_string(guid),
             task_id,
             project_id,
-            pguid);
+			to_string(project_guid));
 }
 
 bool_t toggl_set_time_entry_date(
     void *context,
     const char_t *guid,
     const int64_t unix_timestamp) {
-
-    poco_check_ptr(guid);
-
-    std::stringstream ss;
-    ss  << "toggl_set_time_entry_date guid=" << guid
-        << ", unix_timestamp=" << unix_timestamp;
-    logger().debug(ss.str());
-
     return toggl::noError == app(context)->
            SetTimeEntryDate(to_string(guid), unix_timestamp);
 }
@@ -608,14 +543,6 @@ bool_t toggl_set_time_entry_start(
     void *context,
     const char_t *guid,
     const char_t *value) {
-    poco_check_ptr(guid);
-    poco_check_ptr(value);
-
-    std::stringstream ss;
-    ss  << "toggl_set_time_entry_start guid=" << guid
-        << ", value=" << value;
-    logger().debug(ss.str());
-
     return toggl::noError == app(context)->
            SetTimeEntryStart(to_string(guid), to_string(value));
 }
@@ -624,14 +551,6 @@ bool_t toggl_set_time_entry_end(
     void *context,
     const char_t *guid,
     const char_t *value) {
-    poco_check_ptr(guid);
-    poco_check_ptr(value);
-
-    std::stringstream ss;
-    ss  << "toggl_set_time_entry_end guid=" << guid
-        << ", value=" << value;
-    logger().debug(ss.str());
-
     return toggl::noError == app(context)->
            SetTimeEntryStop(to_string(guid), to_string(value));
 }
@@ -640,15 +559,6 @@ bool_t toggl_set_time_entry_tags(
     void *context,
     const char_t *guid,
     const char_t *value) {
-
-    poco_check_ptr(guid);
-    poco_check_ptr(value);
-
-    std::stringstream ss;
-    ss  << "toggl_set_time_entry_tags guid=" << guid
-        << ", value=" << value;
-    logger().debug(ss.str());
-
     return toggl::noError == app(context)->SetTimeEntryTags(to_string(guid),
             to_string(value));
 }
@@ -657,14 +567,6 @@ bool_t toggl_set_time_entry_billable(
     void *context,
     const char_t *guid,
     const bool_t value) {
-
-    poco_check_ptr(guid);
-
-    std::stringstream ss;
-    ss  << "toggl_set_time_entry_billable guid=" << guid
-        << ", value=" << value;
-    logger().debug(ss.str());
-
     return toggl::noError == app(context)->
            SetTimeEntryBillable(to_string(guid), value);
 }
@@ -673,22 +575,12 @@ bool_t toggl_set_time_entry_description(
     void *context,
     const char_t *guid,
     const char_t *value) {
-    poco_check_ptr(guid);
-    poco_check_ptr(value);
-
-    std::stringstream ss;
-    ss  << "toggl_set_time_entry_description guid=" << guid
-        << ", value=" << value;
-    logger().debug(ss.str());
-
     return toggl::noError == app(context)->
            SetTimeEntryDescription(to_string(guid), to_string(value));
 }
 
 bool_t toggl_stop(
     void *context) {
-    logger().debug("toggl_stop");
-
     return toggl::noError == app(context)->Stop();
 }
 
@@ -721,17 +613,10 @@ bool_t toggl_discard_time_and_continue(
     const char_t *guid,
     const uint64_t at) {
 
-    if (!guid) {
-        logger().error("Cannot discard time without GUID");
-        return false;
-    }
-
     if (!at) {
         logger().error("Cannot discard time without a timestamp");
         return false;
     }
-
-    logger().debug("toggl_discard_time_and_continue");
 
     return toggl::noError == app(context)->DiscardTimeAndContinue(
         to_string(guid),
@@ -741,7 +626,6 @@ bool_t toggl_discard_time_and_continue(
 bool_t toggl_timeline_toggle_recording(
     void *context,
     const bool_t record_timeline) {
-    logger().debug("toggl_timeline_toggle_recording");
     return toggl::noError == app(context)->
            ToggleTimelineRecording(record_timeline);
 }
@@ -756,11 +640,8 @@ bool_t toggl_feedback_send(
     const char_t *topic,
     const char_t *details,
     const char_t *filename) {
-    std::stringstream ss;
-    ss << "toggl_feedback_send topic=" << topic << " details=" << details;
-    logger().debug(ss.str());
 
-    toggl::Feedback feedback;
+	toggl::Feedback feedback;
     feedback.SetSubject(to_string(topic));
     feedback.SetDetails(to_string(details));
     feedback.SetAttachmentPath(to_string(filename));
@@ -771,16 +652,12 @@ bool_t toggl_feedback_send(
 bool_t toggl_set_update_channel(
     void *context,
     const char_t *update_channel) {
-
-    poco_check_ptr(update_channel);
-
     return toggl::noError == app(context)->
            SetUpdateChannel(to_string(update_channel));
 }
 
 char_t *toggl_get_update_channel(
     void *context) {
-
     std::string update_channel("");
     app(context)->UpdateChannel(&update_channel);
     return copy_string(update_channel);
@@ -788,16 +665,12 @@ char_t *toggl_get_update_channel(
 
 char_t *toggl_get_user_fullname(
     void *context) {
-
-    std::string fullname = app(context)->UserFullName();
-    return copy_string(fullname);
+	return copy_string(app(context)->UserFullName());
 }
 
 char_t *toggl_get_user_email(
     void *context) {
-
-    std::string email = app(context)->UserEmail();
-    return copy_string(email);
+	return copy_string(app(context)->UserEmail());
 }
 
 int64_t toggl_parse_duration_string_into_seconds(
@@ -811,70 +684,60 @@ int64_t toggl_parse_duration_string_into_seconds(
 void toggl_on_show_app(
     void *context,
     TogglDisplayApp cb) {
-
     app(context)->UI()->OnDisplayApp(cb);
 }
 
 void toggl_on_sync_state(
     void *context,
     TogglDisplaySyncState cb) {
-
     app(context)->UI()->OnDisplaySyncState(cb);
 }
 
 void toggl_on_unsynced_items(
     void *context,
     TogglDisplayUnsyncedItems cb) {
-
     app(context)->UI()->OnDisplayUnsyncedItems(cb);
 }
 
 void toggl_on_error(
     void *context,
     TogglDisplayError cb) {
-
     app(context)->UI()->OnDisplayError(cb);
 }
 
 void toggl_on_online_state(
     void *context,
     TogglDisplayOnlineState cb) {
-
     app(context)->UI()->OnDisplayOnlineState(cb);
 }
 
 void toggl_on_update(
     void *context,
     TogglDisplayUpdate cb) {
-
     app(context)->UI()->OnDisplayUpdate(cb);
 }
 
 void toggl_on_url(
     void *context,
     TogglDisplayURL cb) {
-
     app(context)->UI()->OnDisplayURL(cb);
 }
 
 void toggl_on_login(
     void *context,
     TogglDisplayLogin cb) {
-
     app(context)->UI()->OnDisplayLogin(cb);
 }
 
 void toggl_on_reminder(
     void *context,
     TogglDisplayReminder cb) {
-
     app(context)->UI()->OnDisplayReminder(cb);
 }
 
 void toggl_on_autotracker_notification(
     void *context,
     TogglDisplayAutotrackerNotification cb) {
-
     app(context)->UI()->OnDisplayAutotrackerNotification(cb);
 }
 
@@ -1095,9 +958,6 @@ bool_t toggl_autotracker_add_rule(
     void *context,
     const char_t *term,
     const uint64_t project_id) {
-
-    poco_check_ptr(term);
-
     return toggl::noError == app(context)->
            AddAutotrackerRule(to_string(term), project_id);
 }
@@ -1105,7 +965,6 @@ bool_t toggl_autotracker_add_rule(
 bool_t toggl_autotracker_delete_rule(
     void *context,
     const int64_t id) {
-
     return toggl::noError == app(context)->DeleteAutotrackerRule(id);
 }
 
@@ -1117,8 +976,6 @@ void testing_sleep(
 bool_t testing_set_logged_in_user(
     void *context,
     const char *json) {
-    poco_check_ptr(json);
-
     toggl::Context *ctx = reinterpret_cast<toggl::Context *>(context);
     return toggl::noError == ctx->SetLoggedInUserFromJSON(std::string(json));
 }
