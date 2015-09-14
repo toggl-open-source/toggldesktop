be04af183b591e611bf6ac2938b69cf01621e658
amulware <amul@amulware.net>
2015-09-14 12:46:37 +0200

    removed winforms remnants (win)

--- src/ui/windows/TogglDesktop/TogglDesktop/UIExtensions.cs
+++ src/ui/windows/TogglDesktop/TogglDesktop/UIExtensions.cs
@@ -9,46 +9,6 @@ namespace TogglDesktop
 {
 static class UIExtensions
 {
-    public static bool TryBeginInvoke(this System.Windows.Forms.Control control, Action action)
-    {
-        if (!control.InvokeRequired)
-            return false;
-
-        control.BeginInvoke(action);
-
-        return true;
-    }
-
-    #region overloads
-
-    public static bool TryBeginInvoke<T0>(this System.Windows.Forms.Control control,
-                                          Action<T0> action, T0 p0)
-    {
-        return control.TryBeginInvoke(() => action(p0));
-    }
-    public static bool TryBeginInvoke<T0, T1>(this System.Windows.Forms.Control control,
-            Action<T0, T1> action, T0 p0, T1 p1)
-    {
-        return control.TryBeginInvoke(() => action(p0, p1));
-    }
-    public static bool TryBeginInvoke<T0, T1, T2>(this System.Windows.Forms.Control control,
-            Action<T0, T1, T2> action, T0 p0, T1 p1, T2 p2)
-    {
-        return control.TryBeginInvoke(() => action(p0, p1, p2));
-    }
-    public static bool TryBeginInvoke<T0, T1, T2, T3>(this System.Windows.Forms.Control control,
-            Action<T0, T1, T2, T3> action, T0 p0, T1 p1, T2 p2, T3 p3)
-    {
-        return control.TryBeginInvoke(() => action(p0, p1, p2, p3));
-    }
-    public static bool TryBeginInvoke<T0, T1, T2, T3, T4>(this System.Windows.Forms.Control control,
-            Action<T0, T1, T2, T3, T4> action, T0 p0, T1 p1, T2 p2, T3 p3, T4 p4)
-    {
-        return control.TryBeginInvoke(() => action(p0, p1, p2, p3, p4));
-    }
-
-    #endregion
-
     public static bool TryBeginInvoke(this Control control, Action action)
     {
         if (control.Dispatcher.CheckAccess())
