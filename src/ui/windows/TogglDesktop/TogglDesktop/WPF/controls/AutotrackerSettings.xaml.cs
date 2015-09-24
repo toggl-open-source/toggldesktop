c1fe0f5cb23b44f9514524e47734bf1492754298
amulware <amul@amulware.net>
2015-09-24 13:25:54 +0200

    added basic non-functional auto tracker ui (win)

---
+++ src/ui/windows/TogglDesktop/TogglDesktop/WPF/controls/AutotrackerSettings.xaml.cs
@@ -0,0 +1,64 @@
+﻿
+using System;
+using System.Windows;
+using System.Windows.Input;
+using TogglDesktop.AutoCompletion;
+
+namespace TogglDesktop.WPF
+{
+    public partial class AutotrackerSettings
+    {
+        public AutotrackerSettings()
+        {
+            this.InitializeComponent();
+        }
+
+        private void termTextBox_OnLostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+        }
+
+        private void termDropDownButton_OnClick(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void termAutoComplete_OnConfirmCompletion(object sender, AutoCompleteItem e)
+        {
+        }
+
+        private void termAutoComplete_OnConfirmWithoutCompletion(object sender, string e)
+        {
+        }
+
+        private void termAutoComplete_OnIsOpenChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void projectTextBox_OnLostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+        }
+
+        private void projectTextBox_OnPreviewKeyDown(object sender, KeyEventArgs e)
+        {
+        }
+
+        private void projectDropDownButton_OnClick(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void projectAutoComplete_OnConfirmCompletion(object sender, AutoCompleteItem e)
+        {
+        }
+
+        private void projectAutoComplete_OnConfirmWithoutCompletion(object sender, string e)
+        {
+        }
+
+        private void projectAutoComplete_OnIsOpenChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void onAddButtonClick(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
