7ce0ae89a7f29a49f94016a509e3789aaee79830
amulware <amul@amulware.net>
2015-09-30 10:32:42 +0200

    made auto tracker ui functional (win)

--- src/ui/windows/TogglDesktop/TogglDesktop/WPF/controls/AutotrackerSettings.xaml.cs
+++ src/ui/windows/TogglDesktop/TogglDesktop/WPF/controls/AutotrackerSettings.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Media;
 using TogglDesktop.AutoCompletion;
 using TogglDesktop.AutoCompletion.Implementation;
 
@@ -11,12 +12,14 @@ namespace TogglDesktop.WPF
     public partial class AutotrackerSettings
     {
         private readonly List<AutotrackerRuleItem> ruleItems = new List<AutotrackerRuleItem>();
+        private Toggl.TogglAutocompleteView selectedProject;
 
         public AutotrackerSettings()
         {
             this.InitializeComponent();
 
             Toggl.OnAutotrackerRules += this.onAutotrackerRules;
+            Toggl.OnProjectAutocomplete += this.onProjectAutocomplete;
         }
 
         #region toggl events
@@ -27,6 +30,11 @@ namespace TogglDesktop.WPF
                 return;
 
             this.fill(rules, terms);
+        }
+
+        private void onProjectAutocomplete(List<Toggl.TogglAutocompleteView> list)
+        {
+            this.projectAutoComplete.SetController(AutoCompleteControllers.ForProjects(list));
         }
 
         #endregion
@@ -42,7 +50,6 @@ namespace TogglDesktop.WPF
 
         private void fillRules(List<Toggl.TogglAutotrackerRuleView> rules)
         {
-            return;
             this.rulesPanel.Children.Clear();
 
             foreach (var item in this.ruleItems)
@@ -80,35 +87,70 @@ namespace TogglDesktop.WPF
                     this.termTextBox.CaretIndex = this.termTextBox.Text.Length;
                 }
             }
-
         }
 
+        private void termAutoComplete_OnIsOpenChanged(object sender, EventArgs e)
+        {
+            this.termDropDownButton.IsChecked = this.termAutoComplete.IsOpen;
+        }
         #endregion
 
         #region project auto completion
 
         private void projectTextBox_OnLostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
         {
-        }
-
-        private void projectTextBox_OnPreviewKeyDown(object sender, KeyEventArgs e)
-        {
+            if (!string.IsNullOrEmpty(this.selectedProject.ProjectLabel))
+            {
+                this.selectProject(this.selectedProject);
+            }
         }
 
         private void projectDropDownButton_OnClick(object sender, RoutedEventArgs e)
         {
+            var open = this.projectDropDownButton.IsChecked ?? false;
+            if (open)
+            {
+                this.projectAutoComplete.OpenAndShowAll();
+            }
+            else
+            {
+                this.projectAutoComplete.IsOpen = false;
+                if (!this.projectTextBox.IsKeyboardFocused)
+                {
+                    this.projectTextBox.Focus();
+                    this.projectTextBox.CaretIndex = this.projectTextBox.Text.Length;
+                }
+            }
         }
 
         private void projectAutoComplete_OnConfirmCompletion(object sender, AutoCompleteItem e)
         {
+            var asProjectItem = e as ProjectItem;
+            if (asProjectItem == null)
+                return;
+
+            var item = asProjectItem.Item;
+
+            this.selectProject(item);
         }
 
         private void projectAutoComplete_OnConfirmWithoutCompletion(object sender, string e)
         {
+            this.selectProject(null);
         }
 
         private void projectAutoComplete_OnIsOpenChanged(object sender, EventArgs e)
         {
+            this.projectDropDownButton.IsChecked = this.projectAutoComplete.IsOpen;
+        }
+
+        private void selectProject(Toggl.TogglAutocompleteView? item)
+        {
+            var project = item ?? default(Toggl.TogglAutocompleteView);
+            this.selectedProject = project;
+            this.projectTextBox.SetText(project.ProjectLabel);
+            this.projectColorCircle.Background = new SolidColorBrush(getProjectColor(project.ProjectColor));
+            this.projectTextBox.CaretIndex = this.projectTextBox.Text.Length;
         }
 
         #endregion
@@ -117,14 +159,27 @@ namespace TogglDesktop.WPF
 
         private void onAddButtonClick(object sender, RoutedEventArgs e)
         {
-            if (Toggl.AddAutotrackerRule(this.termTextBox.Text, 0))
+            if (Toggl.AddAutotrackerRule(this.termTextBox.Text, this.selectedProject.ProjectID))
             {
-                this.termTextBox.Clear();
-                this.projectTextBox.Clear();
+                this.reset();
             }
         }
 
         #endregion
+
+        private void reset()
+        {
+            this.termTextBox.Clear();
+            this.projectTextBox.Clear();
+            this.selectProject(null);
+        }
+
+        private static Color getProjectColor(string colorString)
+        {
+            var projectColourString = string.IsNullOrEmpty(colorString) ? "#999999" : colorString;
+            var projectColor = (Color)(ColorConverter.ConvertFromString(projectColourString) ?? Color.FromRgb(153, 153, 153));
+            return projectColor;
+        }
 
     }
 }
