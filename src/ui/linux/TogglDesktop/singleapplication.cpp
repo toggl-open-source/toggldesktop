5ea410586bccb83e2366c7c0404cbecce66261b0
Tanel Lebedev <tanel.lebedev@gmail.com>
2015-10-20 12:45:14 +0200

    replace singleton app (linux)

---
+++ src/ui/linux/TogglDesktop/singleapplication.cpp
@@ -0,0 +1,64 @@
+#include "SingleApplication.h"
+#include <QtNetwork/QLocalSocket>
+#include <QFileInfo>
+
+#define TIME_OUT                (500)    // 500ms
+
+SingleApplication::SingleApplication(int &argc, char **argv)
+    : QApplication(argc, argv)
+, w(nullptr)
+, is_running_(false)
+, local_server_(nullptr) {
+
+    server_name_ = QFileInfo(QCoreApplication::applicationFilePath()).fileName();
+
+    initLocalConnection();
+}
+
+bool SingleApplication::isRunning() {
+    return is_running_;
+}
+
+void SingleApplication::newLocalConnection() {
+    QLocalSocket *socket = local_server_->nextPendingConnection();
+    if(socket) {
+        socket->waitForReadyRead(2*TIME_OUT);
+        delete socket;
+
+        activateWindow();
+    }
+}
+
+void SingleApplication::initLocalConnection() {
+    is_running_ = false;
+
+    QLocalSocket socket;
+    socket.connectToServer(server_name_);
+    if(socket.waitForConnected(TIME_OUT)) {
+        fprintf(stderr, "%s already running.\n",
+                server_name_.toLocal8Bit().constData());
+        is_running_ = true;
+        return;
+    }
+
+    newLocalServer();
+}
+
+void SingleApplication::newLocalServer() {
+    local_server_ = new QLocalServer(this);
+    connect(local_server_, SIGNAL(newConnection()), this, SLOT(newLocalConnection()));
+    if(!local_server_->listen(server_name_)) {
+        if(local_server_->serverError() == QAbstractSocket::AddressInUseError) {
+            QLocalServer::removeServer(server_name_);
+            local_server_->listen(server_name_);
+        }
+    }
+}
+
+void SingleApplication::activateWindow() {
+    if(w) {
+        w->show();
+        w->raise();
+        w->activateWindow(); // Activates the window
+    }
+}
