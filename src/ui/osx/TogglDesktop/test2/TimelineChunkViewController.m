ce696e077cb393761b7507798ba69193446d3838
Tanel Lebedev <tanel.lebedev@gmail.com>
2015-10-26 19:49:46 +0100

    chunk view controller (osx)

---
+++ src/ui/osx/TogglDesktop/test2/TimelineChunkViewController.m
@@ -0,0 +1,23 @@
+//
+//  TimelineChunkViewController.m
+//  TogglDesktop
+//
+//  Created by Tanel Lebedev on 26/10/15.
+//  Copyright © 2015 Toggl Desktop Developers. All rights reserved.
+//
+
+#import "TimelineChunkViewController.h"
+
+@interface TimelineChunkViewController ()
+
+@end
+
+@implementation TimelineChunkViewController
+
+- (void)viewDidLoad
+{
+	[super viewDidLoad];
+	// Do view setup here.
+}
+
+@end
