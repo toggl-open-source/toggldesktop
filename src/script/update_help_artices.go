dd04bdd8ed56da10d7c239f94fbada0e796e2a10
Tanel Lebedev <tanel.lebedev@gmail.com>
2015-11-03 16:17:15 +0100

    add a script for downloading help articles

--- src/script/update_help_artices.go
+++ src/script/update_help_artices.go
@@ -1,14 +1,39 @@
 package main
 
-import "net/http"
-import "fmt"
-import "os"
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+const url = "https://support.toggl.com/sitemap/"
 
 func main() {
-	response, err := http.Get("https://support.toggl.com/sitemap/")
+	response, err := http.Get(url)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 	defer response.Body.Close()
+
+	fileName := filepath.Join("src", "help", "sitemap.json")
+
+	os.Remove(fileName)
+
+	output, err := os.Create(fileName)
+	if err != nil {
+		fmt.Println("Error while creating", fileName, "-", err)
+		return
+	}
+	defer output.Close()
+
+	n, err := io.Copy(output, response.Body)
+	if err != nil {
+		fmt.Println("Error while downloading", url, "-", err)
+		return
+	}
+
+	fmt.Println(n, "bytes downloaded.")
 }
