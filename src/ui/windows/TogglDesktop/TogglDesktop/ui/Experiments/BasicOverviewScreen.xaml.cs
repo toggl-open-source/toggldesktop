594c31b0a7190cf5b725c098f20eb5978317adc9
amulware <amul@amulware.net>
2015-11-05 09:49:56 +0100

    added basic overview screen for first onboarding experiment (win), see #1641

---
+++ src/ui/windows/TogglDesktop/TogglDesktop/ui/Experiments/BasicOverviewScreen.xaml.cs
@@ -0,0 +1,54 @@
+﻿
+using System.Windows;
+
+namespace TogglDesktop.Experiments
+{
+    public partial class BasicOverviewScreen
+    {
+        public BasicOverviewScreen()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void initialise()
+        {
+            this.fillDummies();
+
+            Toggl.SetManualMode(false);
+            Toggl.ViewTimeEntryList();
+        }
+
+        private void fillDummies()
+        {
+            this.cellDummy0.Display(new Toggl.TogglTimeEntryView
+            {
+                Description = "Meeting",
+                ProjectLabel = "Internal",
+                Color = "#6ac4fc",
+            });
+            this.cellDummy1.Display(new Toggl.TogglTimeEntryView
+            {
+                Description = "Research for presentation",
+                ProjectLabel = "Consulting",
+                ClientLabel = "Toy Factory",
+                Color = "#d086ac",
+            });
+            this.cellDummy2.Display(new Toggl.TogglTimeEntryView
+            {
+                Description = "Code review",
+                ProjectLabel = "Frontend",
+                ClientLabel = "Toy Factory",
+                Color = "#69c600",
+            });
+        }
+
+        protected override void cleanup()
+        {
+        }
+
+        private void nextButtonClick(object sender, RoutedEventArgs e)
+        {
+            this.quitTutorial();
+        }
+    }
+}
