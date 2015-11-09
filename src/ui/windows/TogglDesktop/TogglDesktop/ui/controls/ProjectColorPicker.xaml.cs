3bdbce5201fc8697ec2f658c58a3b332e87d08d2
amulware <amul@amulware.net>
2015-11-09 13:15:31 +0100

    finished project colour picker and hooked up for project creation (win), see #1274

--- src/ui/windows/TogglDesktop/TogglDesktop/ui/controls/ProjectColorPicker.xaml.cs
+++ src/ui/windows/TogglDesktop/TogglDesktop/ui/controls/ProjectColorPicker.xaml.cs
@@ -1,5 +1,8 @@
 ﻿
+using System;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace TogglDesktop
 {
@@ -11,8 +14,9 @@ namespace TogglDesktop
             {
                 return new[]
                 {
-                    "#999999", "#ff00ff", "#00ff00", "#0048BA", "#E52B50", "#841B2D",
-                    "#999999", "#ff00ff", "#00ff00", "#0048BA", "#E52B50", "#841B2D"
+                    "#4dc3ff", "#bc85e6", "#df7baa", "#f68d38", "#b27636",
+                    "#8ab734", "#14a88e", "#268bb5", "#6668b4", "#a4506c",
+                    "#67412c", "#3c6526", "#094558", "#bc2d07", "#999999"
                 };
             }
         }
@@ -21,18 +25,13 @@ namespace TogglDesktop
     public partial class ProjectColorPicker
     {
         private string selectedColor;
+        private string[] colors;
 
         public ProjectColorPicker()
         {
             this.InitializeComponent();
 
             Toggl.OnDisplayProjectColors += this.onDisplayProjectColors;
-
-            this.onDisplayProjectColors(new[]
-                {
-                    "#999999", "#ff00ff", "#00ff00", "#0048BA", "#E52B50", "#841B2D",
-                    "#999999", "#ff00ff", "#00ff00", "#0048BA", "#E52B50", "#841B2D"
-                }, 0);
         }
 
         private void onDisplayProjectColors(string[] strings, ulong count)
@@ -40,6 +39,7 @@ namespace TogglDesktop
             if (this.TryBeginInvoke(this.onDisplayProjectColors, strings, count))
                 return;
 
+            this.colors = strings;
             this.list.ItemsSource = strings;
         }
 
@@ -62,7 +62,94 @@ namespace TogglDesktop
 
         private void onButtonClick(object sender, RoutedEventArgs e)
         {
-            this.popup.IsOpen = !this.popup.IsOpen;
+            var open = !this.popup.IsOpen;
+
+            if (open)
+            {
+                this.open();
+            }
+            else
+            {
+                this.close(true);
+            }
+        }
+
+        private void close(bool focusButton = false)
+        {
+            this.popup.IsOpen = false;
+            if (focusButton)
+            {
+                this.button.Focus();   
+            }
+        }
+
+        private void open()
+        {
+            if (!this.ensureProjectColors())
+                return;
+
+            this.popup.IsOpen = true;
+
+            var i = 0;
+            if (this.selectedColor != null)
+            {
+                var argb = Utils.ProjectColorFromString(this.selectedColor);
+
+                i = Array.FindIndex(this.colors,
+                    c => Utils.ProjectColorFromString(c) == argb);
+                if (i < 0)
+                    i = 0;
+            }
+            this.list.SelectedIndex = i;
+            ((Control)this.list.ItemContainerGenerator.ContainerFromIndex(i)).Focus();
+        }
+
+        private bool ensureProjectColors()
+        {
+            if (this.colors != null)
+                return true;
+
+            Toggl.GetProjectColors();
+
+            if (this.colors != null)
+                return true;
+
+            Toggl.Debug("Error: Did not receive project colours when required.");
+            return false;
+        }
+
+        private void onColorSelect(object sender, RoutedEventArgs e)
+        {
+            this.SelectedColor = (string)this.list.SelectedItem;
+
+            this.close(true);
+        }
+
+        private void onPopupPreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Escape:
+                {
+                    this.close(true);
+                    break;
+                }
+                case Key.Enter:
+                {
+                    this.SelectedColor = (string)this.list.SelectedItem;
+                    this.close(true);
+                    break;
+                }
+            }
+        }
+
+
+        private void onPopupKeyboardFocusWithinChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (!this.popup.IsKeyboardFocusWithin && !this.button.IsKeyboardFocusWithin)
+            {
+                this.close();
+            }
         }
     }
 }
