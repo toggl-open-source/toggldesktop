169aa8b1aecf3558f67fa9f8dd25dc248b2ebc5e
amulware <amul@amulware.net>
2015-12-02 16:42:20 +0100

    added second experiment overlay (win), closes #1697

---
+++ src/ui/windows/TogglDesktop/TogglDesktop/ui/Experiments/Experiment87WelcomeScreen.xaml.cs
@@ -0,0 +1,29 @@
+﻿
+namespace TogglDesktop.Experiments
+{
+    public partial class Experiment87WelcomeScreen
+    {
+        public Experiment87WelcomeScreen()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void initialise()
+        {
+            Toggl.SetManualMode(false);
+            Toggl.ViewTimeEntryList();
+
+            Toggl.OnRunningTimerState += this.onRunningTimerState;
+        }
+
+        protected override void cleanup()
+        {
+            Toggl.OnRunningTimerState -= this.onRunningTimerState;
+        }
+
+        private void onRunningTimerState(Toggl.TogglTimeEntryView te)
+        {
+            this.quitTutorial();
+        }
+    }
+}
