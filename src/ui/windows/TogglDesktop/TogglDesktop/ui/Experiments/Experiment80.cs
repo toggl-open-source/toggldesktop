bb589e9ec56e1fde4204c72c022f09c62ffe4818
amulware <amul@amulware.net>
2015-12-02 12:49:43 +0100

    prepared experiment manager for obm api changes (win)

---
+++ src/ui/windows/TogglDesktop/TogglDesktop/ui/Experiments/Experiment80.cs
@@ -0,0 +1,15 @@
+﻿namespace TogglDesktop.Experiments
+{
+    sealed class Experiment80 : ExperimentBase
+    {
+        public Experiment80()
+            : base(80)
+        {
+        }
+
+        protected override void runIncluded(ExperimentParameters parameters)
+        {
+            parameters.TutorialManager.ActivateScreen<BasicOverviewScreen>();
+        }
+    }
+}
---
+++ src/ui/windows/TogglDesktop/TogglDesktop/ui/Experiments/ExperimentParameters.cs
@@ -0,0 +1,16 @@
+﻿using TogglDesktop.Tutorial;
+
+namespace TogglDesktop.Experiments
+{
+    sealed class ExperimentParameters
+    {
+        public ExperimentParameters(bool included, TutorialManager tutorialManager)
+        {
+            this.Included = included;
+            this.TutorialManager = tutorialManager;
+        }
+
+        public bool Included { get; private set; }
+        public TutorialManager TutorialManager { get; private set; }
+    }
+}
