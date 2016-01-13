8d37885bb749f11ace82609c256aa85075eed0a5
amulware <amul@amulware.net>
2016-01-13 15:12:57 +0100

    finished drag-drop of time entries between days (win), closes #1779

--- src/ui/windows/TogglDesktop/TogglDesktop/ui/controls/TimeEntryCellDayHeader.xaml.cs
+++ src/ui/windows/TogglDesktop/TogglDesktop/ui/controls/TimeEntryCellDayHeader.xaml.cs
@@ -9,6 +9,7 @@ namespace TogglDesktop
     public partial class TimeEntryCellDayHeader
     {
         private bool isSelected;
+        private DateTime date;
 
         public bool IsDummy
         {
@@ -59,6 +60,8 @@ namespace TogglDesktop
             {
                 throw new InvalidDataException("Can only create day header from header time entry view.");
             }
+
+            this.date = Toggl.DateTimeFromUnix(item.Started);
 
             if (this.labelFormattedDate.Text != item.DateHeader)
             {
@@ -142,21 +145,12 @@ namespace TogglDesktop
         }
 
 
-        protected override void OnDragEnter(DragEventArgs e)
-        {
-            if (!e.Data.GetDataPresent("time-entry-cell"))
-            {
-                e.Effects = DragDropEffects.None;
-            }
-        }
-
         protected override void OnDrop(DragEventArgs e)
         {
             if (e.Data.GetDataPresent("time-entry-cell"))
             {
                 var cell = (TimeEntryCell)e.Data.GetData("time-entry-cell");
-                // todo: move cell to this day
-                Console.WriteLine("moved cell to " + this.labelFormattedDate.Text);
+                cell.MoveToDay(this.date);
             }
         }
     }
