c1e063b870ec702499ee6460227236c9d8788dd5
amulware <amul@amulware.net>
2016-01-18 16:15:58 +0200

    started hooking up windows pomodoro ui to api (win)

--- src/ui/windows/TogglDesktop/TogglDesktop/ui/windows/PomodoroNotification.xaml.cs
+++ src/ui/windows/TogglDesktop/TogglDesktop/ui/windows/PomodoroNotification.xaml.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Windows;
+
 namespace TogglDesktop
 {
     public partial class PomodoroNotification
@@ -6,6 +8,36 @@ namespace TogglDesktop
         public PomodoroNotification()
         {
             this.InitializeComponent();
+
+            Toggl.OnDisplayPomodoro += this.onDisplayPomodoro;
+        }
+
+        private void onDisplayPomodoro(string title, string informativetext)
+        {
+            if (this.TryBeginInvoke(onDisplayPomodoro, title, informativetext))
+                return;
+
+            this.timeText.Text = informativetext;
+            this.Title = title;
+
+            this.Show();
+        }
+
+        protected override void onCloseButtonClick(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void onStopClicked(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            Toggl.Stop();
+            Toggl.ShowApp();
+        }
+
+        private void onResetClick(object sender, RoutedEventArgs e)
+        {
+            //TODO: call api
         }
     }
 }
