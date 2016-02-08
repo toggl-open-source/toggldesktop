8e2794c2aecd20e644dc6d3dc2eaa69ab8c10be3
amulware <amul@amulware.net>
2016-02-08 11:54:07 +0100

    added experiment 99

---
+++ src/ui/windows/TogglDesktop/TogglDesktop/ui/Experiments/implementations/Experiment99.cs
@@ -0,0 +1,27 @@
+﻿namespace TogglDesktop.Experiments
+{
+    sealed class Experiment99 : ExperimentBase
+    {
+        public Experiment99()
+            : base(99, false)
+        {
+        }
+
+        protected override void runIncluded(ExperimentParameters parameters) 
+        {
+            if (parameters.IsFirstRun)
+            {
+                parameters.TutorialManager.ActivateScreen<Experiment99Screen>();
+            }
+
+            Toggl.OnUserTimeEntryStart += this.sendStartAction;
+        }
+
+        private void sendStartAction()
+        {
+            Toggl.SendObmAction(this.Id, "start");
+
+            Toggl.OnUserTimeEntryStart -= this.sendStartAction;
+        }
+    }
+}
