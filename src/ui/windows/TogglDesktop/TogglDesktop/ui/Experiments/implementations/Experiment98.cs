e6c1488f2919ad636c35110191db28781c087df3
amulware <amul@amulware.net>
2016-02-08 15:44:13 +0100

    modify empty state for experiment 98 (win)

--- src/ui/windows/TogglDesktop/TogglDesktop/ui/Experiments/implementations/Experiment98.cs
+++ src/ui/windows/TogglDesktop/TogglDesktop/ui/Experiments/implementations/Experiment98.cs
@@ -1,4 +1,8 @@
-﻿namespace TogglDesktop.Experiments
+﻿using System;
+using System.CodeDom;
+using System.Windows.Controls;
+
+namespace TogglDesktop.Experiments
 {
     sealed class Experiment98 : ExperimentBase
     {
@@ -12,9 +16,30 @@
             if (parameters.IsFirstRun)
             {
                 parameters.TutorialManager.ActivateScreen<Experiment98Screen1>();
+                this.pruneEmptyState(parameters);
             }
 
             Toggl.OnUserTimeEntryStart += this.sendStartAction;
+        }
+
+        private void pruneEmptyState(ExperimentParameters parameters)
+        {
+            var emptyStateButton = parameters.TutorialManager
+                .MainWindow
+                .GetView<TimerEntryListView>()
+                .Entries
+                .FindName("emptyListText") as Button;
+
+            if (emptyStateButton != null)
+            {
+                var stackPanel = emptyStateButton.Content as StackPanel;
+                if (stackPanel != null)
+                {
+                    stackPanel.Children[0].RemoveFromParent();
+                    return;
+                }
+            }
+            throw new Exception("Unable to modify empty state in experiment 98.");
         }
 
         private void sendStartAction()
