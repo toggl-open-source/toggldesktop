066994f9b1a8408a5b0ee07647881732191c1fc7
amulware <amul@amulware.net>
2016-02-12 10:37:36 +0100

    update workspace locked time on sync (lib)

--- src/context.cc
+++ src/context.cc
@@ -4011,6 +4011,8 @@ error Context::pullAllUserData(
             user_->LoadUserAndRelatedDataFromJSONString(user_data_json, !since);
         }
 
+		pullWorkspacePreferences(toggl_client);
+
         stopwatch.stop();
         std::stringstream ss;
         ss << "User with related data JSON fetched and parsed in "
