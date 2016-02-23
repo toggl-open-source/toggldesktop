970b7ee633f07bbb570f424e8d495e051e9f7495
IndrekV <indzuliin@gmail.com>
2016-02-23 13:34:52 +0200

    Added loader and improved design of load more button (osx), closes #1791

--- src/ui/osx/TogglDesktop/test2/LoadMoreCell.m
+++ src/ui/osx/TogglDesktop/test2/LoadMoreCell.m
@@ -9,5 +9,21 @@
 #import "LoadMoreCell.h"
 
 @implementation LoadMoreCell
+extern void *ctx;
+
+- (void)initCell
+{
+	[self.loadButton setTitle:@"Load More"];
+	[self.loader stopAnimation:nil];
+	[self.loader setHidden:YES];
+}
+
+- (IBAction)loadMoreClicked:(id)sender
+{
+	[self.loadButton setTitle:@""];
+	[self.loader startAnimation:sender];
+	[self.loader setHidden:NO];
+	toggl_load_more(ctx);
+}
 
 @end
