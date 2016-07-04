0415a0b52ab36ebdf47fb688b30787c0370936f1
IndrekV <indzuliin@gmail.com>
2016-07-04 16:05:51 +0300

    Updated linux app command to avoid crash when using symbolic link (linux), closes #1814

--- src/ui/linux/TogglDesktop.sh
+++ src/ui/linux/TogglDesktop.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-appname=`basename $0 | sed s,\.sh$,,`
-
-dirname=`dirname $0`
+appname=`basename "$(test -L "$0" && readlink "$0" || echo "$0")" | sed s,\.sh$,,`
+dirname=`dirname "$(test -L "$0" && readlink "$0" || echo "$0")"`
 tmp="${dirname#?}"
 
 if [ "${dirname%$tmp}" != "/" ]; then
 dirname=$PWD/$dirname
 fi
+
 LD_LIBRARY_PATH=$dirname/lib
 export LD_LIBRARY_PATH
 $dirname/$appname "$@" &
