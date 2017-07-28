f8645a622da3a6f4dd8c4f2f663c0b4deaf0dddb
IndrekV <indzuliin@gmail.com>
2017-07-28 15:29:24 +0300

    Give each activity 2 seconds to shutdown (lib), closes #2201

--- src/context.cc
+++ src/context.cc
@@ -163,7 +163,7 @@ void Context::stopActivities() {
             Poco::Mutex::ScopedLock lock(reminder_m_);
             if (reminder_.isRunning()) {
                 reminder_.stop();
-                reminder_.wait(5);
+                reminder_.wait(2000);
             }
         }
 
@@ -171,7 +171,7 @@ void Context::stopActivities() {
             Poco::Mutex::ScopedLock lock(ui_updater_m_);
             if (ui_updater_.isRunning()) {
                 ui_updater_.stop();
-                ui_updater_.wait(5);
+                ui_updater_.wait(2000);
             }
         }
 
@@ -180,7 +180,7 @@ void Context::stopActivities() {
             Poco::Mutex::ScopedLock lock(syncer_m_);
             if (syncer_.isRunning()) {
                 syncer_.stop();
-                syncer_.wait(5);
+                syncer_.wait(2000);
             }
         }
     } catch(const Poco::Exception& exc) {
@@ -222,7 +222,7 @@ void Context::Shutdown() {
         timer_.cancel(false);
     }
 
-    // Stops all running threads and waits for their completion.
+    // Stops all running threads and waits for their completion (maximum 10 seconds).
     Poco::ThreadPool::defaultPool().stopAll();
 }
 
