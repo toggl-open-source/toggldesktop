8c6f79f9b743c72d90f44d24bae6d9a1efc55dfe
IndrekV <indzuliin@gmail.com>
2018-04-26 18:17:55 +1000

    Added ListBoxItem element (win)

---
+++ src/ui/windows/TogglDesktop/TogglDesktop/AutoCompletion/ListBoxItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TogglDesktop.AutoCompletion
+{
+    class ListBoxItem
+    {
+        public string Text { get; set; }
+        public string ProjectLabel { get; set; }
+        public string ProjectColor { get; set; }
+        public string Description { get; set; }
+        public string TaskLabel { get; set; }
+        public string ClientLabel { get; set; }
+        public string Category { get; set; }
+        public int Type { get; set; }
+        public int Index { get; set; }
+    }
+}
