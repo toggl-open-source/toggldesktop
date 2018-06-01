3d4ae7772bf96782cb7c9e2c4512a731cb699d64
IndrekV <indzuliin@gmail.com>
2018-06-01 12:01:00 +0300

    Converted MissingWSView to OverlayView (mac), #2421

--- src/ui/osx/TogglDesktop/test2/OverlayViewController.m
+++ src/ui/osx/TogglDesktop/test2/OverlayViewController.m
@@ -1,15 +1,15 @@
 //
-//  MissingWSViewController.m
+//  OverlayViewController.m
 //  TogglDesktop
 //
 //  Created by Indrek Vändrik on 31/08/2017.
-//  Copyright © 2017 Alari. All rights reserved.
+//  Copyright © 2017 Toggl Desktop Developers. All rights reserved.
 //
 
 #import "OverlayViewController.h"
 
 @interface OverlayViewController ()
-
+@property int currentType;
 @end
 
 @implementation OverlayViewController
@@ -19,16 +19,56 @@ extern void *ctx;
 - (void)viewDidLoad
 {
 	[super viewDidLoad];
+}
+
+- (IBAction)actionClicked:(id)sender
+{
+	if (self.currentType == 0)
+	{
+		toggl_open_in_browser(ctx);
+	}
+	else
+	{
+		toggl_accept_tos(ctx);
+	}
+}
+
+- (void)textFieldClicked:(id)sender
+{
+	if (self.currentType == 0 && sender == self.bottomLink)
+	{
+		toggl_fullsync(ctx);
+	}
+}
+
+- (void)setType:(int)type
+{
+	self.currentType = type;
+
+	if (self.currentType == 0)
+	{
+		// missing ws
+		[self setupMissingWS];
+	}
+	else if (self.currentType == 1)
+	{
+		// tos accept
+		[self setupTos];
+	}
+}
+
+- (void)setupMissingWS
+{
+	[self.actionButton setTitle:@"Log in to Toggl.com"];
+	[self.topText setStringValue:@"You no longer have access to your last Workspace. Create a new workspace on Toggl.com to continue tracking"];
+
 	// Setup up text underline for "Force sync" link
 
 	NSMutableParagraphStyle *paragrapStyle = NSMutableParagraphStyle.new;
-
 	paragrapStyle.alignment                = kCTTextAlignmentCenter;
 
-
 	NSMutableAttributedString *string =
-		[[NSMutableAttributedString alloc] initWithString:self.syncLink.stringValue];
-
+		[[NSMutableAttributedString alloc] initWithString:@"Created your new workspace?\n"];
 
 	NSMutableAttributedString *sync =
 		[[NSMutableAttributedString alloc] initWithString:@"Force sync"];
@@ -38,20 +78,109 @@ extern void *ctx;
 	[string appendAttributedString:sync];
 	[string addAttribute:NSParagraphStyleAttributeName value:paragrapStyle range:NSMakeRange(0, string.length)];
 
-	[self.syncLink setAttributedStringValue:string];
+	[self.bottomLink setAttributedStringValue:string];
 }
 
-- (IBAction)loginClicked:(id)sender
+- (void)setupTos
 {
-	toggl_open_in_browser(ctx);
-}
+	[self.actionButton setTitle:@"I accept"];
 
-- (void)textFieldClicked:(id)sender
-{
-	if (sender == self.syncLink)
-	{
-		toggl_fullsync(ctx);
-	}
+	// title text
+	NSMutableParagraphStyle *topParagrapStyle = NSMutableParagraphStyle.new;
+	topParagrapStyle.alignment                = kCTTextAlignmentCenter;
+
+	NSMutableParagraphStyle *paragrapStyle = NSMutableParagraphStyle.new;
+	paragrapStyle.alignment                = kCTTextAlignmentCenter;
+	paragrapStyle.lineHeightMultiple = 1.5;
+
+	NSMutableAttributedString *result = [[NSMutableAttributedString alloc] initWithString:@""];
+
+	NSMutableAttributedString *string = [[NSMutableAttributedString alloc] initWithString:@"Our Terms of Service and Privacy Policy have changed\n\n"];
+
+	[string setAttributes:
+	 @{
+		 NSForegroundColorAttributeName:[NSColor whiteColor],
+		 NSFontAttributeName: [NSFont systemFontOfSize:20],
+		 NSParagraphStyleAttributeName: topParagrapStyle
+	 }
+					range:NSMakeRange(0, [string length])];
+
+	[result appendAttributedString:string];
+
+	// content
+
+	NSURL *tosUrl = [NSURL URLWithString:@"https://toggl.com/legal/terms"];
+	NSURL *privacyUrl = [NSURL URLWithString:@"https://toggl.com/legal/privacy"];
+
+	string = [[NSMutableAttributedString alloc] initWithString:@"Please read and accept our updated "];
+	[string setAttributes:
+	 @{
+		 NSForegroundColorAttributeName:[NSColor whiteColor],
+		 NSParagraphStyleAttributeName: paragrapStyle
+	 }
+					range:NSMakeRange(0, [string length])];
+	[result appendAttributedString:string];
+
+	// terms of service url
+	string = [[NSMutableAttributedString alloc] initWithString:@"Terms of Service"];
+	[string setAttributes:
+	 @{
+		 NSLinkAttributeName: tosUrl,
+		 NSUnderlineStyleAttributeName: [NSNumber numberWithInt:NSUnderlineStyleSingle]
+	 }
+					range:NSMakeRange(0, [string length])];
+
+	[result appendAttributedString:string];
+
+	string = [[NSMutableAttributedString alloc] initWithString:@" and "];
+	[string setAttributes:
+	 @{
+		 NSForegroundColorAttributeName:[NSColor whiteColor],
+		 NSParagraphStyleAttributeName: paragrapStyle
+	 }
+					range:NSMakeRange(0, [string length])];    [result appendAttributedString:string];
+
+	// privacy url
+	string = [[NSMutableAttributedString alloc] initWithString:@"Privacy Policy"];
+
+	[string setAttributes:
+	 @{
+		 NSLinkAttributeName: privacyUrl,
+		 NSUnderlineStyleAttributeName: [NSNumber numberWithInt:NSUnderlineStyleSingle]
+	 }
+					range:NSMakeRange(0, [string length])];
+	[result appendAttributedString:string];
+
+	string = [[NSMutableAttributedString alloc] initWithString:@" to continue using Toggl."];
+	[string setAttributes:
+	 @{
+		 NSForegroundColorAttributeName:[NSColor whiteColor],
+		 NSParagraphStyleAttributeName: paragrapStyle
+	 }
+					range:NSMakeRange(0, [string length])];    [result appendAttributedString:string];
+
+	[self.mainText setTextColor:[NSColor whiteColor]];
+	self.mainText.linkTextAttributes = @{ NSForegroundColorAttributeName:[NSColor whiteColor] };
+
+	[[self.mainText textStorage] setAttributedString:result];
+
+
+	// bottom text
+	NSMutableAttributedString *bottomString = [[NSMutableAttributedString alloc] initWithString:@"If you have any questions, contact us at "];
+
+	NSMutableAttributedString *mail = [[NSMutableAttributedString alloc] initWithString:@"support@toggl.com"];
+
+	[mail setAttributes:
+	 @{
+		 NSForegroundColorAttributeName:[NSColor whiteColor],
+		 NSFontAttributeName : [NSFont boldSystemFontOfSize:12]
+	 }
+				  range:NSMakeRange(0, [mail length])];
+
+	[bottomString appendAttributedString:mail];
+	[bottomString addAttribute:NSParagraphStyleAttributeName value:paragrapStyle range:NSMakeRange(0, string.length)];
+
+	[self.bottomLink setAttributedStringValue:bottomString];
 }
 
 @end
