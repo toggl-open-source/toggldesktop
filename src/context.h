9fb22c52f733cfa15ae25f29db5cd8eaf805e699
IndrekV <indzuliin@gmail.com>
2018-10-06 18:13:16 +0300

    Always do all sync in background and avoid mutex deadlocks that freeze ui (lib), #2428, #2585

--- src/context.h
+++ src/context.h
@@ -643,7 +643,8 @@ class Context : public TimelineDatasource {
 
     bool update_check_disabled_;
 
-    bool had_something_to_push_;
+    bool trigger_sync_;
+    bool trigger_push_;
 
     Poco::LocalDateTime last_time_entry_list_render_at_;
 
