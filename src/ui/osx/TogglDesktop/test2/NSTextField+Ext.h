3ba200cb77f6ab19f46f8f235a58de7399c13355
Nghia Tran <vinhnghiatran@gmail.com>
2018-11-20 13:42:27 +0700

    Reset Cursor's color in Email/Pass TextField

---
+++ src/ui/osx/TogglDesktop/test2/NSTextField+Ext.h
@@ -0,0 +1,19 @@
+//
+//  NSTextField+Ext.h
+//  TogglDesktop
+//
+//  Created by Nghia Tran on 11/20/18.
+//  Copyright © 2018 Alari. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSTextField(Extension)
+
+-(void)resetCursorColor;
+
+@end
+
+NS_ASSUME_NONNULL_END
