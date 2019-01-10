e83fb4554ad2d14bb5ac802564c3cca5c5ad9490
Nghia Tran <vinhnghiatran@gmail.com>
2019-01-10 10:51:39 +0700

    Introduce NotificationCenter+MainThread (mac)

---
+++ src/ui/osx/TogglDesktop/test2/NotificationCenter+MainThread.swift
@@ -0,0 +1,32 @@
+//
+//  NotificationCenter+MainThread.swift
+//  TogglDesktop
+//
+//  Created by Nghia Tran on 1/10/19.
+//  Copyright © 2019 Alari. All rights reserved.
+//
+
+import Foundation
+
+extension NotificationCenter {
+
+    func postOnMainThread(with aName: NSNotification.Name, object anObject: Any?) {
+        runOnMainThreadIfNeed {[unowned self] in
+            self.post(name: aName, object: anObject)
+        }
+    }
+}
+
+func runOnMainThreadIfNeed(_ block: @escaping () -> Void) {
+
+    // If it's main thread, just execute
+    if Thread.isMainThread {
+        block()
+        return
+    }
+
+    // Or run async on Main Thread later
+    DispatchQueue.main.async {
+        block()
+    }
+}
