5b32c1fa72d1ad4d2d3de479d04049a555fbe61b
Nghia Tran <vinhnghiatran@gmail.com>
2019-01-24 17:13:35 +0700

    Remove old MASShortcut

--- third_party/MASShortcut/MASShortcut+Monitoring.h
+++
@@ -1,8 +0,0 @@
-#import "MASShortcut.h"
-
-@interface MASShortcut (Monitoring)
-
-+ (id)addGlobalHotkeyMonitorWithShortcut:(MASShortcut *)shortcut handler:(void (^)())handler;
-+ (void)removeGlobalHotkeyMonitor:(id)monitor;
-
-@end
--- third_party/MASShortcut/MASShortcut+UserDefaults.h
+++
@@ -1,9 +0,0 @@
-#import "MASShortcut.h"
-
-@interface MASShortcut (UserDefaults)
-
-+ (void)registerGlobalShortcutWithUserDefaultsKey:(NSString *)userDefaultsKey handler:(void (^)())handler;
-+ (void)unregisterGlobalShortcutWithUserDefaultsKey:(NSString *)userDefaultsKey;
-+ (void)setGlobalShortcut:(MASShortcut *)shortcut forUserDefaultsKey:(NSString *)userDefaultsKey;
-
-@end
