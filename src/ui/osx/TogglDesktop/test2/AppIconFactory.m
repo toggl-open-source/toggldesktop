cc41bb593c58d7ce8d2928a5e4d22f48cc4c700d
Nghia Tran <vinhnghiatran@gmail.com>
2019-01-24 10:40:57 +0700

    Fix blur inactive_icon app icon os 10.11 (mac)

--- src/ui/osx/TogglDesktop/test2/AppIconFactory.m
+++ src/ui/osx/TogglDesktop/test2/AppIconFactory.m
@@ -20,7 +20,7 @@
 			icon = [NSImage imageNamed:@"AppIconActive"];
 			break;
 		case AppIconTypeDefault :
-			icon = [NSImage imageNamed:@"AppIcon"];
+			icon = [NSImage imageNamed:NSImageNameApplicationIcon];
 			break;
 	}
 
