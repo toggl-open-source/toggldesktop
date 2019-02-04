b9ef4e633c2d9985f3c48edbce073e32545fc947
IndrekV <indzuliin@gmail.com>
2019-02-04 15:52:37 +0200

    Updated ssl cert fetch script, #2786

--- src/ssl/update_certificate_bundle.sh
+++ src/ssl/update_certificate_bundle.sh
@@ -1,4 +1,7 @@
+
+#https://curl.haxx.se/docs/caextract.html
+
 rm cacert.pem
-wget http://curl.haxx.se/ca/cacert.pem
+curl --remote-name --time-cond cacert.pem https://curl.haxx.se/ca/cacert.pem
 openssl s_client -showcerts -connect toggl.com:443 </dev/null >> cacert.pem
 
