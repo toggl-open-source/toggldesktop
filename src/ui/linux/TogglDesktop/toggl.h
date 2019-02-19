44528e4e4a5b2303cbf9bc0835e4669a2564d8f0
IndrekV <indzuliin@gmail.com>
2019-02-19 12:56:04 +0200

    Added countryID to signup request (linux), #2807

--- src/ui/linux/TogglDesktop/toggl.h
+++ src/ui/linux/TogglDesktop/toggl.h
@@ -38,8 +38,6 @@ class TogglApi : public QObject {
         const QString message,
         const QString context);
 
-    u_int64_t countryID;
-
     bool shutdown;
 
     bool startEvents();
@@ -50,7 +48,8 @@ class TogglApi : public QObject {
 
     void login(const QString email, const QString password);
 
-    void signup(const QString email, const QString password);
+    void signup(const QString email, const QString password,
+        const uint64_t countryID);
 
     void googleLogin(const QString accessToken);
 
