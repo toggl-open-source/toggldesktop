2d40386a65731f6d5b12fff9a28874b09f32090a
Nghia Tran <vinhnghiatran@gmail.com>
2019-03-04 13:40:45 +0700

    Fix incorrect constrains for MessageView in MainWindows (mac)

--- src/ui/osx/TogglDesktop/MainWindowController.m
+++ src/ui/osx/TogglDesktop/MainWindowController.m
@@ -92,14 +92,18 @@ extern void *ctx;
 	self.messageView.translatesAutoresizingMaskIntoConstraints = NO;
 	[self.contentView addSubview:self.messageView];
 
-//    [self.contentView addConstraint:[NSLayoutConstraint constraintWithItem:self.contentView attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.messageView attribute:NSLayoutAttributeTop multiplier:1.0 constant:0]];
-//    [self.contentView addConstraint:[NSLayoutConstraint constraintWithItem:self.contentView attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:self.messageView attribute:NSLayoutAttributeLeading multiplier:1.0 constant:0]];
-//    [self.contentView addConstraint:[NSLayoutConstraint constraintWithItem:self.contentView attribute:NSLayoutAttributeTrailing relatedBy:NSLayoutRelationEqual toItem:self.messageView attribute:NSLayoutAttributeTrailing multiplier:1.0 constant:0]];
-//    [self.contentView addConstraint:[NSLayoutConstraint constraintWithItem:self.contentView attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.messageView attribute:NSLayoutAttributeBottom multiplier:1.0 constant:0]];
+	[self.messageView addConstraint:[NSLayoutConstraint constraintWithItem:self.messageView attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.0 constant:240.0]];
+	NSLayoutConstraint *height = [NSLayoutConstraint constraintWithItem:self.messageView attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.0 constant:38.0];
+	height.priority = NSLayoutPriorityDefaultLow;
+	[self.messageView addConstraint:height];
+
+	[self.contentView addConstraint:[NSLayoutConstraint constraintWithItem:self.contentView attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationGreaterThanOrEqual toItem:self.messageView attribute:NSLayoutAttributeLeading multiplier:1.0 constant:30.0]];
+	[self.contentView addConstraint:[NSLayoutConstraint constraintWithItem:self.contentView attribute:NSLayoutAttributeTrailing relatedBy:NSLayoutRelationEqual toItem:self.messageView attribute:NSLayoutAttributeTrailing multiplier:1.0 constant:30.0]];
+	[self.contentView addConstraint:[NSLayoutConstraint constraintWithItem:self.contentView attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.messageView attribute:NSLayoutAttributeBottom multiplier:1.0 constant:10.0]];
 
 	// Able to draw shadow
-//    self.errorContainerView.wantsLayer = YES;
-//    self.errorContainerView.layer.masksToBounds = NO;
+	self.messageView.wantsLayer = YES;
+	self.messageView.layer.masksToBounds = NO;
 //
 //    // Hidden by default
 //    self.errorContainerView.hidden = YES;
