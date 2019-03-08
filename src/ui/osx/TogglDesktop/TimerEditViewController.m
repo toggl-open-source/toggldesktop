8461b2eb49f9963068bdbf0c12c3d290f8b9d7df
Nghia Tran <vinhnghiatran@gmail.com>
2019-03-08 16:46:37 +0700

    Add option to make the AutoComplete container to stich to Window or their view (mac)

--- src/ui/osx/TogglDesktop/TimerEditViewController.m
+++ src/ui/osx/TogglDesktop/TimerEditViewController.m
@@ -122,6 +122,7 @@ NSString *kInactiveTimerColor = @"#999999";
 	// Manual as default
 	self.displayMode = DisplayModeManual;
 	self.projectTextField.isInTimerBar = YES;
+	self.autoCompleteInput.expandToMainWindow = YES;
 	self.liteAutocompleteDataSource.input = self.autoCompleteInput;
 	[self.liteAutocompleteDataSource setFilter:@""];
 
