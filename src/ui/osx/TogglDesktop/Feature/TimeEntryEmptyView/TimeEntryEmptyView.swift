4bd2ccd73570c94e41ac8902973b80a9604be7a3
Nghia Tran <vinhnghiatran@gmail.com>
2019-03-14 16:29:47 +0700

    Basic layout for Empty State (mac)

---
+++ src/ui/osx/TogglDesktop/Feature/TimeEntryEmptyView/TimeEntryEmptyView.swift
@@ -0,0 +1,19 @@
+//
+//  TimeEntryEmptyView.swift
+//  TogglDesktop
+//
+//  Created by Nghia Tran on 3/14/19.
+//  Copyright © 2019 Alari. All rights reserved.
+//
+
+import Cocoa
+
+final class TimeEntryEmptyView: NSView {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
