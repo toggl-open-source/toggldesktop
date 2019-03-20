25823dce1b5a6c969a96e72ecd1e37ec3a175a58
Nghia Tran <vinhnghiatran@gmail.com>
2019-03-20 10:24:22 +0700

    Remove old confirmless code (mac)

--- src/ui/osx/TogglDesktop/TimeEntryViewItem.m
+++ src/ui/osx/TogglDesktop/TimeEntryViewItem.m
@@ -135,8 +135,6 @@
 	{
 		self.Error = nil;
 	}
-	// If duration is less than 15 seconds delete without confirmation
-	self.confirmlessDelete = (self.duration_in_seconds < 15);
 
 	// Grouped mode
 
