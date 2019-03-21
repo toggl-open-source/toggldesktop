b473d83cbd74906ab16bd8aba17e60d9f2a92587
Nghia Tran <vinhnghiatran@gmail.com>
2019-03-21 17:35:48 +0700

    Basic layout for Time Entry Editor (mac)

---
+++ src/ui/osx/TogglDesktop/Feature/TimeEntryEditor/EditorViewController.swift
@@ -0,0 +1,18 @@
+//
+//  EditorViewController.swift
+//  TogglDesktop
+//
+//  Created by Nghia Tran on 3/21/19.
+//  Copyright © 2019 Alari. All rights reserved.
+//
+
+import Cocoa
+
+class EditorViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
