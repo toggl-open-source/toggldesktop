eacab990800024103b870e66b8aeda5541fb7d4b
Nghia Tran <vinhnghiatran@gmail.com>
2019-03-22 15:17:32 +0700

    Override the appearance in Popover and don't return vibrant color (mac)

--- src/ui/osx/TogglDesktop/MainWindowController.m
+++ src/ui/osx/TogglDesktop/MainWindowController.m
@@ -236,7 +236,7 @@ extern void *ctx;
 
 - (BOOL)isEditOpened
 {
-	return self.timeEntryListViewController.timeEntrypopover.shown;
+	return self.timeEntryListViewController.isEditorOpen;
 }
 
 - (void)trackWindowSize
