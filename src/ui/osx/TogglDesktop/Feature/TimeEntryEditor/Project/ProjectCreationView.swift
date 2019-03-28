396adf84280896a9de7379d10804d441488ad0ae
Nghia Tran <vinhnghiatran@gmail.com>
2019-03-28 11:09:53 +0700

    Layout for Project Creation View (mac)

---
+++ src/ui/osx/TogglDesktop/Feature/TimeEntryEditor/Project/ProjectCreationView.swift
@@ -0,0 +1,19 @@
+//
+//  ProjectCreationView.swift
+//  TogglDesktop
+//
+//  Created by Nghia Tran on 3/28/19.
+//  Copyright © 2019 Alari. All rights reserved.
+//
+
+import Cocoa
+
+final class ProjectCreationView: NSView {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
