cf72015b78a0759c0f5b1492767d40a0f28c08d8
Nghia Tran <vinhnghiatran@gmail.com>
2019-03-28 16:18:13 +0700

    Implement data source for Color CollectionView (mac)

--- src/ui/osx/TogglDesktop/Feature/TimeEntryEditor/Project/ProjectColor.swift
+++ src/ui/osx/TogglDesktop/Feature/TimeEntryEditor/Project/ProjectColor.swift
@@ -11,6 +11,20 @@ import Foundation
 struct ProjectColor {
 
     static let `default` = ProjectColor(colorHex: "#c56bff")
-    
+    static let defaultColors: [ProjectColor] = [ProjectColor(colorHex: "#06aaf5"),
+                                                ProjectColor(colorHex: "#c56bff"),
+                                                ProjectColor(colorHex: "#ea468d"),
+                                                ProjectColor(colorHex: "#fb8b14"),
+                                                ProjectColor(colorHex: "#c7741c"),
+                                                ProjectColor(colorHex: "#f1c33f"),
+                                                ProjectColor(colorHex: "#e20505"),
+                                                ProjectColor(colorHex: "#4bc800"),
+                                                ProjectColor(colorHex: "#04bb9b"),
+                                                ProjectColor(colorHex: "#e19a86"),
+                                                ProjectColor(colorHex: "#3750b5"),
+                                                ProjectColor(colorHex: "#a01aa5"),
+                                                ProjectColor(colorHex: "#205500"),
+                                                ProjectColor(colorHex: "#000000")]
+
     let colorHex: String
 }
