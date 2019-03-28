91d643d28bbf18ddbdbd732921d2094e2f06ce4c
Nghia Tran <vinhnghiatran@gmail.com>
2019-03-28 11:30:44 +0700

    Basic layout for Color picker (mac)

---
+++ src/ui/osx/TogglDesktop/Feature/TimeEntryEditor/Project/ColorPickerView.swift
@@ -0,0 +1,19 @@
+//
+//  ColorPickerView.swift
+//  TogglDesktop
+//
+//  Created by Nghia Tran on 3/28/19.
+//  Copyright © 2019 Alari. All rights reserved.
+//
+
+import Cocoa
+
+class ColorPickerView: NSView {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
---
+++ src/ui/osx/TogglDesktop/Feature/TimeEntryEditor/Project/ProjectColor.swift
@@ -0,0 +1,14 @@
+//
+//  ProjectColor.swift
+//  TogglDesktop
+//
+//  Created by Nghia Tran on 3/28/19.
+//  Copyright © 2019 Alari. All rights reserved.
+//
+
+import Foundation
+
+struct ProjectColor {
+
+    let colorHex: String
+}
