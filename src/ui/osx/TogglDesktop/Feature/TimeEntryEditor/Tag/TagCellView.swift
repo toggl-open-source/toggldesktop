cb35f6b2534b67056ecd19d26ba4b7c19a910a2b
Nghia Tran <vinhnghiatran@gmail.com>
2019-04-11 14:00:40 +0700

    Fix background color for TagCell when selecting and hover (mac)

--- src/ui/osx/TogglDesktop/Feature/TimeEntryEditor/Tag/TagCellView.swift
+++ src/ui/osx/TogglDesktop/Feature/TimeEntryEditor/Tag/TagCellView.swift
@@ -13,17 +13,25 @@ protocol TagCellViewDelegate: class {
     func tagSelectionStateOnChange(with tag: Tag, isSelected: Bool)
 }
 
-final class TagCellView: HoverTableCellView {
+final class TagCellView: NSTableCellView {
 
     static let cellHeight: CGFloat = 34.0
 
     weak var delegate: TagCellViewDelegate?
     private var tagItem: Tag?
+    private var isSelected = false
     private lazy var backgroundColor: NSColor = {
         if #available(OSX 10.13, *) {
             return NSColor(named: NSColor.Name("tag-selection-background-color"))!
         } else {
             return ConvertHexColor.hexCode(toNSColor: "#e5f9e8")
+        }
+    }()
+    private lazy var hoverBackgroundColor: NSColor = {
+        if #available(OSX 10.13, *) {
+            return NSColor(named: NSColor.Name("auto-complete-cell-hover"))!
+        } else {
+            return NSColor(calibratedRed: 177.0/255.0, green: 177.0/255.0, blue: 177.0/255.0, alpha: 0.2)
         }
     }()
 
@@ -34,13 +42,21 @@ final class TagCellView: HoverTableCellView {
 
     // MARK: Public
 
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        initTracking()
+    }
+
     override func prepareForReuse() {
         super.prepareForReuse()
+        NSCursor.arrow.set()
+        isSelected = false
         checkButton.state = .off
     }
 
     func render(_ tag: Tag, isSelected: Bool) {
         self.tagItem = tag
+        self.isSelected = isSelected
         checkButton.title = tag.name
         checkButton.state = isSelected ? .on : .off
         backgroundView.fillColor = isSelected ? backgroundColor : .clear
@@ -50,5 +66,30 @@ final class TagCellView: HoverTableCellView {
         guard let tagItem = tagItem else { return }
         let isSelected = checkButton.state == .on
         delegate?.tagSelectionStateOnChange(with: tagItem, isSelected: isSelected)
+    }
+
+    override func mouseExited(with event: NSEvent) {
+        super.mouseExited(with: event)
+        NSCursor.arrow.set()
+        if !isSelected {
+            backgroundView.animator().alphaValue = 0.0
+        }
+    }
+
+    override func mouseEntered(with event: NSEvent) {
+        super.mouseEntered(with: event)
+        NSCursor.pointingHand.set()
+        if !isSelected {
+            backgroundView.fillColor = hoverBackgroundColor
+            backgroundView.animator().alphaValue = 1.0
+        }
+    }
+
+    fileprivate func initTracking() {
+        let trackingArea = NSTrackingArea(rect: bounds,
+                                          options: [.activeAlways, .inVisibleRect, .mouseEnteredAndExited],
+                                          owner: self,
+                                          userInfo: nil)
+        addTrackingArea(trackingArea)
     }
 }
