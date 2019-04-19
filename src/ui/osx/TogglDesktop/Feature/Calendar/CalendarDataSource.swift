df0cbba5ad0233f094529ac992c400db0f302969
Nghia Tran <vinhnghiatran@gmail.com>
2019-04-19 17:09:47 +0700

    Basic logic for calculating date in Calendar (mac)

--- src/ui/osx/TogglDesktop/Feature/Calendar/CalendarDataSource.swift
+++ src/ui/osx/TogglDesktop/Feature/Calendar/CalendarDataSource.swift
@@ -10,9 +10,32 @@ import Foundation
 
 final class CalendarDataSource: NSObject {
 
+    private struct Constants {
+
+        static let shiftWeek = 4 * 4
+    }
+
+    // MARK: Variables
+
+    var selectedDate: Date
+    private var currentDate: DateInfo
+    private var fromDate: DateInfo
+    private var toDate: DateInfo
+
+    private var numberOfItems: Int {
+        return (toDate.weekOfYear - fromDate.weekOfYear + 1) * 7 // number of week * 7 days a week
+    }
 
     // MARK: Init
 
+    init(_ selectedDate: Date) {
+        self.selectedDate = selectedDate
+        currentDate = DateInfo(date: selectedDate)
+        let from = Calendar.current.date(byAdding: .weekOfYear, value: -Constants.shiftWeek, to: selectedDate)!
+        let to = Calendar.current.date(byAdding: .weekOfYear, value: Constants.shiftWeek, to: selectedDate)!
+        fromDate = DateInfo(date: from)
+        toDate = DateInfo(date: to)
+    }
 }
 
 extension CalendarDataSource: NSCollectionViewDelegate, NSCollectionViewDataSource, NSCollectionViewDelegateFlowLayout {
@@ -22,7 +45,7 @@ extension CalendarDataSource: NSCollectionViewDelegate, NSCollectionViewDataSour
     }
 
     func collectionView(_ collectionView: NSCollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 0
+        return numberOfItems
     }
 
     func collectionView(_ collectionView: NSCollectionView, itemForRepresentedObjectAt indexPath: IndexPath) -> NSCollectionViewItem {
--- src/ui/osx/TogglDesktop/Feature/Calendar/DateInfo.swift
+++ src/ui/osx/TogglDesktop/Feature/Calendar/DateInfo.swift
@@ -9,5 +9,17 @@
 import Foundation
 
 struct DateInfo {
+
+    let day: Int
+    let month: Int
+    let year: Int
+    let weekOfYear: Int
     
+    init(date: Date) {
+        let calendar = Calendar.current
+        day = calendar.component(.day, from: date)
+        month = calendar.component(.month, from: date)
+        year = calendar.component(.year, from: date)
+        weekOfYear = calendar.component(Calendar.Component.weekOfYear, from: date)
+    }
 }
