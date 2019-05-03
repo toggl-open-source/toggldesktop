f81badc548b706a180a8327d462d3fc2e349a5bf
Nghia Tran <vinhnghiatran@gmail.com>
2019-05-03 16:56:53 +0700

    Basic layout for Time Input Controller and TimeView (mac)

---
+++ src/ui/osx/TogglDesktop/Feature/Time/TimeInputView.swift
@@ -0,0 +1,19 @@
+//
+//  TimeInputView.swift
+//  TogglDesktop
+//
+//  Created by Nghia Tran on 5/3/19.
+//  Copyright © 2019 Alari. All rights reserved.
+//
+
+import Cocoa
+
+class TimeInputView: NSView {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
---
+++ src/ui/osx/TogglDesktop/Feature/Time/TimeInputViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TimeInputViewController.swift
+//  TogglDesktop
+//
+//  Created by Nghia Tran on 5/3/19.
+//  Copyright © 2019 Alari. All rights reserved.
+//
+
+import Cocoa
+
+final class TimeInputViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
