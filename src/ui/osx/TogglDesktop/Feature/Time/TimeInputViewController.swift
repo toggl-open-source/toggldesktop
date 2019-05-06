40646f2d58f79bf773a99b9495e9316b51b7babb
Nghia Tran <vinhnghiatran@gmail.com>
2019-05-06 14:25:05 +0700

    Setup layout for TimeInputController (mac)

--- src/ui/osx/TogglDesktop/Feature/Time/TimeInputViewController.swift
+++ src/ui/osx/TogglDesktop/Feature/Time/TimeInputViewController.swift
@@ -10,9 +10,66 @@ import Cocoa
 
 final class TimeInputViewController: NSViewController {
 
+    // MARK: OUTLET
+
+    @IBOutlet weak var durationContainerView: NSView!
+    @IBOutlet weak var timeStackView: NSStackView!
+    @IBOutlet weak var clockContainerView: NSView!
+    @IBOutlet weak var durationLbl: NSTextField!
+    
+    // MARK: Variables
+
+    private var isAppearing = false
+    private var timeEntry: TimeEntryViewItem! {
+        didSet {
+            if isAppearing {
+                layoutContent()
+            }
+        }
+
+    }
+    // MARK: View Cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do view setup here.
+
+        initCommon()
+        setupTimeInputs()
+        layoutContent()
     }
-    
+
+    override func viewDidAppear() {
+        super.viewDidAppear()
+        isAppearing = true
+    }
+
+    override func viewWillDisappear() {
+        super.viewWillDisappear()
+        isAppearing = false
+    }
+
+    private func layoutContent() {
+
+    }
+}
+
+// MARK: Private
+
+extension TimeInputViewController {
+
+    fileprivate func initCommon() {
+
+    }
+
+    fileprivate func setupTimeInputs() {
+
+        // Duration
+        let durationInputView = TimeInputView.xibView() as TimeInputView
+        durationInputView.translatesAutoresizingMaskIntoConstraints = false
+        durationInputView.addSubview(durationInputView)
+        durationInputView.topAnchor.constraint(equalTo: durationLbl.bottomAnchor, constant: 10).isActive = true
+        durationInputView.leftAnchor.constraint(equalTo: durationContainerView.leftAnchor, constant: 20).isActive = true
+
+        // Start
+    }
 }
