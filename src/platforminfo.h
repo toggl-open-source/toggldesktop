6961ffc37d4786a320e71df91ae37ee3f76e921a
Martin Bříza <m@rtinbriza.cz>
2019-05-29 18:03:11 +0200

    Add more detailed info about the display and build type to platforminfo (linux)

--- src/platforminfo.h
+++ src/platforminfo.h
@@ -1,19 +1,28 @@
 #ifndef PLATFORMINFO_H
 #define PLATFORMINFO_H
 
+#include <string>
 #include <sstream>
+#include <map>
 
 #if defined(__linux)
 
 void RetrieveOsDetails(std::stringstream &ss);
+std::map<std::string, std::string> RetrieveOsDetailsMap();
 
 #elif defined(_WIN32) || defined(WIN32)
 inline void RetrieveOsDetails(std::stringstream &ss) {
 
 }
+std::map<std::string, std::string> RetrieveOsDetailsMap() {
+    return {};
+}
 #elif defined(__APPLE__)
 inline void RetrieveOsDetails(std::stringstream &ss) {
 
+}
+std::map<std::string, std::string> RetrieveOsDetailsMap() {
+    return {};
 }
 #endif
 
