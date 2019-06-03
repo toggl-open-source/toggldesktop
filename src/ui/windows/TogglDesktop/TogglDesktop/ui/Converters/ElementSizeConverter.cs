b4a2004bc4c8a68e3024ec9838f4358b2fd474f5
Taras Petruk <taras.petruk@gmail.com>
2019-06-03 15:41:24 +0300

    Restrict max width of client name #2993 (win)

---
+++ src/ui/windows/TogglDesktop/TogglDesktop/ui/Converters/ElementSizeConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace TogglDesktop.Converters
+{
+    public class ElementSizeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var percentage = double.Parse(parameter.ToString());
+            return double.Parse(value.ToString()) * percentage;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
