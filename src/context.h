9850292d4a2552fd02b724a80433f673abb896e0
Martin Bříza <m@rtinbriza.cz>
2019-06-13 17:47:36 +0200

    Pass TogglClient by reference (lib)

--- src/context.h
+++ src/context.h
@@ -590,18 +590,20 @@ class TOGGL_INTERNAL_EXPORT Context : public TimelineDatasource {
     error pushChanges(
         TogglClient *https_client,
         bool *had_something_to_push);
-    error pushClients(const std::vector<Client *> &clients,
-                      const std::string &api_token,
-                      TogglClient toggl_client);
+    error pushClients(
+        const std::vector<Client *> &clients,
+        const std::string &api_token,
+        const TogglClient &toggl_client);
     error pushProjects(
         const std::vector<Project *> &projects,
         const std::vector<Client *> &clients,
         const std::string &api_token,
-        TogglClient toggl_client);
-    error pushEntries(const std::map<std::string, BaseModel *> &models,
-                      const std::vector<TimeEntry *> &time_entries,
-                      const std::string &api_token,
-                      TogglClient toggl_client);
+        const TogglClient &toggl_client);
+    error pushEntries(
+        const std::map<std::string, BaseModel *> &models,
+        const std::vector<TimeEntry *> &time_entries,
+        const std::string &api_token,
+        const TogglClient &toggl_client);
     error updateEntryProjects(
         const std::vector<Project *> &projects,
         const std::vector<TimeEntry *> &time_entries);
