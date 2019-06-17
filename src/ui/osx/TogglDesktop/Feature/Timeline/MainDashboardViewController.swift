adb06e854ec2bdef88f6e1a215e6bb34dd24df49
Nghia Tran <vinhnghiatran@gmail.com>
2019-06-17 15:13:09 +0700

    Basic layout for MainDashboardController (mac)

---
+++ src/ui/osx/TogglDesktop/Feature/Timeline/MainDashboardViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainDashboardViewController.swift
+//  TogglDesktop
+//
+//  Created by Nghia Tran on 6/17/19.
+//  Copyright © 2019 Alari. All rights reserved.
+//
+
+import Cocoa
+
+class MainDashboardViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
