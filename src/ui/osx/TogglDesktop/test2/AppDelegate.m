35338f35be548409b37dd5d1ef3116fd35a2c55f
IndrekV <indzuliin@gmail.com>
2019-07-15 07:51:55 +0300

    Bring Idle window in focus when shown and allow closing idle window with cmd+w (mac)

--- src/ui/osx/TogglDesktop/test2/AppDelegate.m
+++ src/ui/osx/TogglDesktop/test2/AppDelegate.m
@@ -1454,6 +1454,7 @@ const NSString *appName = @"osx_native_app";
 	NSAssert([NSThread isMainThread], @"Rendering stuff should happen on main thread");
 
 	[self.idleNotificationWindowController displayIdleEvent:idleEvent];
+	[NSApp activateIgnoringOtherApps:YES];
 }
 
 - (PLCrashReporter *)configuredCrashReporter
