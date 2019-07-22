24573e6857b42010a484156bf6d47ff4e9b55830
Nghia Tran <vinhnghiatran@gmail.com>
2019-07-22 20:32:28 +0700

    Remove DeepDiff 3rd-party library (mac)

--- third_party/DeepDiff/build/DeepDiff.build/Release/DeepDiff-macOS.build/DerivedSources/DeepDiff_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char DeepDiffVersionString[];
- extern const double DeepDiffVersionNumber;
-
- const unsigned char DeepDiffVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:DeepDiff  PROJECT:DeepDiff-1" "\n";
- const double DeepDiffVersionNumber __attribute__ ((used)) = (double)1.;
