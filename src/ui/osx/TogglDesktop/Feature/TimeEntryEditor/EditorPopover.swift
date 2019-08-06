8ded53cfefa9b07ccce1eac3425516bc9b9fd7f2
Nghia Tran <vinhnghiatran@gmail.com>
2019-08-06 14:06:09 +0700

    Fix couldn't close the Editor in Timeline (mac)

--- src/ui/osx/TogglDesktop/Feature/TimeEntryEditor/EditorPopover.swift
+++ src/ui/osx/TogglDesktop/Feature/TimeEntryEditor/EditorPopover.swift
@@ -26,6 +26,7 @@ final class EditorPopover: NoVibrantPopoverView {
 
     @objc func prepareViewController() {
         let editor = EditorViewController(nibName: NSNib.Name("EditorViewController"), bundle: nil)
+        editor.delegate = self
         editor.view.appearance = appearance
         let size = DesktopLibraryBridge.shared().getEditorWindowSize()
         editor.view.frame.size = size
@@ -47,5 +48,14 @@ final class EditorPopover: NoVibrantPopoverView {
 
     override func popoverDidResize() {
         DesktopLibraryBridge.shared().setEditorWindowSize(contentSize)
+    }
+}
+
+// MARK: EditorViewControllerDelegate
+
+extension EditorPopover: EditorViewControllerDelegate {
+
+    func editorShouldClose() {
+        close(focusTimer: false)
     }
 }
