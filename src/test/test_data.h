b3e791e7481a979640dbc032c6db57530a8a2e07
Taras Petruk <taras.petruk@gmail.com>
2019-08-12 16:08:41 +0300

    Fix incorrect test.db path string

--- src/test/test_data.h
+++ src/test/test_data.h
@@ -5,12 +5,6 @@
 
 #include <string>
 
-#if defined(WIN32) || defined(_WIN32)
-#define TESTDB L"test.db"
-#else
-#define TESTDB "test.db"
-#endif
-
 std::string loadTestData();
 std::string loadTestDataFile(const std::string filename);
 
