f2662660bde9df44c28a5464b22387d1fd57d156
Taras Petruk <taras.petruk@gmail.com>
2019-09-11 13:53:58 +0300

    Fix crash when showing notification (win)
    Fallback to Windows native notification in case custom notification fails
    Increase reminder notification timeout from 6 sec to 10 sec

---
+++ src/ui/windows/TogglDesktop/TogglDesktop/ui/NotificationExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+using Hardcodet.Wpf.TaskbarNotification;
+
+namespace TogglDesktop
+{
+    public static class NotificationExtensions
+    {
+        public static bool ShowNotification(this TaskbarIcon icon, UIElement element, PopupAnimation animation,
+            TimeSpan? timeout = null)
+        {
+            try
+            {
+                icon.ShowCustomBalloon(element, animation, (int?)timeout?.TotalMilliseconds);
+            }
+            catch (Exception e)
+            {
+                BugsnagService.NotifyBugsnag(new Exception("Failed to show a custom notification", e));
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
