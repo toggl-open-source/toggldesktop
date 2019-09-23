toggl-open-source/toggldesktop

commit 2c22dd1a39610742762372004024a13dda721330

Use UIModifiedAt and UpdatedAt comparison to detect unsynced items (lib), #3128

src/time_entry.cc
@@ -457,8 +457,8 @@ void TimeEntry::LoadFromJSON(Json::Value data) {
     SetBillable(data["billable"].asBool());
     SetDurOnly(data["duronly"].asBool());
     SetUpdatedAtString(data["at"].asString());
+    SetUIModifiedAt(Formatter::Parse8601(data["at"].asString()));
 
-    SetUIModifiedAt(0);
     ClearUnsynced();
 }
 

src/database.cc
@@ -1969,7 +1969,13 @@ error Database::loadTimeEntriesFromSQLStatement(
                 } else {
                     model->SetValidationError(rs[19].convert<std::string>());
                 }
-                model->ClearDirty();
+                if (model->UIModifiedAt() != model->UpdatedAt()) {
+                    model->SetUnsynced();
+                    model->SetDirty();
+                } else {
+                    model->ClearDirty();
+                }
+
                 list->push_back(model);
                 more = rs.moveNext();
             }

commit e6cb138d874500d96f6aa28180f636f2660dcfec

Set UIModifiedAt to 0 for synced items (lib)
Fixes issue with unsynced items counter on Windows app caused by 2c22dd1a39610742762372004024a13dda721330

src/time_entry.cc
@@ -457,7 +457,7 @@ void TimeEntry::LoadFromJSON(Json::Value data) {
     SetBillable(data["billable"].asBool());
     SetDurOnly(data["duronly"].asBool());
     SetUpdatedAtString(data["at"].asString());
-    SetUIModifiedAt(Formatter::Parse8601(data["at"].asString()));
+    SetUIModifiedAt(0);
 
     ClearUnsynced();
 }