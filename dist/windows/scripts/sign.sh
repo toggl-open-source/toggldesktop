4f66bdec7aebe1620433198f2b6527d46b1827a6
Martin Bříza <m@rtinbriza.cz>
2019-10-01 15:04:13 +0200

    Windows signing attempt

---
+++ dist/windows/scripts/sign.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [[ -z "$SIGNTOOL" ]]; then
+    SIGNTOOL="C:\Program Files (x86)\Windows Kits\10\bin\x64\signtool.exe"
+fi
+
+PATHS="src/ui/windows/TogglDesktop/TogglDesktop/bin/Release
+       src/ui/windows/TogglDesktop/TogglDesktop/bin/x64/Release"
+
+EXTENSIONS="exe dll"
+
+arguments=$(sed 's/\</\*./g' <<< $EXTENSIONS | sed 's/\s/ -or -name /g' | sed 's/^/-name /')
+
+for i in $(find $PATHS $arguments 2>/dev/null); do
+    echo "Signing $i"
+    $SIGNTOOL sign -a -t http://timestamp.verisign.com/scripts/timestamp.dll -f "Certificate.pfx" $i
+done
+
