9bbb05809e17ff717b3645b22c1f340951373f58
Martin Bříza <m@rtinbriza.cz>
2019-10-10 17:44:45 +0200

    Update the Linux package script so it's more configurable (actions)

--- dist/linux/package.sh
+++ dist/linux/package.sh
@@ -14,13 +14,40 @@ if [ ! -z "$TOGGL_VERSION" ]; then
     VERSION_DEFINE="-DTOGGL_VERSION=$TOGGL_VERSION"
 fi
 
+if [ -z "$PREFIX" ]; then
+    PREFIX="$PWD/package"
+fi
+if [ -z "$BINDIR" ]; then
+    BINDIR="$PREFIX/bin"
+fi
+if [ -z "$LIBDIR" ]; then
+    LIBDIR="$PREFIX/lib"
+fi
+if [ -z "$THIRDPARTYDIR" ]; then
+    THIRDPARTYDIR="$PREFIX/lib"
+fi
+if [ -z "$LIBEXECDIR" ]; then
+    LIBEXECDIR="$PREFIX/bin"
+fi
+if [ -z "$SCRIPTDIR" ]; then
+    SCRIPTDIR="$PREFIX"
+fi
+
+function relpath() {
+    if [ -f "$1" ]; then
+        realpath --relative-to="$(dirname $1)" "$2"
+    else
+        realpath --relative-to="$1" "$2"
+    fi
+}
+
 function build() {
     echo "=========== Will build in $fullbuilddir" >&2
 
     pushd $builddir >/dev/null
 
     echo "=========== Configuring" >&2
-    cmake -DTOGGL_PRODUCTION_BUILD=ON -DTOGGL_ALLOW_UPDATE_CHECK=ON $VERSION_DEFINE -DUSE_BUNDLED_LIBRARIES=ON -DCMAKE_INSTALL_PREFIX="$PWD/package" ..
+    cmake -DTOGGL_PRODUCTION_BUILD=ON -DTOGGL_ALLOW_UPDATE_CHECK=ON $VERSION_DEFINE -DUSE_BUNDLED_LIBRARIES=ON -DCMAKE_INSTALL_PREFIX="$PREFIX" -DTOGGL_INTERNAL_LIB_DIR="$THIRDPARTYDIR" ..
     echo "=========== Building..." >&2
     make -j4
     echo "=========== Installing" >&2
@@ -29,7 +56,9 @@ function build() {
 }
 
 function compose() {
-    pushd "$builddir/package" >/dev/null
+    pushd "$PREFIX" >/dev/null
+
+    mkdir -p "$BINDIR" "$LIBDIR" "$THIRDPARTYDIR" "$LIBEXECDIR" "$SCRIPTDIR"
 
     echo "=========== Composing the package" >&2
     rm -fr include lib/cmake
@@ -45,7 +74,7 @@ function compose() {
     qmake=$(find $libdir/../bin/ $libdir/../../bin/ -name qmake -or -name qmake-qt5 | head -n1)
     echo "qmake: " $qmake
 
-    cp -Lrfu $(ldd bin/TogglDesktop | grep -e libQt -e ssl -e crypto -e libicu -e double-conversion -e jpeg -e re2 -e avcodec -e avformat -e avutil -e webp | sed 's/.* => \(.*\)[(]0x.*/\1/') lib
+    cp -Lrfu $(ldd bin/TogglDesktop | grep -e libQt -e ssl -e crypto -e libicu -e double-conversion -e jpeg -e re2 -e avcodec -e avformat -e avutil -e webp | sed 's/.* => \(.*\)[(]0x.*/\1/') "$THIRDPARTYDIR"
     ls "$qmake" >/dev/null
 
     libexecdir=$($qmake -query QT_INSTALL_LIBEXECS)
@@ -53,10 +82,11 @@ function compose() {
     translationdir=$($qmake -query QT_INSTALL_TRANSLATIONS)
     datadir=$($qmake -query QT_INSTALL_DATA)
 
-    cp "$libexecdir/QtWebEngineProcess" bin
-    mkdir -p lib
+    mkdir -p "$LIBEXECDIR"
+    cp "$libexecdir/QtWebEngineProcess" "$LIBEXECDIR"
+    mkdir -p "$LIBDIR"
     for i in $PLUGINS; do
-        newpath=lib/qt5/plugins/$(dirname $i)/
+        newpath="$THIRDPARTYDIR/qt5/plugins/$(dirname $i)/"
         file=$(basename $i)
         mkdir -p $newpath
         cp -Lrfu $plugindir/$i $newpath
@@ -67,40 +97,45 @@ function compose() {
         echo "========"
         for j in $(ldd $newpath/$file | grep -e libQt -e ssl | sed 's/.* => \(.*\)[(]0x.*/\1/'); do
             if [ ! -f lib/$(basename "$j") ]; then
-                cp -vLrfu $j lib
+                cp -vLrfu $j "$THIRDPARTYDIR"
             fi
         done
     done
 
-    for i in $(ls lib/*.so); do
+    for i in $(ls "$THIRDPARTYDIR"/*.so); do
         for j in $(ldd $i | grep -e libQt | sed 's/.* => \(.*\)[(]0x.*/\1/'); do
             if [ ! -f lib/$(basename "$j") ]; then
-                cp -vLrfu $j lib
+                cp -vLrfu $j "$THIRDPARTYDIR"
             fi
         done
     done
 
-    patchelf --set-rpath '$ORIGIN/../lib/' bin/TogglDesktop
-    patchelf --set-rpath '$ORIGIN/../lib/' bin/QtWebEngineProcess
+    patchelf --set-rpath '$ORIGIN/'$(relpath "$BINDIR/TogglDesktop" "$THIRDPARTYDIR") "$BINDIR/TogglDesktop"
+    patchelf --set-rpath '$ORIGIN/'$(relpath "$LIBEXECDIR/QtWebEngineProcess" "$THIRDPARTYDIR") "$LIBEXECDIR/QtWebEngineProcess"
 
-    for i in $(ls lib/*.so*); do
+    for i in $(ls "$THIRDPARTYDIR"/*.so*); do
         patchelf --set-rpath '$ORIGIN' $i
     done
 
-    mkdir -p lib/qt5/translations lib/qt5/resources
-    cp -Lrfu "$translationdir/qtwebengine_locales" lib/qt5/translations
-    cp -Lrfu "$datadir/resources/"* lib/qt5/resources
+    mkdir -p "$THIRDPARTYDIR/qt5/translations" "$THIRDPARTYDIR/qt5/resources"
+    cp -Lrfu "$translationdir/qtwebengine_locales" "$THIRDPARTYDIR/qt5/translations"
+    cp -Lrfu "$datadir/resources/"* "$THIRDPARTYDIR/qt5/resources"
 
-    mv "bin/TogglDesktop.sh" "."
+    if [ "$SCRIPTDIR" != "$BINDIR" ]; then
+        mv -n "bin/TogglDesktop.sh" "$SCRIPTDIR/TogglDesktop.sh"
+    fi
 
-    cat <<EOF >bin/qt.conf
+    cat <<EOF >"$LIBEXECDIR/qt.conf"
     [Paths]
-    Prefix=..
-    Plugins=lib/qt5/plugins
-    Data=lib/qt5
-    Translations=lib/qt5/translations
+    Prefix=../lib
+    Plugins=qt5/plugins
+    Data=qt5
+    Translations=qt5/translations
 EOF
 
+}
+
+function package() {
     # echo "=========== Stripping" >&2
     # for i in bin/QtWebEngineProcess $(find . -name \*.so); do
     #     strip --strip-unneeded $i;
@@ -116,6 +151,7 @@ EOF
 if [[ "$#" -ne 1 ]]; then
     build
     compose
+    package
 else
     $1
 fi
