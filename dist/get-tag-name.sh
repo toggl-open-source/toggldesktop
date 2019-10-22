89537b350a98ba284d6dbc3fe5a1f41064a0908a
Martin Bříza <m@rtinbriza.cz>
2019-10-22 19:00:35 +0200

    Get the tag in a smarter way (actions)

--- dist/get-tag-name.sh
+++ dist/get-tag-name.sh
@@ -2,8 +2,4 @@
 
 set -e
 
-EVENT_TYPE=$(cat "$GITHUB_EVENT_PATH" | jq -r ".ref_type")
-if [[ "$EVENT_TYPE" != "tag" ]]; then exit 1; fi
-TAG_NAME=$(cat "$GITHUB_EVENT_PATH" | jq -r ".ref")
-
-echo $TAG_NAME
+echo ${GITHUB_REF/refs\/tags\//}
