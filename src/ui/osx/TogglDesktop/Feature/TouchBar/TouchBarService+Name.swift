565c9250a901f9a2cc5ea7661a4f3174e461a695
Nghia Tran <nghiatran@The-Storm.local>
2019-11-07 14:06:52 +0700

    Introduce IdleNotificationTouchBar (mac)

--- src/ui/osx/TogglDesktop/Feature/TouchBar/TouchBarService+Name.swift
+++ src/ui/osx/TogglDesktop/Feature/TouchBar/TouchBarService+Name.swift
@@ -11,6 +11,7 @@ import Foundation
 @available(OSX 10.12.2, *)
 extension NSTouchBar.CustomizationIdentifier {
     static let mainTouchBar = NSTouchBar.CustomizationIdentifier("com.toggl.toggldesktop.maintouchbar")
+    static let idleNotificationTouchBar = NSTouchBar.CustomizationIdentifier("com.toggl.toggldesktop.idleNotificationTouchBar")
 }
 
 @available(OSX 10.12.2, *)
@@ -19,6 +20,9 @@ extension NSTouchBarItem.Identifier {
     static let timeEntryItem = NSTouchBarItem.Identifier("com.toggl.toggldesktop.timeentrytouchbar.timeentryitems")
     static let runningTimeEntry = NSTouchBarItem.Identifier("com.toggl.toggldesktop.timeentrytouchbar.runningtimeentry")
     static let startStopItem = NSTouchBarItem.Identifier("com.toggl.toggldesktop.timeentrytouchbar.startstopbutton")
-    static let cancelItem = NSTouchBarItem.Identifier("com.toggl.toggldesktop.timeentrytouchbar.cancelactionbutton")
-    static let deleteItem = NSTouchBarItem.Identifier("com.toggl.toggldesktop.timeentrytouchbar.deletetimeentrybutton")
+
+    static let discardIdleItem = NSTouchBarItem.Identifier("com.toggl.toggldesktop.timeentrytouchbar.discardIdleItem")
+    static let discardIdleAndContinueItem = NSTouchBarItem.Identifier("com.toggl.toggldesktop.timeentrytouchbar.discardIdleAndContinueItem")
+    static let keepIdleTimeItem = NSTouchBarItem.Identifier("com.toggl.toggldesktop.timeentrytouchbar.keepIdleTimeItem")
+    static let addIdleTimeItem = NSTouchBarItem.Identifier("com.toggl.toggldesktop.timeentrytouchbar.addIdleTimeItem")
 }
