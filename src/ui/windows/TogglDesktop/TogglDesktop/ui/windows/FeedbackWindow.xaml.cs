b0e05e58f4fc2a01db3696903ed9d6947d9d63d4
Taras Petruk <taras.petruk@gmail.com>
2019-11-13 13:40:39 +0200

    Implement Feedback window UI (win)
    Implement Feedback window UI according to mockups
    Implement immediate attached file size validation
    Implement field validation logic (validate on Send click and LostFocus, switch focus to field when validation error occurs)
    First step towards MVVM and RFP: created a FeedbackViewModel, started using ReactiveUI

--- src/ui/windows/TogglDesktop/TogglDesktop/ui/windows/FeedbackWindow.xaml.cs
+++ src/ui/windows/TogglDesktop/TogglDesktop/ui/windows/FeedbackWindow.xaml.cs
@@ -1,83 +1,72 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
-using Microsoft.Win32;
+using System.Windows.Input;
+using TogglDesktop.ViewModels;
 
 namespace TogglDesktop
 {
     public partial class FeedbackWindow
     {
-        private string attachedFileName;
+        public FeedbackWindowViewModel ViewModel
+        {
+            get => (FeedbackWindowViewModel)DataContext;
+            private set => DataContext = value;
+        }
 
         public FeedbackWindow()
         {
             this.InitializeComponent();
             this.Closing += ResetOnClosing;
-            this.reset();
+            this.ResetState();
         }
 
         private void ResetOnClosing(object sender, CancelEventArgs e)
         {
-            this.reset();
+            this.ResetState();
         }
 
-        private void reset()
+        private void ResetState()
         {
-            this.topicComboBox.SelectedItem = null;
-            this.messageTextBox.Text = "";
-            this.attachedFileText.Text = "(maximum image size 5MB)";
-            this.errorText.Visibility = Visibility.Hidden;
-            this.attachedFileName = null;
+            ViewModel = new FeedbackWindowViewModel(RefreshValidationBindings, OnFeedbackSent);
         }
 
-        private void onTopicSelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void OnCancelClick(object sender, RoutedEventArgs e)
         {
-            this.errorText.Visibility = Visibility.Hidden;
-        }
-
-        private void onAttachImageClick(object sender, RoutedEventArgs e)
-        {
-            var dialog = new OpenFileDialog();
-            if ((bool)dialog.ShowDialog(this))
-            {
-                this.attachedFileName = dialog.FileName;
-                this.attachedFileText.Text = "Attached: " + dialog.SafeFileName;
-            }
-        }
-
-        private void onSendClick(object sender, RoutedEventArgs e)
-        {
-            if (this.topicComboBox.SelectedItem == null)
-            {
-                this.errorText.Text = "Please choose a topic!";
-                this.errorText.Visibility = Visibility.Visible;
-
-                this.topicComboBox.Focus();
-                return;
-            }
-
-            if (this.messageTextBox.Text == "")
-            {
-                this.errorText.Text = "Please type in your feedback!";
-                this.errorText.Visibility = Visibility.Visible;
-                this.messageTextBox.Focus();
-                return;
-            }
-
-            if (!Toggl.SendFeedback(
-                this.topicComboBox.Text,
-                this.messageTextBox.Text,
-                this.attachedFileName))
-            {
-                this.errorText.Text = "File upload failed! (check file size)";
-                this.errorText.Visibility = Visibility.Visible;
-                return;
-            }
-
-            MessageBox.Show(this, "Your feedback was sent successfully.", "Thank you!");
-            
-            this.reset();
+            this.ResetState();
             this.Hide();
         }
+
+        private void RefreshValidationBindings()
+        {
+            RefreshTopicComboBoxBinding();
+            RefreshFeedbackTextBoxBinding();
+        }
+
+        private void OnFeedbackSent()
+        {
+            MessageBox.Show(this, "Our support team will be notified.", "Feedback sent successfully!");
+            this.ResetState();
+            this.Hide();
+        }
+
+        private void OnFeedbackTextBoxValidationError(object sender, ValidationErrorEventArgs e)
+        {
+            if (!Equals(Keyboard.FocusedElement, topicComboBox))
+            {
+                feedbackTextBox.Dispatcher.BeginInvoke(new Action(() => Keyboard.Focus(feedbackTextBox)), System.Windows.Threading.DispatcherPriority.Background);
+            }
+        }
+
+        private void OnTopicComboBoxValidationError(object sender, ValidationErrorEventArgs e)
+        {
+            topicComboBox.Dispatcher.BeginInvoke(new Action(() => Keyboard.Focus(topicComboBox)), System.Windows.Threading.DispatcherPriority.Background);
+        }
+
+        private void OnTopicComboBoxLostFocus(object sender, RoutedEventArgs e) => RefreshTopicComboBoxBinding();
+        private void OnFeedbackTextBoxLostFocus(object sender, RoutedEventArgs e) => RefreshFeedbackTextBoxBinding();
+        private void RefreshFeedbackTextBoxBinding() => feedbackTextBox.GetBindingExpression(TextBox.TextProperty).UpdateSource();
+        private void RefreshTopicComboBoxBinding() => topicComboBox.GetBindingExpression(ComboBox.SelectedItemProperty).UpdateSource();
     }
 }
