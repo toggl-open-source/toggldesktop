da3bd5885edb48d795f72b0ca32aef72cc6073eb
Taras Petruk <taras.petruk@gmail.com>
2019-11-14 10:17:07 +0200

    Code style fixes in BytesToStringConverter.cs (win)

--- src/ui/windows/TogglDesktop/TogglDesktop/ui/Converters/BytesToStringConverter.cs
+++ src/ui/windows/TogglDesktop/TogglDesktop/ui/Converters/BytesToStringConverter.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Globalization;
 using System.Windows.Data;
+using static System.Convert;
+using static System.Math;
 
 namespace TogglDesktop.Converters
 {
@@ -17,11 +19,13 @@ namespace TogglDesktop.Converters
         {
             string[] suf = { "B", "KB", "MB", "GB", "TB", "PB", "EB" };
             if (byteCount == 0)
-                return "0" + suf[0];
-            var bytes = Math.Abs(byteCount);
-            var place = System.Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024)));
-            var num = Math.Round(bytes / Math.Pow(1024, place), 1);
-            return (Math.Sign(byteCount) * num).ToString() + " " + suf[place];
+            {
+                return $"0 {suf[0]}";
+            }
+            var bytes = Abs(byteCount);
+            var place = ToInt32(Floor(Log(bytes, 1024)));
+            var num = Round(bytes / Pow(1024, place), 1);
+            return $"{(Sign(byteCount) * num)} {suf[place]}";
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
