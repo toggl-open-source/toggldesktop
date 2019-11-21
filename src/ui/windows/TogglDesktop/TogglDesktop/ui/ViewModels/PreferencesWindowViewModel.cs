61a62122629d194eaf9f8de41546c27370554c47
Taras Petruk <taras.petruk@gmail.com>
2019-11-21 09:05:00 +0200

    Implement proper HotKeyBox validation logic (win)

--- src/ui/windows/TogglDesktop/TogglDesktop/ui/ViewModels/PreferencesWindowViewModel.cs
+++ src/ui/windows/TogglDesktop/TogglDesktop/ui/ViewModels/PreferencesWindowViewModel.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Reactive.Linq;
 using System.Windows.Input;
 using MahApps.Metro.Controls;
 using ReactiveUI;
@@ -10,7 +11,7 @@ namespace TogglDesktop.ViewModels
 {
     public class PreferencesWindowViewModel : ReactiveValidationObject<PreferencesWindowViewModel>
     {
-        private static readonly Dictionary<HotKey, string> KnownShortcuts =
+        private readonly Dictionary<HotKey, string> _knownShortcuts =
             new Dictionary<HotKey, string>
             {
                 { new HotKey(Key.N, ModifierKeys.Control), "New time entry" },
@@ -23,21 +24,34 @@ namespace TogglDesktop.ViewModels
                 { new HotKey(Key.V, ModifierKeys.Control), "New time entry from clipboard" },
             };
 
+        private const string ShowHideTogglDescription = "Show/Hide Toggl";
+        private const string ContinueStopTimerDescription = "Continue/Stop Timer";
+
         private HotKey _showHideToggl;
         private HotKey _continueStopTimer;
         private HotKey _continueStopTimerSaved;
         private HotKey _showHideTogglSaved;
 
+        private readonly ValidationHelper _showHideTogglValidation;
+        private readonly ValidationHelper _continueStopTimerValidation;
+
         public PreferencesWindowViewModel()
         {
-            this.ValidationRule(
+            this.WhenAnyValue(x => x.ShowHideToggl)
+                .Buffer(2, 1)
+                .Subscribe(b => UpdateKnownShortcuts(b[0], b[1], ShowHideTogglDescription));
+            this.WhenAnyValue(x => x.ContinueStopTimer)
+                .Buffer(2, 1)
+                .Subscribe(b => UpdateKnownShortcuts(b[0], b[1], ContinueStopTimerDescription));
+
+            _showHideTogglValidation = this.ValidationRule(
                 vm => vm.ShowHideToggl,
-                hotKey => hotKey == null || (!KnownShortcuts.ContainsKey(hotKey)),
-                hotKey => $"This shortcut is already taken by {KnownShortcuts[hotKey]}");
-            this.ValidationRule(
+                hotKey => IsHotKeyValid(hotKey, ShowHideTogglDescription),
+                hotKey => $"This shortcut is already taken by {_knownShortcuts[hotKey]}");
+            _continueStopTimerValidation = this.ValidationRule(
                 vm => vm.ContinueStopTimer,
-                hotKey => hotKey == null || (!KnownShortcuts.ContainsKey(hotKey)),
-                hotKey => $"This shortcut is already taken by {KnownShortcuts[hotKey]}");
+                hotKey => IsHotKeyValid(hotKey, ContinueStopTimerDescription),
+                hotKey => $"This shortcut is already taken by {_knownShortcuts[hotKey]}");
         }
 
         public HotKey ShowHideToggl
@@ -70,6 +84,29 @@ namespace TogglDesktop.ViewModels
             ShowHideToggl = _showHideTogglSaved;
             _continueStopTimerSaved = LoadHotKey(Toggl.GetKeyStart, Toggl.GetKeyModifierStart);
             ContinueStopTimer = _continueStopTimerSaved;
+        }
+
+        public void ResetPropsWithErrorsToPreviousValues()
+        {
+            if (!_showHideTogglValidation.IsValid) ShowHideToggl = _showHideTogglSaved;
+            if (!_continueStopTimerValidation.IsValid) ContinueStopTimer = _continueStopTimerSaved;
+        }
+
+        private bool IsHotKeyValid(HotKey hotKey, string hotKeyDescription)
+        {
+            return hotKey.IsNullOrNone() || _knownShortcuts[hotKey] == hotKeyDescription;
+        }
+
+        private void UpdateKnownShortcuts(HotKey previousValue, HotKey newValue, string hotKeyDescription)
+        {
+            if (!previousValue.IsNullOrNone()) _knownShortcuts.Remove(previousValue);
+            if (!newValue.IsNullOrNone())
+            {
+                if (!_knownShortcuts.ContainsKey(newValue))
+                {
+                    _knownShortcuts.Add(newValue, hotKeyDescription);
+                }
+            }
         }
 
         private static HotKey LoadHotKey(Func<Key> getKey, Func<ModifierKeys> getModifiers)
