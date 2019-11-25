b8162cebd0883ba9a86075338c80f495ae6f8d2c
Taras Petruk <taras.petruk@gmail.com>
2019-11-25 21:42:37 +0200

    Autocompletion popup styles - item templates (win)

---
+++ src/ui/windows/TogglDesktop/TogglDesktop/ui/Converters/ColorsToBrushConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace TogglDesktop.Converters
+{
+    class ColorsToBrushConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            foreach (var val in values)
+            {
+                switch (val)
+                {
+                    case Brush brush:
+                        return brush;
+                    case string str when !string.IsNullOrEmpty(str):
+                        return (SolidColorBrush)(new BrushConverter().ConvertFrom(str));
+                }
+            }
+
+            return null;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
