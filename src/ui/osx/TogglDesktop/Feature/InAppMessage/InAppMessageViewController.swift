cbdbd36d475046e63b6cfdd4f330bb269402b117
Nghia Tran <vinhnghiatran@gmail.com>
2019-12-03 20:25:33 +0700

    Basic layout for InAppMessage (mac)

---
+++ src/ui/osx/TogglDesktop/Feature/InAppMessage/InAppMessageViewController.swift
@@ -0,0 +1,20 @@
+//
+//  InAppMessageViewController.swift
+//  TogglDesktop
+//
+//  Created by Nghia Tran on 12/3/19.
+//  Copyright © 2019 Alari. All rights reserved.
+//
+
+import Cocoa
+
+final class InAppMessageViewController: NSViewController {
+
+    // MARK: Variables
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
