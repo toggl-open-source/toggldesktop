ea1edd66128380eb2e0d76af360a41eff9c86c64
Martin Bříza <m@rtinbriza.cz>
2019-12-04 14:42:44 +0100

    Reintroduce main window menu, connect some actions (linux)

--- src/ui/linux/TogglDesktop/mainwindow.h
+++ src/ui/linux/TogglDesktop/mainwindow.h
@@ -15,6 +15,22 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_actionAbout_trigggered();
+    void on_actionHelp_triggered();
+    void on_actionUpdateCheck_triggered();
+    void on_actionPreferences_triggered();
+    void on_actionQuit_triggered();
+
+    void on_actionNew_triggered();
+    void on_actionContinue_triggered();
+    void on_actionStop_triggered();
+    void on_actionShow_triggered();
+    void on_actionEdit_triggered();
+
+    void on_actionSync_triggered();
+    void on_actionClearCache_triggered();
+    void on_actionLogout_triggered();
 private:
     Ui::MainWindow *ui;
 };
