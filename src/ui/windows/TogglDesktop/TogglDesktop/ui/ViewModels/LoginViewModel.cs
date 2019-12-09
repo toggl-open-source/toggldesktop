5b23f0c3e834353caf291a7055e2d518c42131e8
Taras Petruk <taras.petruk@gmail.com>
2019-12-09 18:21:12 +0200

    Introducing LoginViewModel (part 2) (win)
    Implement validation logic in viewmodel

--- src/ui/windows/TogglDesktop/TogglDesktop/ui/ViewModels/LoginViewModel.cs
+++ src/ui/windows/TogglDesktop/TogglDesktop/ui/ViewModels/LoginViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Reactive;
 using System.Reactive.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -13,18 +14,33 @@ using Google.Apis.Http;
 using Google.Apis.Oauth2.v2;
 using Google.Apis.Util;
 using ReactiveUI;
+using ReactiveUI.Validation.Extensions;
+using ReactiveUI.Validation.Helpers;
 
 namespace TogglDesktop.ViewModels
 {
-    public class LoginViewModel : ReactiveObject
+    public class LoginViewModel : ReactiveValidationObject<LoginViewModel>
     {
         private IList<CountryViewModel> _countries;
         private CountryViewModel _selectedCountry;
         private ConfirmAction _confirmAction;
+        private string _email;
+        private string _password;
+        private bool _isEmailFocused;
+        private bool _isPasswordFocused;
+        private bool _isCountrySelectionFocused;
+        private bool _isTosCheckboxFocused;
+        private bool _isTosChecked;
+        private readonly ValidationHelper _emailValidation;
+        private readonly ValidationHelper _passwordValidation;
+        private readonly ValidationHelper _selectedCountryValidation;
+        private readonly ValidationHelper _isTosCheckedValidation;
         private readonly ObservableAsPropertyHelper<string> _confirmButtonText;
         private readonly ObservableAsPropertyHelper<string> _googleLoginButtonText;
-        private readonly ObservableAsPropertyHelper<string> _signuplLoginToggleText;
+        private readonly ObservableAsPropertyHelper<string> _signupLoginToggleText;
         private HttpClientFactory _httpClientFactory;
+        private readonly ObservableAsPropertyHelper<bool> _isLoading;
+        private readonly ObservableAsPropertyHelper<bool> _isViewDisabled;
 
         public LoginViewModel()
         {
@@ -38,8 +54,38 @@ namespace TogglDesktop.ViewModels
                 .ToProperty(this, x => x.GoogleLoginButtonText, out _googleLoginButtonText);
             this.WhenAnyValue(x => x.SelectedConfirmAction,
                     x => x == ConfirmAction.LogIn ? "Sign up for free" : "Back to Log in")
-                .ToProperty(this, x => x.SignupLoginToggleText, out _signuplLoginToggleText);
+                .ToProperty(this, x => x.SignupLoginToggleText, out _signupLoginToggleText);
+            ConfirmLoginSignupCommand = ReactiveCommand.CreateFromTask(ConfirmLoginSignupAsync);
+            ConfirmGoogleLoginSignupCommand = ReactiveCommand.CreateFromTask(ConfirmGoogleLoginSignupAsync);
+            IsLoginSignupExecuting = ConfirmLoginSignupCommand.IsExecuting
+                .CombineLatest(ConfirmGoogleLoginSignupCommand.IsExecuting,
+                    (isExecuting1, isExecuting2) => isExecuting1 || isExecuting2);
+            IsLoginSignupExecuting
+                .ToProperty(this, x => x.IsLoading, out _isLoading);
+            IsLoginSignupExecuting
+                .Select(x => !x)
+                .ToProperty(this, x => x.IsViewEnabled, out _isViewDisabled);
+
+            _emailValidation = this.ValidationRule(
+                x => x.Email,
+                email => !string.IsNullOrEmpty(email),
+                "Please enter a valid email");
+            _passwordValidation = this.ValidationRule(
+                x => x.Password,
+                password => !string.IsNullOrEmpty(password),
+                "A password is required");
+            _selectedCountryValidation = this.ValidationRule(
+                x => x.SelectedCountry,
+                selectedCountry => selectedCountry != null,
+                "Please select country");
+            _isTosCheckedValidation = this.ValidationRule(
+                x => x.IsTosChecked,
+                isTosChecked => isTosChecked,
+                "Please accept the terms");
         }
+        public ReactiveCommand<Unit, bool> ConfirmLoginSignupCommand { get; }
+        public ReactiveCommand<Unit, Unit> ConfirmGoogleLoginSignupCommand { get; }
+        public IObservable<bool> IsLoginSignupExecuting { get; }
 
         public IList<CountryViewModel> Countries
         {
@@ -59,9 +105,126 @@ namespace TogglDesktop.ViewModels
             set => this.RaiseAndSetIfChanged(ref _confirmAction, value);
         }
 
+        public string Email
+        {
+            get => _email;
+            set => this.RaiseAndSetIfChanged(ref _email, value);
+        }
+
+        public string Password
+        {
+            get => _password;
+            set => this.RaiseAndSetIfChanged(ref _password, value);
+        }
+        public bool IsEmailFocused
+        {
+            get => _isEmailFocused;
+            set => this.RaiseAndSetIfChanged(ref _isEmailFocused, value);
+        }
+
+        public bool IsPasswordFocused
+        {
+            get => _isPasswordFocused;
+            set => this.RaiseAndSetIfChanged(ref _isPasswordFocused, value);
+        }
+
+        public bool IsCountrySelectionFocused
+        {
+            get => _isCountrySelectionFocused;
+            set => this.RaiseAndSetIfChanged(ref _isCountrySelectionFocused, value);
+        }
+
+        public bool IsTosCheckboxFocused
+        {
+            get => _isTosCheckboxFocused;
+            set => this.RaiseAndSetIfChanged(ref _isTosCheckboxFocused, value);
+        }
+
+        public bool IsTosChecked
+        {
+            get => _isTosChecked;
+            set => this.RaiseAndSetIfChanged(ref _isTosChecked, value);
+        }
+
         public string ConfirmButtonText => _confirmButtonText.Value;
         public string GoogleLoginButtonText => _googleLoginButtonText.Value;
-        public string SignupLoginToggleText => _signuplLoginToggleText.Value;
+        public string SignupLoginToggleText => _signupLoginToggleText.Value;
+        public bool IsLoading => _isLoading.Value;
+        public bool IsViewEnabled => _isViewDisabled.Value;
+
+        private bool PerformValidation(bool isGoogleLogin = false)
+        {
+            IsEmailFocused = false;
+            IsPasswordFocused = false;
+            IsCountrySelectionFocused = false;
+            IsTosCheckboxFocused = false;
+
+            if (!isGoogleLogin && !_emailValidation.IsValid)
+            {
+                IsEmailFocused = true;
+            }
+            else if (!isGoogleLogin && !_passwordValidation.IsValid)
+            {
+                IsPasswordFocused = true;
+            }
+            else if (SelectedConfirmAction == ConfirmAction.SignUp && !_selectedCountryValidation.IsValid)
+            {
+                IsCountrySelectionFocused = true;
+            }
+            else if (SelectedConfirmAction == ConfirmAction.SignUp && !_isTosCheckedValidation.IsValid)
+            {
+                IsTosCheckboxFocused = true;
+            }
+            else
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private async Task<bool> ConfirmLoginSignupAsync()
+        {
+            if (!PerformValidation())
+            {
+                return false;
+            }
+
+            var success = false;
+            switch (SelectedConfirmAction)
+            {
+                case ConfirmAction.LogIn:
+                    success = await ConfirmAsync(Toggl.Login);
+                    break;
+                case ConfirmAction.SignUp:
+                    success = await ConfirmAsync(Toggl.Signup);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            return success;
+        }
+
+        private async Task ConfirmGoogleLoginSignupAsync()
+        {
+            if (!PerformValidation(isGoogleLogin: true))
+            {
+                return;
+            }
+
+            switch (SelectedConfirmAction)
+            {
+                case ConfirmAction.LogIn:
+                    await GoogleLoginAsync();
+                    break;
+                case ConfirmAction.SignUp:
+                    await GoogleSignupAsync();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
 
         private void OnDisplayCountries(List<Toggl.TogglCountryView> list)
         {
@@ -74,12 +237,12 @@ namespace TogglDesktop.ViewModels
             _httpClientFactory = HttpClientFactoryFromProxySettings(settings);
         }
 
-        public async void GoogleSignup()
+        public async Task GoogleSignupAsync()
         {
             await GoogleAuth(accessToken => Toggl.GoogleSignup(accessToken, SelectedCountry?.ID ?? default), "Signup");
         }
 
-        public async void GoogleLogin()
+        public async Task GoogleLoginAsync()
         {
             await GoogleAuth(accessToken => Toggl.GoogleLogin(accessToken), "Login");
         }
@@ -134,6 +297,14 @@ namespace TogglDesktop.ViewModels
             }
 
             return credential;
+        }
+
+        private async Task<bool> ConfirmAsync(Func<string, string, long, bool> confirmAction)
+        {
+            var email = Email;
+            var password = Password;
+            var selectedCountryId = SelectedCountry?.ID ?? -1;
+            return await Task.Run(() => confirmAction(email, password, selectedCountryId));
         }
 
         private static HttpClientFactory HttpClientFactoryFromProxySettings(Toggl.TogglSettingsView settings)
@@ -194,7 +365,6 @@ namespace TogglDesktop.ViewModels
 
     public enum ConfirmAction
     {
-        Unknown = 0,
         LogIn,
         SignUp
     }
