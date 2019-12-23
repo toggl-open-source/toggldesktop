dbb639d0d4d3c6b535af0dfe890dadd2d39a2506
Nghia Tran <vinhnghiatran@gmail.com>
2019-12-23 15:46:15 +0700

    Replace deprecated api with modern apis (lib)

--- src/lib/osx/Kopsik/MacOSApplicationInfo.mm
+++ src/lib/osx/Kopsik/MacOSApplicationInfo.mm
@@ -13,42 +13,47 @@
 #include <iostream>
 
 static const int kTitleBufferSize = 255;
-static const int kFilenameBufferSize = 255;
 
-int macOSGetWindowInfo(
-    std::string *title,
-    std::string *filename,
-    bool *idle) {
-    *title = "";
-    *filename = "";
-    *idle = false;
+@interface MacOSApplicationUtility : NSObject
 
-    // get pid
-    NSRunningApplication *frontApp;
++ (NSRunningApplication *) getFrontApplication;
+@end
+
+@implementation MacOSApplicationUtility
+
++ (NSRunningApplication *) getFrontApplication
+{
     NSArray<NSRunningApplication *> *runningApps = [[NSWorkspace sharedWorkspace] runningApplications];
     for (NSRunningApplication *app in runningApps) {
         if (app.isActive) {
-            frontApp = app;
-            break;
+            return app;
         }
     }
+    return nil;
+}
 
+@end
+
+int macOSGetActiveWindowInfo(std::string *title, std::string *filename) {
+    *title = "";
+    *filename = "";
+
+    // Get front app
+    NSRunningApplication *frontApp = [MacOSApplicationUtility getFrontApplication];
     if (frontApp == nil) {
         return -1;
     }
 
     // window title
-    CFArrayRef windows = CGWindowListCopyWindowInfo(
-        kCGWindowListOptionOnScreenOnly, kCGNullWindowID);
+    CFArrayRef windows = CGWindowListCopyWindowInfo(kCGWindowListOptionOnScreenOnly, kCGNullWindowID);
     for (int i = 0; i < CFArrayGetCount(windows); i++) {
         CFDictionaryRef dict = (CFDictionaryRef)CFArrayGetValueAtIndex(
             windows, i);
 
-        CFNumberRef window_layer = (CFNumberRef)CFDictionaryGetValue(
-            dict, kCGWindowLayer);
+        // Get window layer and skip the background
+        CFNumberRef window_layer = (CFNumberRef)CFDictionaryGetValue(dict, kCGWindowLayer);
         int window_layer_int;
         CFNumberGetValue(window_layer, kCFNumberIntType, &window_layer_int);
-
         if (window_layer_int != 0) {
             continue;
         }
@@ -59,27 +64,17 @@ int macOSGetWindowInfo(
         CFNumberGetValue(window_owner_pid, kCFNumberIntType,
                          &window_owner_pid_int);
 
-        ProcessSerialNumber owner_serial_number;
-        GetProcessForPID(window_owner_pid_int, &owner_serial_number);
-
-        Boolean are_same;
-        OSStatus err = SameProcess(
-            &front_process_serial_number, &owner_serial_number, &are_same);
-        if (err) {
-            continue;
-        }
-
+        // Get app with pid
         NSRunningApplication *app = [NSRunningApplication runningApplicationWithProcessIdentifier:window_owner_pid_int];
         if (app == nil) {
             continue;
         }
+
+        // Compare if it's the same process
+        // We don't compare pid, we use -isEqual
+        // Ref: https://developer.apple.com/documentation/appkit/nsrunningapplication/1526998-processidentifier
         if ([app isEqual:frontApp]) {
-
-        }
-
-        if (TRUE == are_same) {
-            CFStringRef window_name = (CFStringRef)CFDictionaryGetValue(
-                dict, kCGWindowName);
+            CFStringRef window_name = (CFStringRef)CFDictionaryGetValue(dict, kCGWindowName);
             if (window_name) {
                 char title_buffer[kTitleBufferSize];
                 CFStringGetCString(window_name, title_buffer,
@@ -96,16 +91,11 @@ int macOSGetWindowInfo(
     CFRelease(windows);
 
     // get application filename
-    CFStringRef processName = NULL;
-    err = CopyProcessName(&front_process_serial_number, &processName);
-    if (err) {
-        return err;
+    NSString *appName = frontApp.localizedName;
+    if (appName == nil) {
+        return -1;
     }
-    char filename_buffer[kFilenameBufferSize];
-    CFStringGetCString(processName, filename_buffer,
-                       kFilenameBufferSize, kCFStringEncodingUTF8);
-    CFRelease(processName);
-    *filename = std::string(filename_buffer);
+    *filename = std::string(appName.UTF8String);
 
     return 0;
 }
