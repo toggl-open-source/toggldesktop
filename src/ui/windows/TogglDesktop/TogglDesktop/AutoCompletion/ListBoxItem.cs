f42552b0943fa78661d5bcfc378d8a5bb037be78
Taras Petruk <taras.petruk@gmail.com>
2020-01-03 19:24:59 +0200

    remove unused using statements (win)

--- src/ui/windows/TogglDesktop/TogglDesktop/AutoCompletion/ListBoxItem.cs
+++ src/ui/windows/TogglDesktop/TogglDesktop/AutoCompletion/ListBoxItem.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Globalization;
-
-namespace TogglDesktop.AutoCompletion
+﻿namespace TogglDesktop.AutoCompletion
 {
     class ListBoxItem
     {
