e90b52ba2288a59f0a9d1511cad5c6800772afc4
Nghia Tran <vinhnghiatran@gmail.com>
2020-01-03 17:10:10 +0700

    Basic logic for the info icon (mac)

---
+++ src/ui/osx/TogglDesktop/Feature/Timeline/TimelineActivityRecorderViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TimelineActivityRecorderViewController.swift
+//  TogglDesktop
+//
+//  Created by Nghia Tran on 1/3/20.
+//  Copyright © 2020 Alari. All rights reserved.
+//
+
+import Cocoa
+
+class TimelineActivityRecorderViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
