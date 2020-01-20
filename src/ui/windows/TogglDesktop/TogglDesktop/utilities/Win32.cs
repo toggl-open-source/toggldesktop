41136f1d83e3b23aa4aab62ac0af4164c015b095
Taras Petruk <taras.petruk@gmail.com>
2020-01-20 18:16:55 +0200

    Get rid of unused drag&drop code (win)

--- src/ui/windows/TogglDesktop/TogglDesktop/utilities/Win32.cs
+++ src/ui/windows/TogglDesktop/TogglDesktop/utilities/Win32.cs
@@ -40,32 +40,6 @@ static class Win32
     [return: MarshalAs(UnmanagedType.Bool)]
     public static extern bool GetWindowRect(IntPtr hWnd, out Rectangle lpRect);
 
-    
-    [DllImport("user32.dll")]
-    [return: MarshalAs(UnmanagedType.Bool)]
-    public static extern bool GetCursorPos(ref Win32Point pt);
-
-    [StructLayout(LayoutKind.Sequential)]
-    public struct Win32Point
-    {
-        public Int32 X;
-        public Int32 Y;
-    }
-    const int WS_EX_TRANSPARENT = 0x00000020;
-    const int GWL_EXSTYLE = (-20);
-
-    [DllImport("user32.dll")]
-    static extern int GetWindowLong(IntPtr hwnd, int index);
-
-    [DllImport("user32.dll")]
-    static extern int SetWindowLong(IntPtr hwnd, int index, int newStyle);
-
-    public static void SetWindowExTransparent(IntPtr hwnd)
-    {
-        var extendedStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
-        SetWindowLong(hwnd, GWL_EXSTYLE, extendedStyle | WS_EX_TRANSPARENT);
-    }
-
     internal enum AccentState
     {
         ACCENT_DISABLED = 1,
