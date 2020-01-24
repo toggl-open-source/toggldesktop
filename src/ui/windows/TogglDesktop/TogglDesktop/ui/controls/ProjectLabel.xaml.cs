e4e7be3d25799794e3e4e3cd14e8e2d9e7373bf8
Taras Petruk <taras.petruk@gmail.com>
2020-01-24 18:52:58 +0200

    Create ProjectViewModel for ProjectLabel (win)

--- src/ui/windows/TogglDesktop/TogglDesktop/ui/controls/ProjectLabel.xaml.cs
+++ src/ui/windows/TogglDesktop/TogglDesktop/ui/controls/ProjectLabel.xaml.cs
@@ -1,77 +1,44 @@
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media;
+using TogglDesktop.ViewModels;
 
 namespace TogglDesktop
 {
     public partial class ProjectLabel : UserControl
     {
+        public ProjectViewModel ViewModel
+        {
+            get => (ProjectViewModel)DataContext;
+            set => DataContext = value;
+        }
+
         public ProjectLabel()
         {
             InitializeComponent();
+            ViewModel = new ProjectViewModel();
         }
-
-        public bool ShowAddProjectCaption { get; set; }
-
-        public static readonly DependencyProperty ColorProperty = DependencyProperty.Register(
-            "Color", typeof(Brush), typeof(ProjectLabel), new PropertyMetadata(default(Brush)));
-
-        public Brush Color
-        {
-            get { return (Brush) GetValue(ColorProperty); }
-            set { SetValue(ColorProperty, value); }
-        }
-
-        public static readonly DependencyProperty ProjectNameProperty = DependencyProperty.Register(
-            "ProjectName", typeof(string), typeof(ProjectLabel), new PropertyMetadata(default(string), OnProjectNameChanged));
-
-        public string ProjectName
-        {
-            get { return (string) GetValue(ProjectNameProperty); }
-            set { SetValue(ProjectNameProperty, value); }
-        }
-
-        public static readonly DependencyProperty TaskNameProperty = DependencyProperty.Register(
-            "TaskName", typeof(string), typeof(ProjectLabel), new PropertyMetadata(default(string)));
-
-        public string TaskName
-        {
-            get { return (string) GetValue(TaskNameProperty); }
-            set { SetValue(TaskNameProperty, value); }
-        }
-
-        public static readonly DependencyProperty ClientNameProperty = DependencyProperty.Register(
-            "ClientName", typeof(string), typeof(ProjectLabel), new PropertyMetadata(default(string)));
-
-        public string ClientName
-        {
-            get { return (string) GetValue(ClientNameProperty); }
-            set { SetValue(ClientNameProperty, value); }
-        }
-
-        private static void OnProjectNameChanged(DependencyObject obj, DependencyPropertyChangedEventArgs args)
-        {
-            var projectLabel = obj as ProjectLabel;
-            var showAddProjectCaption = projectLabel.ShowAddProjectCaption && projectLabel.IsEmpty;
-            projectLabel.addProjectCaption.ShowOnlyIf(showAddProjectCaption);
-        }
-
-        public bool IsEmpty => string.IsNullOrEmpty(ProjectName);
 
         public void Clear()
         {
-            ProjectName = string.Empty;
-            TaskName = string.Empty;
-            ClientName = string.Empty;
-            Color = default;
+            ViewModel.ProjectName = string.Empty;
+            ViewModel.TaskName = string.Empty;
+            ViewModel.ClientName = string.Empty;
+            ViewModel.Color = default;
         }
 
         public void SetProject(Toggl.TogglTimeEntryView item)
         {
-            ProjectName = item.ProjectLabel;
-            TaskName = item.TaskLabel;
-            ClientName = item.ClientLabel;
-            Color = Utils.ProjectColorBrushFromString(item.Color);
+            ViewModel.ProjectName = item.ProjectLabel;
+            ViewModel.TaskName = item.TaskLabel;
+            ViewModel.ClientName = item.ClientLabel;
+            ViewModel.Color = Utils.ProjectColorBrushFromString(item.Color);
+        }
+
+        public void SetProject(Toggl.TogglAutocompleteView item)
+        {
+            ViewModel.ProjectName = item.ProjectLabel;
+            ViewModel.TaskName = item.TaskLabel;
+            ViewModel.ClientName = item.ClientLabel;
+            ViewModel.Color = Utils.ProjectColorBrushFromString(item.ProjectColor);
         }
     }
 }
