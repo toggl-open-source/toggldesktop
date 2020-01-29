a7c32ce87c4b45f52ce1fb8768d5eef40eaad6ba
Taras Petruk <taras.petruk@gmail.com>
2020-01-29 13:51:41 +0200

    Wire up close button and notification body click events (win)

--- src/ui/windows/TogglDesktop/TogglDesktop/ui/controls/AutotrackerNotification.xaml.cs
+++ src/ui/windows/TogglDesktop/TogglDesktop/ui/controls/AutotrackerNotification.xaml.cs
@@ -8,16 +8,12 @@ namespace TogglDesktop
 {
     public partial class AutotrackerNotification
     {
-        private readonly TaskbarIcon icon;
-        private readonly MainWindow mainWindow;
-
         private ulong projectId;
         private ulong taskId;
 
         public AutotrackerNotification(TaskbarIcon icon, MainWindow mainWindow)
+            : base(icon, mainWindow)
         {
-            this.icon = icon;
-            this.mainWindow = mainWindow;
             this.InitializeComponent();
             Toggl.OnAutotrackerNotification += this.onAutotrackerNotification;
         }
@@ -33,19 +29,14 @@ namespace TogglDesktop
 
             this.RemoveFromParent();
 
-            this.icon.ShowNotification(this, PopupAnimation.Slide, TimeSpan.FromSeconds(6));
+            _icon.ShowNotification(this, PopupAnimation.Slide, TimeSpan.FromSeconds(6));
         }
 
         private void onStartButtonClick(object sender, RoutedEventArgs e)
         {
-            this.icon.CloseBalloon();
+            Close();
             Toggl.Start("", "", this.taskId, this.projectId, null, null);
-            this.mainWindow.ShowOnTop();
-        }
-
-        private void onNotificationMouseDown(object sender, MouseButtonEventArgs e)
-        {
-            this.icon.CloseBalloon();
+            _parentWindow.ShowOnTop();
         }
     }
 }
