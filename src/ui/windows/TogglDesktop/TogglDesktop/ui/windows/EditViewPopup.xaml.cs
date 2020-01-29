f885b7e75c49ea7b8830c6fdb2b3206a62898e1b
Taras Petruk <taras.petruk@gmail.com>
2020-01-29 16:44:47 +0200

    Migrate EditViewPopup to MetroWindow, remove inside shadow (win)

--- src/ui/windows/TogglDesktop/TogglDesktop/ui/windows/EditViewPopup.xaml.cs
+++ src/ui/windows/TogglDesktop/TogglDesktop/ui/windows/EditViewPopup.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Windows;
-using System.Windows.Input;
-using System.Windows.Interop;
 using System.Windows.Media.Animation;
 using TogglDesktop.Diagnostics;
 
@@ -18,17 +16,16 @@ namespace TogglDesktop
 
         private AnimationStates animationState = AnimationStates.Closing;
 
-        private readonly WindowInteropHelper interopHelper;
-
         private bool isLeft;
-        private bool isResizing;
         private bool skipAnimation;
         private object animationToken;
+
+        private readonly Thickness _leftResizeBorderThickness = new Thickness(4, 0, 0, 0);
+        private readonly Thickness _rightResizeBorderThickness = new Thickness(0, 0, 4, 0);
 
         public EditViewPopup()
         {
             this.InitializeComponent();
-            this.interopHelper = new WindowInteropHelper(this);
 
             this.MinWidth = this.EditView.MinWidth;
             this.mainGrid.Width = 0;
@@ -84,33 +81,11 @@ namespace TogglDesktop
             }
         }
 
-        #region ui events
-
-        private void onResizeHandleLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            this.startResizing();
-        }
-
-        private void onResizeHandleLeftButtonUp(object sender, MouseButtonEventArgs e)
-        {
-            this.endResizing();
-        }
-
-        private void onWindowMouseMove(object sender, MouseEventArgs e)
-        {
-            if (e.LeftButton == MouseButtonState.Released)
-            {
-                this.endResizing();
-            }
-        }
-
         protected override void OnClosing(CancelEventArgs e)
         {
             this.ClosePopup();
             e.Cancel = true;
         }
-
-        #endregion
 
         #region animate
 
@@ -208,14 +183,10 @@ namespace TogglDesktop
 
         #endregion
 
-        #region controlling
-
         public void SetPlacement(bool left,
             double x, double y, double height, double maxWidth, bool fixHeight = false)
         {
-            this.setShadow(left ^ fixHeight, height);
-
-            this.resizeHandle.HorizontalAlignment = left ? HorizontalAlignment.Left : HorizontalAlignment.Right;
+            this.ResizeBorderThickness = left ? _leftResizeBorderThickness : _rightResizeBorderThickness;
 
             this.skipAnimation = fixHeight;
 
@@ -239,47 +210,5 @@ namespace TogglDesktop
 
             this.MaxWidth = maxWidth;
         }
-
-        private void setShadow(bool left, double height)
-        {
-            this.mainFormShadow.Height = height;
-            this.mainFormShadow.HorizontalAlignment = left ? HorizontalAlignment.Right : HorizontalAlignment.Left;
-        }
-
-        private void startResizing()
-        {
-            if (this.isResizing)
-                return;
-
-            const int htleft = 10;
-            const int htright = 11;
-
-            Mouse.Capture(null);
-
-            this.ResizeMode = ResizeMode.CanResize;
-
-            Win32.SendMessage(this.interopHelper.Handle,
-                Win32.wmNcLButtonDown,
-                this.isLeft ? htright : htleft,
-                0);
-
-            this.resizeHandle.CaptureMouse();
-
-            this.isResizing = true;
-        }
-
-        private void endResizing()
-        {
-            if (!this.isResizing)
-                return;
-
-            Mouse.Capture(null);
-            this.ResizeMode = ResizeMode.NoResize;
-            this.isResizing = false;
-        }
-
-
-        #endregion
-
     }
 }
