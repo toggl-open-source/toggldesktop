e4633ce4574d05b13704ed27316dff055af2e9bd
Taras Petruk <taras.petruk@gmail.com>
2020-02-04 14:50:17 +0200

    Make it possible to add Buttons inside AutoCompletionPopup (win)

--- src/ui/windows/TogglDesktop/TogglDesktop/AutoCompletion/ListBoxItem.cs
+++ src/ui/windows/TogglDesktop/TogglDesktop/AutoCompletion/ListBoxItem.cs
@@ -11,7 +11,7 @@
         public string ClientLabel { get; set; }
         public string Category { get; set; }
         public string WorkspaceName { get; set; }
-        public int Type { get; set; }
+        public ItemType Type { get; set; }
         public int Index { get; set; }
     }
 }
