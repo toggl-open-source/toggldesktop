051240c5f22da82048ef60e281a2efb63791bf60
Nghia Tran <vinhnghiatran@gmail.com>
2020-02-04 20:36:26 +0700

    Enabled Timeline Record if the user enable Auto Tracker (mac)

--- src/ui/osx/TogglDesktop/UIEvents.m
+++ src/ui/osx/TogglDesktop/UIEvents.m
@@ -58,6 +58,7 @@ NSString *const kStartDisplayInAppMessage = @"kStartDisplayInAppMessage";
 NSString *const kDeselectAllTimeEntryList = @"kDeselectAllTimeEntryList";
 NSString *const kDidAdddManualTimeNotification = @"kDidAdddManualTimeNotification";
 NSString *const kStarTimeEntryWithStartTime = @"kStarTimeEntryWithStartTime";
+NSString *const kEnabledTimelineRecorder = @"kEnabledTimelineRecorder";
 
 const char *kFocusedFieldNameDuration = "duration";
 const char *kFocusedFieldNameDescription = "description";
