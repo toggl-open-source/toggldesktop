faaf5824c3a5368b06d155a33d30a8dd131a20a2
Taras Petruk <taras.petruk@gmail.com>
2020-02-17 18:28:16 +0200

    Implement Autotracker tab new design (win)

--- src/ui/windows/TogglDesktop/TogglDesktop/ui/controls/ProjectTextBox.cs
+++ src/ui/windows/TogglDesktop/TogglDesktop/ui/controls/ProjectTextBox.cs
@@ -6,54 +6,55 @@ namespace TogglDesktop
 {
     class ProjectTextBox : ExtendedTextBox
     {
-        private string taskText;
+        // private string taskText;
         private TextBlock taskTextBlock;
 
-        public override void OnApplyTemplate()
-        {
-            base.OnApplyTemplate();
-
-            this.taskTextBlock = (TextBlock)this.GetTemplateChild("taskTextBlock");
-            this.updateTaskText();
-        }
+        // public override void OnApplyTemplate()
+        // {
+        //     base.OnApplyTemplate();
+        //
+        //     // this.taskTextBlock = (TextBlock)this.GetTemplateChild("taskTextBlock");
+        //     // this.updateTaskText();
+        // }
 
         protected override void OnTextChanged(TextChangedEventArgs e)
         {
-            if (!this.IsTextChangingProgrammatically)
-            {
-                this.setTask(null);
-            }
+            // if (!this.IsTextChangingProgrammatically)
+            // {
+            //     this.setTask(null);
+            // }
 
             base.OnTextChanged(e);
         }
 
         public void SetText(string project, string task)
         {
-            this.SetText(project);
-            this.setTask(task);
+            var resultText = project + (task.IsNullOrEmpty() ? "" : $" - {task}");
+            this.SetText(resultText);
+            // this.setTask(task);
         }
 
-        private void setTask(string task)
-        {
-            this.taskText = task;
-            this.updateTaskText();
-        }
+        // private void setTask(string task)
+        // {
+        //     this.taskText = task;
+        //     this.updateTaskText();
+        // }
 
-        private void updateTaskText()
-        {
-            if (this.taskTextBlock == null)
-                return;
-
-            if (string.IsNullOrEmpty(this.taskText))
-            {
-                this.taskTextBlock.Text = "";
-                this.taskTextBlock.Visibility = Visibility.Collapsed;
-            }
-            else
-            {
-                this.taskTextBlock.Text = "- " + this.taskText;
-                this.taskTextBlock.Visibility = Visibility.Visible;
-            }
-        }
+        // private void updateTaskText()
+        // {
+        //     if (this.taskTextBlock == null)
+        //         return;
+        //
+        //     if (string.IsNullOrEmpty(this.taskText))
+        //     {
+        //         this.taskTextBlock.Text = "";
+        //         this.taskTextBlock.Visibility = Visibility.Collapsed;
+        //     }
+        //     else
+        //     {
+        //         this.taskTextBlock.Text = "- " + this.taskText;
+        //         this.taskTextBlock.Visibility = Visibility.Visible;
+        //     }
+        // }
     }
 }
