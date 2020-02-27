953a6ec3648a9d991d7159d9a0078d3875d39e35
Nghia Tran <vinhnghiatran@gmail.com>
2020-02-27 16:37:15 +0700

    Fix missing parse_releases.sh (ga)

--- dist/osx/update_releases.sh
+++ dist/osx/update_releases.sh
@@ -16,11 +16,15 @@ if [[ ! $channel =~ ^(dev|beta|stable)$ ]]; then
 fi
 
 prepare() {
+  # Copy to tmp folder before checkout to gh-pages
   cp dist/osx/update_updates.sh tmp/update_updates.sh
   cp dist/osx/update_release_links.sh tmp/update_release_links.sh
+  cp dist/osx/parse_releases.sh tmp/parse_releases.sh
 }
 
 fetch_releases () {
+  # Checkout gh-page in Github action
+  # Need to add TogglDesktop repo as an origin
   git remote update
   git remote set-url origin https://github.com/toggl-open-source/toggldesktop.git
   git remote -v
@@ -47,7 +51,7 @@ copy_releases_to_gh_pages () {
 prepare
 fetch_releases
 timestamp=$(date +"%d-%m-%Y")
-release_cmd="OS=$os CHANNEL=$channel VERSION=$version TIMESTAMP=$timestamp ./parse_releases.sh"
+release_cmd="OS=$os CHANNEL=$channel VERSION=$version TIMESTAMP=$timestamp ./tmp/parse_releases.sh"
 echo $release_cmd
 eval $release_cmd
 ./tmp/update_updates.sh
