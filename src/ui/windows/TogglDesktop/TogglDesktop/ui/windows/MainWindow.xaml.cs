402ea2b45a3e9e65e4c561d0bb0d01d4bb363d3f
Taras Petruk <taras.petruk@gmail.com>
2020-03-03 15:13:19 +0200

    Remove running entry text from the context menu (win)

--- src/ui/windows/TogglDesktop/TogglDesktop/ui/windows/MainWindow.xaml.cs
+++ src/ui/windows/TogglDesktop/TogglDesktop/ui/windows/MainWindow.xaml.cs
@@ -806,16 +806,9 @@ namespace TogglDesktop
 
                 var description = timeEntry.Value.Description;
 
-                if (string.IsNullOrEmpty(description))
-                {
-                    this.Title = "Toggl Desktop";
-                    this.runningMenuText.Text = "Timer is tracking";
-                }
-                else
-                {
-                    this.Title = description + " - Toggl Desktop";
-                    this.runningMenuText.Text = description;
-                }
+                this.Title = string.IsNullOrEmpty(description)
+                    ? "Toggl Desktop"
+                    : description + " - Toggl Desktop";
 
                 if (this.IsInManualMode)
                     this.SetManualMode(false);
@@ -823,7 +816,6 @@ namespace TogglDesktop
             else
             {
                 this.trayToolTip.TimeEntryLabel = null;
-                this.runningMenuText.Text = "Timer is not tracking";
                 this.Title = "Toggl Desktop";
             }
 
