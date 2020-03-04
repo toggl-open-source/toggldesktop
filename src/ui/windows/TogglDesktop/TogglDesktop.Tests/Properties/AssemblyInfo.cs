026f6a44ae8f6e9b00499f25ffaf111bf6f23b0f
Taras Petruk <taras.petruk@gmail.com>
2020-03-04 19:29:00 +0200

    Cover library with C# tests (win)

--- src/ui/windows/TogglDesktop/TogglDesktop.Tests/Properties/AssemblyInfo.cs
+++ src/ui/windows/TogglDesktop/TogglDesktop.Tests/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TogglDesktopDLLInteropTest")]
+[assembly: AssemblyTitle("TogglDesktop.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TogglDesktopDLLInteropTest")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyProduct("TogglDesktop.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c8440fba-f925-4afb-9fa4-985273987893")]
+[assembly: Guid("39E383CB-D417-4568-A37E-58AEEF4B7C73")]
 
 // Version information for an assembly consists of the following four values:
 //
