d0c68e1591fc8c32ec559a469f4e51f7ff268689
Taras Petruk <taras.petruk@gmail.com>
2020-03-17 17:16:57 +0200

    Bump the copyright year (win)

--- src/ui/windows/TogglDesktop/TogglDesktop/Properties/AssemblyInfo.cs
+++ src/ui/windows/TogglDesktop/TogglDesktop/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Toggl")]
 [assembly: AssemblyProduct("TogglDesktop")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
