e71076e8f674ed8fdff33ad2037e8bd1b4cd6ec9
Nghia Tran <vinhnghiatran@gmail.com>
2020-04-01 14:08:07 +0700

    Basic model for each kind of Onboarding (mac)

---
+++ src/ui/osx/TogglDesktop/Feature/Onboarding/OnboardingService.swift
@@ -0,0 +1,13 @@
+//
+//  OnboardingService.swift
+//  TogglDesktop
+//
+//  Created by Nghia Tran on 4/1/20.
+//  Copyright © 2020 Alari. All rights reserved.
+//
+
+import Foundation
+
+final class OnboardingService {
+    
+}
