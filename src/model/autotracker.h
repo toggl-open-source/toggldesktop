3d8277f5b792c62613a315a41fcdd6c2ff0a254e
Taras Petruk <taras.petruk@gmail.com>
2020-04-01 15:56:11 +0300

    Add days of week to AutotrackerRule model (lib)
    add day of week matching (lib)
    
    Initialize days of week bitset (lib)

--- src/model/autotracker.h
+++ src/model/autotracker.h
@@ -6,6 +6,7 @@
 #include <string>
 #include <sstream>
 #include <vector>
+#include <bitset>
 
 #include <Poco/Types.h>
 
@@ -25,14 +26,18 @@ class TOGGL_INTERNAL_EXPORT AutotrackerRule : public BaseModel {
     Property<std::vector<std::string>> Terms;
     Property<Poco::UInt64> PID { 0 };
     Property<Poco::UInt64> TID { 0 };
+    Property<std::bitset<7>> DaysOfWeek { std::bitset<7>(0x7F) };
 
     const std::string TermsString() const;
 
     bool Matches(const TimelineEvent &event) const;
+    
+    Poco::UInt32 DaysOfWeekUInt32() const;
 
     void SetTerms(const std::string &value);
     void SetPID(Poco::UInt64 value);
     void SetTID(Poco::UInt64 value);
+    void SetDaysOfWeek(const Poco::UInt32 daysOfWeek);
 
     // Override BaseModel
     std::string String() const override;
