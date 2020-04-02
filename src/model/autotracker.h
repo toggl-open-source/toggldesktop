1159adabca2f4db4a4454eed60902325addb9cb6
Taras Petruk <taras.petruk@gmail.com>
2020-04-02 19:32:18 +0300

    Add start time and end time to AutotrackerRule model (lib)
    Add getters/setters for Start/End Time (lib)

--- src/model/autotracker.h
+++ src/model/autotracker.h
@@ -27,6 +27,8 @@ class TOGGL_INTERNAL_EXPORT AutotrackerRule : public BaseModel {
     Property<Poco::UInt64> PID { 0 };
     Property<Poco::UInt64> TID { 0 };
     Property<std::bitset<7>> DaysOfWeek { std::bitset<7>(0x7F) };
+    Property<std::string> StartTime { "" };
+    Property<std::string> EndTime { "" };
 
     const std::string TermsString() const;
 
@@ -38,6 +40,8 @@ class TOGGL_INTERNAL_EXPORT AutotrackerRule : public BaseModel {
     void SetPID(Poco::UInt64 value);
     void SetTID(Poco::UInt64 value);
     void SetDaysOfWeek(const Poco::UInt32 daysOfWeek);
+    void SetStartTime(const std::string &value);
+    void SetEndTime(const std::string &value);
 
     // Override BaseModel
     std::string String() const override;
