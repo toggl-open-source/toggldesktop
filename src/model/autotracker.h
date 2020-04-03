d96f04dde8ce0872a715e526102caf171a50a7b7
Taras Petruk <taras.petruk@gmail.com>
2020-04-03 16:21:43 +0300

    make zero the default value for days_of_week_ for more error-prone code (lib)

--- src/model/autotracker.h
+++ src/model/autotracker.h
@@ -26,7 +26,7 @@ class TOGGL_INTERNAL_EXPORT AutotrackerRule : public BaseModel {
     Property<std::vector<std::string>> Terms;
     Property<Poco::UInt64> PID { 0 };
     Property<Poco::UInt64> TID { 0 };
-    Property<std::bitset<7>> DaysOfWeek { std::bitset<7>(0x7F) };
+    Property<std::bitset<7>> DaysOfWeek { std::bitset<7>(0) };
     Property<std::string> StartTime { "" };
     Property<std::string> EndTime { "" };
 
