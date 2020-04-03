7a3d47c976ab0781ab44c8de03c77b1409b40561
Nghia Tran <vinhnghiatran@gmail.com>
2020-04-03 20:07:46 +0700

    Introduce OnboardingService (lib)

---
+++ src/lib/osx/Kopsik/onboarding_service.cpp
@@ -0,0 +1,30 @@
+//
+//  onboarding_service.cpp
+//  TogglDesktopLibrary
+//
+//  Created by Nghia Tran on 4/3/20.
+//  Copyright © 2020 Toggl. All rights reserved.
+//
+
+#include "onboarding_service.h"
+
+namespace toggl {
+
+// User actions
+void OnboardingService::openApp() {
+
+}
+
+void OnboardingService::stopTimeEntry() {
+
+}
+
+void OnboardingService::openTimelineTab() {
+
+}
+
+void OnboardingService::turnOnRecordActivity() {
+
+}
+
+}
