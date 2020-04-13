f8ab36daf1a033b2142309f2ba0868dee1dc7a00
Taras Petruk <taras.petruk@gmail.com>
2020-04-13 10:18:54 +0300

    Sign with a password (win)

--- dist/windows/scripts/sign.sh
+++ dist/windows/scripts/sign.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# This script accepts the following parameters:
+#
+# * cert_password
+
+CONFIG=$@
+
+for line in $CONFIG; do
+  eval "$line"
+done
+
 if [[ -z "$SIGNTOOL" ]]; then
     SIGNTOOL="/c/Program Files (x86)/Windows Kits/10/bin/10.0.18362.0/x64/signtool.exe"
 fi
@@ -13,6 +23,6 @@ arguments=$(sed 's/\</\*./g' <<< $EXTENSIONS | sed 's/\s/ -or -name /g' | sed 's
 
 for i in $(find $PATHS $arguments 2>/dev/null); do
     echo "Signing $i"
-    "$SIGNTOOL" sign -a -t http://timestamp.verisign.com/scripts/timestamp.dll -f "Certificate.pfx" $i
+    "$SIGNTOOL" sign -a -t http://timestamp.verisign.com/scripts/timestamp.dll -f "Certificate.pfx" -p $cert_password $i
 done
 
