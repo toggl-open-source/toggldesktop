e0cf84e5628a5c7213dd3974cc55068027a80d77
Nghia Tran <vinhnghiatran@gmail.com>
2020-05-11 15:32:11 +0700

    Support Success & Rule State (mac)

--- src/ui/osx/TogglDesktop/Feature/PasswordStrength/PasswordStrengthView.swift
+++ src/ui/osx/TogglDesktop/Feature/PasswordStrength/PasswordStrengthView.swift
@@ -10,19 +10,34 @@ import Cocoa
 
 final class PasswordStrengthView: NSViewController {
 
+    enum DisplayState {
+        case success // Show Success state
+        case hasIssues // Show all password requirements
+    }
+
     // MARK: OUTLET
 
     @IBOutlet weak var titleLbl: NSTextField!
     @IBOutlet weak var stackView: NSStackView!
+    @IBOutlet weak var successStackView: NSStackView!
+    @IBOutlet weak var successBox: NSBox!
+    @IBOutlet weak var ruleBox: NSBox!
 
     // MARK: Variables
 
     private lazy var validation = PasswordStrengthValidation()
+    private var displayState = DisplayState.hasIssues {
+        didSet {
+            guard displayState != oldValue else { return }
+            updateUI()
+        }
+    }
 
     // MARK: View Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        prepareUI()
         renderRulesStackView()
     }
 
@@ -30,6 +45,11 @@ final class PasswordStrengthView: NSViewController {
 
     @objc func updateValidation(for text: String) {
         let matchedRules = validation.validate(text: text)
+
+        // If all rules are matched -> Update the UI
+        displayState = matchedRules.count == PasswordStrengthValidation.Rule.allCases.count ? .success : .hasIssues
+
+        // Update individual state
         stackView.arrangedSubviews.forEach { (view) in
             guard let ruleView = view as? PasswordRuleView,
                 let rule = ruleView.rule else { return }
@@ -47,6 +67,12 @@ final class PasswordStrengthView: NSViewController {
 
 extension PasswordStrengthView {
 
+    private func prepareUI() {
+        let view = PasswordRuleView.xibView() as PasswordRuleView
+        view.updateSuccessStatus()
+        successStackView.addArrangedSubview(view)
+    }
+
     private func renderRulesStackView() {
         let views = PasswordStrengthValidation.Rule.allCases.map { rule -> PasswordRuleView in
             let view = PasswordRuleView.xibView() as PasswordRuleView
@@ -55,6 +81,17 @@ extension PasswordStrengthView {
         }
         views.forEach {
             stackView.addArrangedSubview($0)
+        }
+    }
+
+    private func updateUI() {
+        switch displayState {
+        case .success:
+            successBox.isHidden = false
+            ruleBox.isHidden = true
+        case .hasIssues:
+            successBox.isHidden = true
+            ruleBox.isHidden = false
         }
     }
 }
