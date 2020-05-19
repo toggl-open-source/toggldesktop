d5029db54bec415730292fb26dfefbee131ef461
Taras Petruk <taras.petruk@gmail.com>
2020-05-19 10:25:18 +0300

    Make PasswordBox popup visible only when passwordbox contains focus and action is signup (win)

--- src/ui/windows/TogglDesktop/TogglDesktop/ui/Converters/EnumEqualConverter.cs
+++ src/ui/windows/TogglDesktop/TogglDesktop/ui/Converters/EnumEqualConverter.cs
@@ -1,16 +1,15 @@
 using System;
 using System.Globalization;
-using System.Windows;
 using System.Windows.Data;
 
 namespace TogglDesktop.Converters
 {
-    public class EnumEqualToVisibilityConverter : IValueConverter
+    public class EnumEqualConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value == null || parameter == null || !value.GetType().IsEnum) return false;
-            return Enum.Equals(value, parameter) ? Visibility.Visible : Visibility.Collapsed;
+            return Enum.Equals(value, parameter);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
