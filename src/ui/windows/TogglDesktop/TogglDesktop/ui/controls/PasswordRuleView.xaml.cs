0b8d563b7114b943562f46fa68e41e81f019242c
Taras Petruk <taras.petruk@gmail.com>
2020-05-25 19:09:31 +0300

    Extract rule into a separate view (win)

---
+++ src/ui/windows/TogglDesktop/TogglDesktop/ui/controls/PasswordRuleView.xaml.cs
@@ -0,0 +1,40 @@
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TogglDesktop
+{
+    public partial class PasswordRuleView : UserControl
+    {
+        public PasswordRuleView()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty IsRuleSatisfiedProperty = DependencyProperty.Register(
+            "IsRuleSatisfied", typeof(bool), typeof(PasswordRuleView), new PropertyMetadata(default(bool)));
+
+        public bool IsRuleSatisfied
+        {
+            get { return (bool) GetValue(IsRuleSatisfiedProperty); }
+            set { SetValue(IsRuleSatisfiedProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsValidationFailedProperty = DependencyProperty.Register(
+            "IsValidationFailed", typeof(bool), typeof(PasswordRuleView), new PropertyMetadata(default(bool)));
+
+        public bool IsValidationFailed
+        {
+            get { return (bool) GetValue(IsValidationFailedProperty); }
+            set { SetValue(IsValidationFailedProperty, value); }
+        }
+
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register(
+            "Text", typeof(string), typeof(PasswordRuleView), new PropertyMetadata(default(string)));
+
+        public string Text
+        {
+            get { return (string) GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+    }
+}
