01eedeb278f378bae98794c271df418280e2d8e3
Taras Petruk <taras.petruk@gmail.com>
2020-06-02 14:13:03 +0300

    support double type by 'make csapi'
    try to fix 'make csapi' issue with no-parameter functions
    
    'make csapi' enum support part 1
    
    do not generate ToString() if struct has no string fields

--- src/toggl_api.h
+++ src/toggl_api.h
@@ -321,7 +321,7 @@ extern "C" {
         TogglTimeEntryView *te);
 
     typedef void (*TogglContinueSignIn)(
-    );
+        );
 
     typedef void (*TogglDisplayIdleNotification)(
         const char_t *guid,
