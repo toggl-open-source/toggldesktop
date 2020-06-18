ae78e5e29d30d1fd14dbf35bf8a7288a176c72b6
Andrii Nester <nester.andrej@gmail.com>
2020-06-18 17:15:49 +0300

    Add unit tests target to macOS project (mac)
    Issue #4107

--- src/context.cc
+++ src/context.cc
@@ -2360,6 +2360,16 @@ void Context::SetEnvironment(const std::string &value) {
 
     TogglClient::GetInstance().SetIgnoreCert(("development" == environment_));
     urls::SetRequestsAllowed("test" != environment_);
+
+    // stopping heavy tasks for better unit tests performance/speed
+    if (value == "test") {
+        if (ui_updater_.isRunning()) {
+            ui_updater_.stop();
+        }
+        if (reminder_.isRunning()) {
+            reminder_.stop();
+        }
+    }
 }
 
 Database *Context::db() const {
