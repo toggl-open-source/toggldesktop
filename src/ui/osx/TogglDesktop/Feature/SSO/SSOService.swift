50e7d2d6df3dcb7503a58e13a1f6e11d34224196
Nghia Tran <vinhnghiatran@gmail.com>
2020-06-19 16:01:50 +0700

    Update new SSO Callback (mac)

--- src/ui/osx/TogglDesktop/Feature/SSO/SSOService.swift
+++ src/ui/osx/TogglDesktop/Feature/SSO/SSOService.swift
@@ -17,7 +17,7 @@ final class SSOService {
             static let SsoError = "ssoError"
             static let LoginPath = "sso-login"
             static let MustLinkPath = "sso-must-link"
-            static let Finish = "finish"
+            static let APIToken = "api_token"
         }
 
         struct Values {
@@ -29,11 +29,9 @@ final class SSOService {
     static let shared = SSOService()
 
     enum Route {
-        case success
-        case needConfirmationBeforeLink(String, String)
-        case emailDoNotExist
-        case notInWorkspace
-        case none
+        case success(String)
+        case needLinkSSO(String, String)
+        case error(String)
     }
 
     // MARK: Public
@@ -48,20 +46,14 @@ final class SSOService {
     }
 
     func handleCallback(with url: URL) {
+        // Determine if it's the valid URL callback
         guard let component = URLComponents(url: url, resolvingAgainstBaseURL: false) else {
             NotificationCenter.default.post(name: NSNotification.Name(rawValue: kDisplayError), object: "Invalid Callback URL \(url.absoluteString)")
             return
         }
 
         // https://www.notion.so/toggl/SSO-API-e791bd2451f84ccba43f2e1adba8d85f?p=6c0a5301131747c18be276bbcc211d57
-        // It's where we determine what the next steps are:
-        // 1 - Successful login (user authenticated on IdP and has linked or newly created account in Toggl)
-        // 2 - First successful login with existing Toggl account with the same email - needs confirmation before link account
-        // 3 - Toggl account with this email doesn't exist
-        // 4 - User is not part of the SSO workspace
-        //
-        // The Endpoint from Backend is not finished yet
-        // Prepare and update later
+        // Handle next steps from the URL callback
         let route = getRoute(with: component)
         handle(route)
     }
@@ -74,65 +66,57 @@ extension SSOService {
     private func getRoute(with component: URLComponents) -> Route {
         // Get the first path
         guard let host = component.host else {
-            present(error: "Missing Path")
-            return .none
+            return .error("Missing Path")
         }
 
         switch host {
-        case Constants.Keys.LoginPath:
+        case Constants.Keys.LoginPath: // sso-login
 
             // Success path
-            if let finish = component.queryItems?.first(where: { $0.name == Constants.Keys.Finish }), finish.value == "true" {
-                return Route.success
+            if let apiTokenQuery = component.queryItems?.first(where: { $0.name == Constants.Keys.APIToken }),
+                let apiToken = apiTokenQuery.value, !apiToken.isEmpty {
+                return .success(apiToken)
             }
 
             // Check error
             guard let errorCode = component.queryItems?.first(where: { $0.name == Constants.Keys.SsoError }),
                 let value = errorCode.value else {
-                    present(error: "Missing SSO error code")
-                    return .none
+                    return .error("Missing SSO error code")
             }
 
             switch value {
             case Constants.Values.NoAccount:
-                return Route.emailDoNotExist
+                return .error("SSO Email doesn't exist")
             case Constants.Values.NotInWorkspace:
-                return Route.notInWorkspace
+                return .error("SSO Email isn't belong to any workspace")
             default:
-                return .none
+                return .error("SSO Error")
             }
-
-        case Constants.Keys.MustLinkPath:
+        case Constants.Keys.MustLinkPath: // sso-must-link
             // Extract values from the query
             guard let codeQuery = component.queryItems?.first(where: { $0.name == Constants.Keys.ConfirmationCode }),
                 let code = codeQuery.value  else {
-                present(error: "Missing Confirmation Code")
-                return .none
+                return .error("Missing Confirmation Code")
             }
             guard let emailQuery = component.queryItems?.first(where: { $0.name == Constants.Keys.Email }),
                 let email = emailQuery.value  else {
-                present(error: "Missing SSO Email")
-                return .none
+                return .error("Missing SSO Email")
             }
-            return .needConfirmationBeforeLink(email, code)
+            return .needLinkSSO(email, code)
         default:
-            present(error: "Unsupported path \(host)")
+            break
         }
-        return .none
+        return .error("Unsupported path \(host)")
     }
 
     private func handle(_ route: Route) {
         switch route {
-        case .emailDoNotExist:
-            present(error: "SSO Email doesn't exist")
-        case .notInWorkspace:
-            present(error: "SSO Email isn't belong to any workspace")
-        case .success:
+        case .error(let message):
+            present(error: message)
+        case .success(let apiToken):
             break
-        case .needConfirmationBeforeLink(let email, let code):
+        case .needLinkSSO(let email, let code):
             NotificationCenter.default.post(name: Notification.Name(rawValue: kLinkSSOEmail), object: SSOPayload(email: email, confirmationCode: code))
-        case .none:
-            break
         }
     }
 
