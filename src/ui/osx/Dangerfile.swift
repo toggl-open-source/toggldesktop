9081932b8838d094e5c4406fc577bab12ef1e582
Andrii Nester <nester.andrej@gmail.com>
2020-07-23 20:04:42 +0300

    Danger again

--- src/ui/osx/Dangerfile.swift
+++ src/ui/osx/Dangerfile.swift
@@ -4,12 +4,12 @@ let danger = Danger()
 let editedFiles = danger.git.modifiedFiles + danger.git.createdFiles
 message("These files have changed: \(editedFiles.joined(separator: ", "))")
 
-//SwiftLint.lint()
+SwiftLint.lint()
 
 // Instead of making a markdown table in the main message
 // sprinkle those comments inline, this can be a bit noisy
 // but it definitely feels magical.
-SwiftLint.lint(inline: true)
+//SwiftLint.lint(inline: true)
 
 // Have different runs of SwiftLint against different sub-folders
 //SwiftLint.lint(directory: "TogglDesktop", configFile: "TogglDesktop/.swiftlint.yml")
