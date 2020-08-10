fd6e763a0488b8058d2cdfb7deb7d5ad063ff1a4
Taras Petruk <taras.petruk@gmail.com>
2020-08-10 15:56:00 +0300

    Call fetchMessage in staging as well because the production/staging check occurs later anyway (lib)

--- src/context.cc
+++ src/context.cc
@@ -307,8 +307,8 @@ error Context::StartEvents() {
 
             analytics_.TrackOs(db_->AnalyticsClientID(), os_info.str());
             analytics_.TrackOSDetails(db_->AnalyticsClientID());
-            fetchMessage(0);
         }
+        fetchMessage(0);
     } catch(const Poco::Exception& exc) {
         return displayError(exc.displayText());
     } catch(const std::exception& ex) {
