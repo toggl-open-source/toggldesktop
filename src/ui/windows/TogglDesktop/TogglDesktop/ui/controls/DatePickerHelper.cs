bfa2b434f0b533697711b55a3961752ed4f37103
atsvirchkova <anna.tsvirchkova@toggl.com>
2020-08-18 12:12:42 +0200

    Style refactoring for date picker

---
+++ src/ui/windows/TogglDesktop/TogglDesktop/ui/controls/DatePickerHelper.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace TogglDesktop
+{
+    public class DatePickerHelper : DependencyObject
+    {
+        public static readonly DependencyProperty PreviousDayCommandProperty = 
+            DependencyProperty.RegisterAttached("PreviousDayCommand", typeof(ICommand),
+                typeof(DatePickerHelper));
+
+        public static ICommand GetPreviousDayCommand(DatePickerHelper d)
+        {
+            return (ICommand) d.GetValue(PreviousDayCommandProperty);
+        }
+
+        public static void SetPreviousDayCommand(DatePickerHelper d, ICommand value)
+        {
+            d.SetValue(PreviousDayCommandProperty, value);
+        }
+
+        public static readonly DependencyProperty NextDayCommandProperty =
+            DependencyProperty.RegisterAttached("NextDayCommand", typeof(ICommand),
+                typeof(DatePickerHelper));
+
+        public static ICommand GetNextDayCommand(DatePickerHelper d)
+        {
+            return (ICommand)d.GetValue(PreviousDayCommandProperty);
+        }
+
+        public static void SetNextDayCommand(DatePickerHelper d, ICommand value)
+        {
+            d.SetValue(PreviousDayCommandProperty, value);
+        }
+    }
+}
