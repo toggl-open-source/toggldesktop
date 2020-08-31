0f292a6016983179b3531ea7bef94de3aa88c731
atsvirchkova <anna.tsvirchkova@toggl.com>
2020-08-31 16:49:31 +0200

    Fix dark mode colors

--- src/ui/windows/TogglDesktop/TogglDesktop/ui/ViewModels/TimelineViewModel.cs
+++ src/ui/windows/TogglDesktop/TogglDesktop/ui/ViewModels/TimelineViewModel.cs
@@ -48,7 +48,6 @@ namespace TogglDesktop.ViewModels
 
         public ReactiveCommand<Unit, Unit> SelectNextDay { get; }
 
-        [Reactive]
         public List<DateTime> HourViews { get; }
 
         [Reactive]
