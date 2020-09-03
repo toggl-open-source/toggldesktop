67bd28447d9e51e1c822f2a14fb8ef2f8e210037
atsvirchkova <anna.tsvirchkova@toggl.com>
2020-09-03 19:48:01 +0200

    Fix build - add missing file

---
+++ src/ui/windows/TogglDesktop/TogglDesktop/ui/Converters/RectangleHeightToRadiusConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace TogglDesktop.Converters
+{
+    public class RectangleHeightToRadiusConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (parameter == null || !(value is double)) return 0;
+            var height = (double)value;
+            if (height >= 15) return parameter.ToString() == "RadiusX" ? 15 : 7;
+            if (height >= 5) return parameter.ToString() == "RadiusX" ? 15 : 2;
+            return 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
