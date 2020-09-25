8b3ed6dc9d8539dc55416f53c5690a4b12851178
Andrii Nester <nester.andrej@gmail.com>
2020-09-25 13:31:10 +0300

    Part 3

--- src/ui/osx/TogglDesktop/Features/Timer/TimerDescriptionFieldHandler.swift
+++ src/ui/osx/TogglDesktop/Features/Timer/TimerDescriptionFieldHandler.swift
@@ -10,6 +10,11 @@ import Foundation
 
 class TimerDescriptionFieldHandler {
 
+    enum Constants {
+        static let projectToken: Character = "@"
+        static let tagToken: Character = "#"
+    }
+
     enum State {
         case descriptionUpdate(String)
         case projectDropdownShow
@@ -30,6 +35,13 @@ class TimerDescriptionFieldHandler {
 
         let text = editor.string
         let cursorLocation = editor.selectedRange.location
+
+        let (token, query) = text.findTokenAndQueryMatchesForAutocomplete([Constants.projectToken], cursorLocation)
+
+        print(">>> token = \(token); query = \(query)")
+
+        return
+
         let lastTypedIndex = text.index(text.startIndex, offsetBy: cursorLocation - 1)
         let typedSymbol = text[lastTypedIndex]
 
@@ -87,5 +99,70 @@ class TimerDescriptionFieldHandler {
 //                }
 //            }
 //        }
+    }
+}
+
+private extension String {
+
+    func findTokenAndQueryMatchesForAutocomplete(
+        _ token: Character,
+        _ cursorPosition: Int
+    ) -> (Character?, String) {
+        return findTokenAndQueryMatchesForAutocomplete([token], cursorPosition)
+    }
+
+    func findTokenAndQueryMatchesForAutocomplete(
+        _ tokens: [Character],
+        _ cursorPosition: Int
+    ) -> (Character?, String) {
+
+        do {
+            let joinedTokens = tokens.map { String($0) }.joined(separator: "|")
+            let regex = try NSRegularExpression(pattern: "(^| )(\(joinedTokens))")
+            let searchRange = startIndex..<index(startIndex, offsetBy: cursorPosition.clamp(min: 0, max: self.count))
+            let matches = regex.matches(in: self, range: NSRange(searchRange, in: self))
+
+            guard let match = matches.last else { return (nil, self) }
+
+            let queryStart = index(startIndex, offsetBy: match.range.lowerBound)
+            let matchSubstring = self[queryStart..<endIndex]
+            let matchedTheFirstWord = tokens.contains { matchSubstring.starts(with: String($0)) }
+            let queryWithToken = String(matchedTheFirstWord ? matchSubstring : matchSubstring.dropFirst())
+
+            let token = queryWithToken.first
+            let query = String(queryWithToken.dropFirst())
+
+            return (token, query)
+
+        } catch {
+            return (nil, "")
+        }
+    }
+
+    static func matches(for regex: String, in text: String) -> [String] {
+
+        do {
+            let regex = try NSRegularExpression(pattern: regex)
+            let results = regex.matches(in: text,
+                                        range: NSRange(text.startIndex..., in: text))
+            return results.map {
+                String(text[Range($0.range, in: text)!])
+            }
+        } catch let error {
+            print("invalid regex: \(error.localizedDescription)")
+            return []
+        }
+    }
+}
+
+private extension Comparable {
+    func clamp(min: Self, max: Self) -> Self {
+        if self > max {
+            return max
+        } else if self < min {
+            return min
+        }
+
+        return self
     }
 }
