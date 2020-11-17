957506b4ba5efd1cc6d303bfa0129def9e43e9f6
atsvirchkova <anna.tsvirchkova@toggl.com>
2020-11-17 18:08:25 +0100

    Center activity blocks horizontally (win)

---
+++ src/ui/windows/TogglDesktop/TogglDesktop/ui/Converters/CalculateCenterConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace TogglDesktop.Converters
+{
+    public class CalculateCenterConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double totalWidth && parameter is double elementWidth)
+            {
+                return (totalWidth - elementWidth) / 2;
+            }
+
+            return 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
