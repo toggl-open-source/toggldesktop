f31fb6031682c36e91347e4ffe2b406399b299d9
Andrii Nester <nester.andrej@gmail.com>
2021-05-24 14:47:38 +0300

    Test notarization

--- dist/osx/build.sh
+++ dist/osx/build.sh
@@ -82,6 +82,9 @@ function notarize() {
     echo "Notarization" "Uploading for notarization…"
     /usr/bin/xcrun altool --notarize-app --primary-bundle-id "com.toggl.toggldesktop.TogglDesktop.zip" -itc_provider "B227VTMZ94" -u ${DEVELOPER_USERNAME} -p ${DEVELOPER_PASSWORD} -f ${BUNDLE_ZIP} --output-format xml > ${UPLOAD_INFO_PLIST} || cat ${UPLOAD_INFO_PLIST}
     echo "Notarization" "Waiting while notarized…"
+    
+    plutil -p ${UPLOAD_INFO_PLIST}
+
     while true; do
         /usr/bin/xcrun altool --notarization-info `/usr/libexec/PlistBuddy -c "Print :notarization-upload:RequestUUID" ${UPLOAD_INFO_PLIST}` -u ${DEVELOPER_USERNAME} -p ${DEVELOPER_PASSWORD} --output-format xml > ${REQUEST_INFO_PLIST} || cat ${REQUEST_INFO_PLIST}
         if [[ `/usr/libexec/PlistBuddy -c "Print :notarization-info:Status" ${REQUEST_INFO_PLIST}` != "in progress" ]]; then
