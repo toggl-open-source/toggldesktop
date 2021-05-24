ae92ff9b4f95df74e0dbe6556b9e84c051c53223
Andrii Nester <nester.andrej@gmail.com>
2021-05-24 15:01:32 +0300

    More logs

--- dist/osx/build.sh
+++ dist/osx/build.sh
@@ -84,9 +84,13 @@ function notarize() {
     echo "Notarization" "Waiting while notarized…"
     
     plutil -p ${UPLOAD_INFO_PLIST}
+    /usr/libexec/PlistBuddy -c "Print :notarization-upload:RequestUUID" ${UPLOAD_INFO_PLIST}
 
     while true; do
         /usr/bin/xcrun altool --notarization-info `/usr/libexec/PlistBuddy -c "Print :notarization-upload:RequestUUID" ${UPLOAD_INFO_PLIST}` -u ${DEVELOPER_USERNAME} -p ${DEVELOPER_PASSWORD} --output-format xml > ${REQUEST_INFO_PLIST} || cat ${REQUEST_INFO_PLIST}
+
+        plutil -p ${REQUEST_INFO_PLIST}
+
         if [[ `/usr/libexec/PlistBuddy -c "Print :notarization-info:Status" ${REQUEST_INFO_PLIST}` != "in progress" ]]; then
             break
         fi
